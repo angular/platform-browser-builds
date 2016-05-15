13b5f515fe849d24a96c7a2dd1db7ea0d16d4fe9
Martin Probst <martin@probst.io>
2016-05-15 09:57:20 +0000

    50c9bed feat(security): expose the safe value types.

--- src/platform_browser.d.ts
+++ src/platform_browser.d.ts
@@ -5,6 +5,6 @@ export { ELEMENT_PROBE_PROVIDERS } from './dom/debug/ng_probe';
 export { BROWSER_APP_COMMON_PROVIDERS, BROWSER_SANITIZATION_PROVIDERS, BROWSER_PROVIDERS, By, Title, enableDebugTools, disableDebugTools, HAMMER_GESTURE_CONFIG, HammerGestureConfig } from './browser_common';
 export * from '../private_export';
 export { DOCUMENT } from './dom/dom_tokens';
-export { DomSanitizationService, SecurityContext } from './security/dom_sanitization_service';
+export { DomSanitizationService, SafeHtml, SafeScript, SafeStyle, SafeUrl, SafeResourceUrl, SecurityContext } from './security/dom_sanitization_service';
 export { bootstrapStatic, browserStaticPlatform, BROWSER_APP_STATIC_PROVIDERS, BrowserPlatformLocation } from './platform_browser_static';
 export declare function browserPlatform(): PlatformRef;
