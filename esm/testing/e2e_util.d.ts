752d1db680ad1b6204eab9c4a09b31efe0d3eb70
GitHub <noreply@github.com>
2016-06-23 23:29:20 +0000

    894747c fix(platform-browser/testing-e2e): clean up unused exports from e2e testing helpers (#9387)

--- esm/testing/e2e_util.d.ts
+++ esm/testing/e2e_util.d.ts
@@ -1,4 +1,1 @@
-export declare var browser: protractor.IBrowser;
-export declare var $: cssSelectorHelper;
-export declare function clickAll(buttonSelectors: any): void;
 export declare function verifyNoBrowserErrors(): void;
