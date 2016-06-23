c92b2327ef47114590326090bc8626c4938c0019
GitHub <noreply@github.com>
2016-06-23 23:52:25 +0000

    3d8eb8c fix(platform-browser/testing): clean up public api for platform-browser/testing (#9519)

--- testing.d.ts
+++ testing.d.ts
@@ -5,6 +5,4 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-export * from './testing/browser_util';
-export * from './testing/matchers';
 export * from './testing/browser';
--- testing.js
+++ testing.js
@@ -9,7 +9,5 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-__export(require('./testing/browser_util'));
-__export(require('./testing/matchers'));
 __export(require('./testing/browser'));
 //# sourceMappingURL=testing.js.map
