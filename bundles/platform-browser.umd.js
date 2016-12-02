69f3edc08ed774ffda23de836bcbdca4a75f24c0
GitHub <noreply@github.com>
2016-12-02 01:15:56 +0000

    6c0b65b docs(changelog): add instructions for people that depend on the `cache` operator

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-614a35d
+ * @license Angular v2.3.0-rc.0-6c0b65b
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4657,7 +4657,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION$1 = new core.Version('2.3.0-rc.0-614a35d');
+  var /** @type {?} */ VERSION$1 = new core.Version('2.3.0-rc.0-6c0b65b');
 
   exports.VERSION = VERSION$1;
   exports.BrowserModule = BrowserModule;
