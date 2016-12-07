9937ce07bd49365659a8b517483def22383a6f66
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 22:07:54 +0000

    13b41bd chore(release): update version of tsc-wrapped to 0.4.2

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-cf52284
+ * @license Angular v2.3.0-13b41bd
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4646,7 +4646,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('2.3.0-rc.0-cf52284');
+  var /** @type {?} */ VERSION = new core.Version('2.3.0-13b41bd');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
