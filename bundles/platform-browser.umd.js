7a06a5dda3d5333fa96272a1f99aaa6fcbc07f3e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 18:19:47 +0000

    cf52284 docs(changelog): fix typo AplicationRef => ApplicationRef (#13284)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-4a09c81
+ * @license Angular v2.3.0-rc.0-cf52284
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4646,7 +4646,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('2.3.0-rc.0-4a09c81');
+  var /** @type {?} */ VERSION = new core.Version('2.3.0-rc.0-cf52284');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
