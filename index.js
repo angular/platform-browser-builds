cfa5c3f9cfd60155978ed78878c0506280347bde
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 00:32:03 +0000

    25c2141 fix(language-service): remove incompletely used parameter from `createLanguageServiceFromTypescript()` (#13278)

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-393c100');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-25c2141');
 export { BrowserModule, platformBrowser, Title, disableDebugTools, enableDebugTools, AnimationDriver, By, NgProbeToken, DOCUMENT, EVENT_MANAGER_PLUGINS, EventManager, HAMMER_GESTURE_CONFIG, HammerGestureConfig, DomSanitizer, __platform_browser_private__ } from './src/platform-browser';
 //# sourceMappingURL=index.js.map
