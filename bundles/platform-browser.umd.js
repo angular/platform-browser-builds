d6e33293f9ed006e2e62719dafc937d594f31b63
Victor Berchet <victor@suumit.com>
2016-12-09 23:52:29 +0000

    08ff2e5 fix(http): check response body text against undefined (#13017)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-90c2235
+ * @license Angular v2.3.0-08ff2e5
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4804,7 +4804,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('2.3.0-90c2235');
+  var /** @type {?} */ VERSION = new core.Version('2.3.0-08ff2e5');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
