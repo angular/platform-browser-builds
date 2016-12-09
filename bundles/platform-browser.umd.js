a4c830f31c2d59c46d8897de4cf0b3b0caa4e108
Victor Berchet <victor@suumit.com>
2016-12-09 18:56:59 +0000

    56c361f test(compiler): test i18n explicit id

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-72361fb
+ * @license Angular v2.3.0-56c361f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4804,7 +4804,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('2.3.0-72361fb');
+  var /** @type {?} */ VERSION = new core.Version('2.3.0-56c361f');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
