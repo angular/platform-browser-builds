f503400e045124ac9024f110a2b5315fd28c39a3
Victor Berchet <victor@suumit.com>
2016-12-12 19:29:55 +0000

    2e500cc fix(http): create a copy of headers when merge options (#13365)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-56dce0e
+ * @license Angular v2.3.0-2e500cc
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4808,7 +4808,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('2.3.0-56dce0e');
+  var /** @type {?} */ VERSION = new core.Version('2.3.0-2e500cc');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
