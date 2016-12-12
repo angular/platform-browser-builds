baf8c616844ed419a2803cdcbdab715bf08c626e
Victor Berchet <victor@suumit.com>
2016-12-12 19:30:29 +0000

    4c35be3 feat(forms): add novalidate by default (#13092)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-e9f307f
+ * @license Angular v2.3.0-4c35be3
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4808,7 +4808,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('2.3.0-e9f307f');
+  var /** @type {?} */ VERSION = new core.Version('2.3.0-4c35be3');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
