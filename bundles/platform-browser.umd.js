55d53aa9840c372cc4763c9aba5537af076729a2
Victor Berchet <victor@suumit.com>
2016-12-14 23:26:28 +0000

    c65b4fa refactor: format & lint

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-169ed82
+ * @license Angular v2.3.0-c65b4fa
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4812,7 +4812,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('2.3.0-169ed82');
+  var /** @type {?} */ VERSION = new core.Version('2.3.0-c65b4fa');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
