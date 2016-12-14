20aad7a86ed719b18815f56617820c304f1f4a54
Victor Berchet <victor@suumit.com>
2016-12-14 22:49:41 +0000

    aa40366 fix(compiler): fix simplify a reference without a name

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-aa3769b
+ * @license Angular v2.3.0-aa40366
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4809,7 +4809,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('2.3.0-aa3769b');
+  var /** @type {?} */ VERSION = new core.Version('2.3.0-aa40366');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
