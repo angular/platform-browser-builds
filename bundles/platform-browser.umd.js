edab695e919d1aad2908b0982de74506287fc4e5
Chuck Jazdzewski <chuckj@google.com>
2016-12-15 20:19:01 +0000

    14e785f fix(build): use bash string comparison operator (#13502)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-33910dd
+ * @license Angular v4.0.0-beta.0-14e785f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4812,7 +4812,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-33910dd');
+  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-14e785f');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
