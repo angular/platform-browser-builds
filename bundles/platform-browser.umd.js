ebd6bd98a58e2087e6086a6adee27e5b1683757b
GitHub <noreply@github.com>
2016-12-15 22:09:26 +0000

    5b6e8ea refactor(compiler): format update (#13506)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-732f446
+ * @license Angular v4.0.0-beta.0-5b6e8ea
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4812,7 +4812,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-732f446');
+  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-5b6e8ea');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
