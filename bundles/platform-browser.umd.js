c59502cda2820e47322b59dd8dc1e435b9b854d2
Chuck Jazdzewski <chuckj@google.com>
2016-12-16 23:46:04 +0000

    e785085 fix(compiler): do not lex `}}` when interpolation is disabled (#13531)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-a23fa94
+ * @license Angular v4.0.0-beta.0-e785085
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4816,7 +4816,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-a23fa94');
+  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-e785085');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
