b89e0d2d504b0a2791ceaeb80b2009a8c685163f
Chuck Jazdzewski <chuckj@google.com>
2016-12-16 23:36:59 +0000

    a23fa94 fix(common): capitalize first letter of all words in TitleCasePipe (#13511)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-4568d5d
+ * @license Angular v4.0.0-beta.0-a23fa94
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4816,7 +4816,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-4568d5d');
+  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-a23fa94');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
