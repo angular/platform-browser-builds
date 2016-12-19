e0fbf601e00759793ef9e7e7ec4a4ab1c1f6090f
GitHub <noreply@github.com>
2016-12-19 23:41:46 +0000

    e49c7fa refactor(compiler-cli): support extracting the mesage bundle without writing a file (#13580)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-6b65fc1
+ * @license Angular v4.0.0-beta.0-e49c7fa
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4816,7 +4816,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-6b65fc1');
+  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-e49c7fa');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
