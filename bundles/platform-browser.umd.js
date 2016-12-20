7d07397e6dcb95a6e2344a38add5a7f1670bbdc6
Chuck Jazdzewski <chuckj@google.com>
2016-12-20 00:40:07 +0000

    171a9bd feat: update to rxjs@5.0.1 and unpin the rxjs peerDeps via ^5.0.1 (#13572)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-e49c7fa
+ * @license Angular v4.0.0-beta.0-171a9bd
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4816,7 +4816,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-e49c7fa');
+  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.0-171a9bd');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
