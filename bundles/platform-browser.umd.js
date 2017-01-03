93304b05d2ccf33bbc7238eaf740ba51d190224f
Hans <hans@hansl.ca>
2017-01-03 18:17:42 +0000

    c5c53f3 fix(core): Remove reference to "Angular 2" in dev mode warning (#13751)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-bb0d23f
+ * @license Angular v4.0.0-beta.1-c5c53f3
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4819,7 +4819,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.1-bb0d23f');
+  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.1-c5c53f3');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
