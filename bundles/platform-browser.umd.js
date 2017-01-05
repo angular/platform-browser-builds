bd367b85019720d84bac95d4d21beb059721bd52
Igor Minar <iminar@google.com>
2017-01-05 17:38:55 +0000

    2dd6280 fix(common): do not override locale provided on bootstrap (#13654)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-35f9a1c
+ * @license Angular v4.0.0-beta.1-2dd6280
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4819,7 +4819,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.1-35f9a1c');
+  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.1-2dd6280');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
