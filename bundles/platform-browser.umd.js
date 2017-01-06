22ffdb87f5f446394bcae5bef8647b9d5b6c15a8
Igor Minar <igor@angularjs.org>
2017-01-06 21:15:19 +0000

    02dd90f docs(changelog): cherry-pick 2.4.2 release notes into the master branch

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.2-1c85e99
+ * @license Angular v4.0.0-beta.2-02dd90f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4814,7 +4814,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.2-1c85e99');
+  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.2-02dd90f');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
