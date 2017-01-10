272f7f757be4176e206a845cab2d0b4905fa63d0
Matias Niemelä <matias@yearofmoo.com>
2017-01-10 00:09:38 +0000

    61ba223 fix(router): throw an error when navigate to null/undefined path

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.2-6164eb2
+ * @license Angular v2.4.2-61ba223
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4656,7 +4656,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('2.4.2-6164eb2');
+  var /** @type {?} */ VERSION = new core.Version('2.4.2-61ba223');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
