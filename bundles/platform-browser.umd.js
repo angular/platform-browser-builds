39522d9456f2559c9921322292bf8d0f7f6b9024
Matias Niemelä <matias@yearofmoo.com>
2017-01-10 23:56:28 +0000

    aeed737 fix(compiler-cli): avoid handling functions in loadChildren as lazy load routes paths

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.2-d43e5dd
+ * @license Angular v4.0.0-beta.2-aeed737
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2411,6 +2411,152 @@
       return BrowserPlatformLocation;
   }(_angular_common.PlatformLocation));
 
+  /**
+   *  A service that can be used to get and add meta tags.
+    * *
+   */
+  var Meta = (function () {
+      /**
+       * @param {?} _dom
+       */
+      function Meta(_dom) {
+          this._dom = _dom;
+      }
+      /**
+       * @param {?} tag
+       * @param {?=} forceCreation
+       * @return {?}
+       */
+      Meta.prototype.addTag = function (tag, forceCreation) {
+          if (forceCreation === void 0) { forceCreation = false; }
+          if (!tag)
+              return null;
+          return this._getOrCreateElement(tag, forceCreation);
+      };
+      /**
+       * @param {?} tags
+       * @param {?=} forceCreation
+       * @return {?}
+       */
+      Meta.prototype.addTags = function (tags, forceCreation) {
+          var _this = this;
+          if (forceCreation === void 0) { forceCreation = false; }
+          if (!tags)
+              return [];
+          return tags.reduce(function (result, tag) {
+              if (tag) {
+                  result.push(_this._getOrCreateElement(tag, forceCreation));
+              }
+              return result;
+          }, []);
+      };
+      /**
+       * @param {?} attrSelector
+       * @return {?}
+       */
+      Meta.prototype.getTag = function (attrSelector) {
+          if (!attrSelector)
+              return null;
+          return this._dom.query("meta[" + attrSelector + "]");
+      };
+      /**
+       * @param {?} attrSelector
+       * @return {?}
+       */
+      Meta.prototype.getTags = function (attrSelector) {
+          if (!attrSelector)
+              return [];
+          var /** @type {?} */ list /*NodeList*/ = this._dom.querySelectorAll(this._dom.defaultDoc(), "meta[" + attrSelector + "]");
+          return list ? [].slice.call(list) : [];
+      };
+      /**
+       * @param {?} tag
+       * @param {?=} selector
+       * @return {?}
+       */
+      Meta.prototype.updateTag = function (tag, selector) {
+          if (!tag)
+              return null;
+          selector = selector || this._parseSelector(tag);
+          var /** @type {?} */ meta = this.getTag(selector);
+          if (meta) {
+              return this._setMetaElementAttributes(tag, meta);
+          }
+          return this._getOrCreateElement(tag, true);
+      };
+      /**
+       * @param {?} attrSelector
+       * @return {?}
+       */
+      Meta.prototype.removeTag = function (attrSelector) { this.removeTagElement(this.getTag(attrSelector)); };
+      /**
+       * @param {?} meta
+       * @return {?}
+       */
+      Meta.prototype.removeTagElement = function (meta) {
+          if (meta) {
+              this._dom.remove(meta);
+          }
+      };
+      /**
+       * @param {?} meta
+       * @param {?=} forceCreation
+       * @return {?}
+       */
+      Meta.prototype._getOrCreateElement = function (meta, forceCreation) {
+          if (forceCreation === void 0) { forceCreation = false; }
+          if (!forceCreation) {
+              var /** @type {?} */ selector = this._parseSelector(meta);
+              var /** @type {?} */ elem = this.getTag(selector);
+              // It's allowed to have multiple elements with the same name so it's not enough to
+              // just check that element with the same name already present on the page. We also need to
+              // check if element has tag attributes
+              if (elem && this._containsAttributes(meta, elem))
+                  return elem;
+          }
+          var /** @type {?} */ element = (this._dom.createElement('meta'));
+          this._setMetaElementAttributes(meta, element);
+          var /** @type {?} */ head = this._dom.getElementsByTagName(this._dom.defaultDoc(), 'head')[0];
+          this._dom.appendChild(head, element);
+          return element;
+      };
+      /**
+       * @param {?} tag
+       * @param {?} el
+       * @return {?}
+       */
+      Meta.prototype._setMetaElementAttributes = function (tag, el) {
+          var _this = this;
+          Object.keys(tag).forEach(function (prop) { return _this._dom.setAttribute(el, prop, tag[prop]); });
+          return el;
+      };
+      /**
+       * @param {?} tag
+       * @return {?}
+       */
+      Meta.prototype._parseSelector = function (tag) {
+          var /** @type {?} */ attr = tag.name ? 'name' : 'property';
+          return attr + "=\"" + tag[attr] + "\"";
+      };
+      /**
+       * @param {?} tag
+       * @param {?} elem
+       * @return {?}
+       */
+      Meta.prototype._containsAttributes = function (tag, elem) {
+          var _this = this;
+          return Object.keys(tag).every(function (key) { return _this._dom.getAttribute(elem, key) === tag[key]; });
+      };
+      Meta.decorators = [
+          { type: core.Injectable },
+      ];
+      /** @nocollapse */
+      Meta.ctorParameters = function () { return [
+          { type: DomAdapter, },
+      ]; };
+      return Meta;
+  }());
+
   var BrowserGetTestability = (function () {
       function BrowserGetTestability() {
       }
@@ -4404,6 +4550,12 @@
   /**
    * @return {?}
    */
+  function meta() {
+      return new Meta(getDOM());
+  }
+  /**
+   * @return {?}
+   */
   function _document() {
       return getDOM().defaultDoc();
   }
@@ -4432,7 +4584,8 @@
       BrowserModule.decorators = [
           { type: core.NgModule, args: [{
                       providers: [
-                          BROWSER_SANITIZATION_PROVIDERS, { provide: core.ErrorHandler, useFactory: errorHandler, deps: [] },
+                          BROWSER_SANITIZATION_PROVIDERS,
+                          { provide: core.ErrorHandler, useFactory: errorHandler, deps: [] },
                           { provide: DOCUMENT, useFactory: _document, deps: [] },
                           { provide: EVENT_MANAGER_PLUGINS, useClass: DomEventsPlugin, multi: true },
                           { provide: EVENT_MANAGER_PLUGINS, useClass: KeyEventsPlugin, multi: true },
@@ -4441,8 +4594,13 @@
                           { provide: DomRootRenderer, useClass: DomRootRenderer_ },
                           { provide: core.RootRenderer, useExisting: DomRootRenderer },
                           { provide: SharedStylesHost, useExisting: DomSharedStylesHost },
-                          { provide: AnimationDriver, useFactory: _resolveDefaultAnimationDriver }, DomSharedStylesHost,
-                          core.Testability, EventManager, ELEMENT_PROBE_PROVIDERS, Title
+                          { provide: AnimationDriver, useFactory: _resolveDefaultAnimationDriver },
+                          { provide: Meta, useFactory: meta },
+                          DomSharedStylesHost,
+                          core.Testability,
+                          EventManager,
+                          ELEMENT_PROBE_PROVIDERS,
+                          Title,
                       ],
                       exports: [_angular_common.CommonModule, core.ApplicationModule]
                   },] },
@@ -4656,10 +4814,11 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new core.Version('2.4.2-d43e5dd');
+  var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.2-aeed737');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
+  exports.Meta = Meta;
   exports.Title = Title;
   exports.disableDebugTools = disableDebugTools;
   exports.enableDebugTools = enableDebugTools;
