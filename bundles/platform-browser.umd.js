9b00c87ffabcb446cb5e5608be0ce7db4f7d9aa4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-19 20:21:42 +0000

    b049217 chore(docs): add missing comments (#14003)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.3-1200cf2
+ * @license Angular v4.0.0-beta.4-b049217
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.3-1200cf2');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-b049217');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
