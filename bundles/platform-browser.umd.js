b20521722995702638cd54360e55177327cf585c
Miško Hevery <misko@hevery.com>
2017-01-19 00:11:09 +0000

    635bf02 fix(router): enable loadChildren with function in aot (#13909)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.3-2d7b3a8
+ * @license Angular v4.0.0-beta.3-635bf02
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.3-2d7b3a8');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.3-635bf02');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
