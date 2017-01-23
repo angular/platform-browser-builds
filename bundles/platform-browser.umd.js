3ea3ac1a9e48aeb4e5325cc6cfe4271ab3a463ba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-23 19:41:10 +0000

    d3a3a8e fix(core): fix not declared variable in view engine (#14045)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-dff6ee3
+ * @license Angular v4.0.0-beta.4-d3a3a8e
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-dff6ee3');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-d3a3a8e');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
