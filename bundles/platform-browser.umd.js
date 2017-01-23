a5041aaa77a07016fc9e3ba9e55bda3120461636
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-23 21:38:13 +0000

    f20d1a8 ci: add @license to tools/validate-commit-message

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-e21e9c5
+ * @license Angular v4.0.0-beta.4-f20d1a8
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-e21e9c5');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-f20d1a8');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
