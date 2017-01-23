68c4095efd4bf4de4a566e6176722c6f788f6a2c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-23 19:08:46 +0000

    dff6ee3 ci: validate the message of each new commit as part of the CI linting

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-0589f93
+ * @license Angular v4.0.0-beta.4-dff6ee3
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-0589f93');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-dff6ee3');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
