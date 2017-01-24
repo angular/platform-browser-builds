26c0792d3e122d58a3a32a519848b60e885f0a25
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-24 18:28:47 +0000

    6541737 feat(core): add pure expression support to view engine

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-f20d1a8
+ * @license Angular v4.0.0-beta.4-6541737
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-f20d1a8');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-6541737');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
