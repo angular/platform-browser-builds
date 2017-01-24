4ceb55fa6d847eb9cf82e161229cc378a4dc7884
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-24 23:23:04 +0000

    6152eb2 fix(upgrade/static): ensure upgraded injector is initialized early enough (#14065)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-1c24271
+ * @license Angular v4.0.0-beta.4-6152eb2
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-1c24271');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-6152eb2');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
