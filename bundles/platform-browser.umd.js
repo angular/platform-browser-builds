de49bb3299efdb71c54a16d546a94aa33f86e47d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-25 21:34:41 +0000

    d2859cd style(compiler): run format

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-4931a61
+ * @license Angular v4.0.0-beta.4-d2859cd
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-4931a61');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.4-d2859cd');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
