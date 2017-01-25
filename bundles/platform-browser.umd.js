21340f5ac9fe192a2be61f29390a12c6f1b37227
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-25 23:48:16 +0000

    7ed39eb docs(changelog): add changelog for 2.4.5

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.4-84542d8
+ * @license Angular v2.4.5-7ed39eb
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4670,7 +4670,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('2.4.4-84542d8');
+    var /** @type {?} */ VERSION = new core.Version('2.4.5-7ed39eb');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
