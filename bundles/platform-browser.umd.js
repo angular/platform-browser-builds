61590596582b7dd0a1f8caad3bb5596465308f19
GitHub <noreply@github.com>
2017-01-26 01:58:14 +0000

    05b2b49 fix(compiler): allow empty translations for attributes (#14085)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-3ef73c2
+ * @license Angular v4.0.0-beta.5-05b2b49
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.5-3ef73c2');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.5-05b2b49');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
