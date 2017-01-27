523d7187d265fe92851d5766015f401f805001b2
Miško Hevery <misko@hevery.com>
2017-01-27 21:40:52 +0000

    c37af2a refactor(core): simplify ReflectiveInjector by removing code for Dart implementation (#14126)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-da41a95
+ * @license Angular v4.0.0-beta.5-c37af2a
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.5-da41a95');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.5-c37af2a');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
