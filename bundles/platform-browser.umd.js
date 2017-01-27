31a6b32e2e0e47896e634c0484b5966831f58bce
Igor Minar <iminar@google.com>
2017-01-27 08:37:38 +0000

    e130bc1 feat(aio): add initial angular-cli scaffold (#14118)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-b141a22
+ * @license Angular v4.0.0-beta.5-e130bc1
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.5-b141a22');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.5-e130bc1');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
