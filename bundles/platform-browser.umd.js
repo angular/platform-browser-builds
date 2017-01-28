cb1468fbba39efb9ece7d3df9957aeb50e7466bd
Igor Minar <iminar@google.com>
2017-01-28 02:40:54 +0000

    24ea3f0 test(compiler): don't print warnings from closure compiler (#14159)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-3368f29
+ * @license Angular v4.0.0-beta.5-24ea3f0
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4829,7 +4829,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.5-3368f29');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.5-24ea3f0');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
