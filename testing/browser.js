b070ddd89c92c847061231e9e3c79cee6e67eba4
Miško Hevery <misko@hevery.com>
2017-02-01 00:53:18 +0000

    8270bec fix: failing integration test

---
+++ testing/browser.js
@@ -0,0 +1,35 @@
+import { APP_ID, NgModule, NgZone, PLATFORM_INITIALIZER, createPlatformFactory, platformCore } from '@angular/core';
+import { AnimationDriver, BrowserModule } from '@angular/platform-browser';
+import { BrowserDetection, createNgZone } from './browser_util';
+import { BrowserDomAdapter, ELEMENT_PROBE_PROVIDERS } from './private_import_platform-browser';
+function initBrowserTests() {
+    BrowserDomAdapter.makeCurrent();
+    BrowserDetection.setup();
+}
+const _TEST_BROWSER_PLATFORM_PROVIDERS = [{ provide: PLATFORM_INITIALIZER, useValue: initBrowserTests, multi: true }];
+/**
+ * Platform for testing
+ *
+ * @stable
+ */
+export const platformBrowserTesting = createPlatformFactory(platformCore, 'browserTesting', _TEST_BROWSER_PLATFORM_PROVIDERS);
+/**
+ * NgModule for testing.
+ *
+ * @stable
+ */
+export class BrowserTestingModule {
+}
+BrowserTestingModule.decorators = [
+    { type: NgModule, args: [{
+                exports: [BrowserModule],
+                providers: [
+                    { provide: APP_ID, useValue: 'a' }, ELEMENT_PROBE_PROVIDERS,
+                    { provide: NgZone, useFactory: createNgZone },
+                    { provide: AnimationDriver, useValue: AnimationDriver.NOOP }
+                ]
+            },] },
+];
+/** @nocollapse */
+BrowserTestingModule.ctorParameters = () => [];
+//# sourceMappingURL=browser.js.map
