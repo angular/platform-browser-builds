5929d64c8551901fe99da6a8aab7bc9d603a2255
Igor Minar <iminar@google.com>
2017-02-07 21:16:13 +0000

    2e14130 feat(aio): refactor DocViewer w/ services component builder (#14294)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-a378aab
+ * @license Angular v4.0.0-beta.6-2e14130
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4832,7 +4832,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.6-a378aab');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.6-2e14130');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
