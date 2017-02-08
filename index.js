8ef4a332f813be5072abb916bdd0e4848560e19c
Miško Hevery <misko@hevery.com>
2017-02-08 01:45:36 +0000

    c33fda2 perf: Don’t subclass Error; resulting in smaller binary (#14160)

--- index.js
+++ index.js
@@ -11,4 +11,5 @@
  * Entry point for all public APIs of the platform-browser package.
  */
 export { BrowserModule, platformBrowser, Meta, Title, disableDebugTools, enableDebugTools, AnimationDriver, By, NgProbeToken, DOCUMENT, EVENT_MANAGER_PLUGINS, EventManager, HAMMER_GESTURE_CONFIG, HammerGestureConfig, DomSanitizer, VERSION, __platform_browser_private__ } from './src/platform-browser';
+// This file only reexports content of the `src` folder. Keep it that way.
 //# sourceMappingURL=index.js.map
