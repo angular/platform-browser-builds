60cbe82a53d9ccfa2f8dbd0af0223515f306feaf
Miško Hevery <misko@hevery.com>
2017-02-09 21:55:23 +0000

    45cc444 fix(core): Remove ChangeDetectorRef Paramter from KeyValueDifferFactory and IterableDifferFactory (#14311)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-56e2f84
+ * @license Angular v4.0.0-beta.7-45cc444
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4838,7 +4838,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-56e2f84');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-45cc444');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
