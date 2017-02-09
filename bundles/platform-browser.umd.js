4b8152952f64ef576ffc42414b0f6209fcc1656c
Miško Hevery <misko@hevery.com>
2017-02-09 18:22:30 +0000

    09b4bd0 docs(changelog): add changelog for 4.0.0-beta.7

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-d1feb47
+ * @license Angular v4.0.0-beta.7-09b4bd0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4838,7 +4838,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.6-d1feb47');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-09b4bd0');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
