9a05d3e2719d914f80b2b92105e30a9b7dc19ce4
Igor Minar <iminar@google.com>
2017-02-10 01:41:50 +0000

    b988733 test(aio): remove unnecessary hack from e2e tests (#14356)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-44bb337
+ * @license Angular v4.0.0-beta.7-b988733
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4838,7 +4838,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-44bb337');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-b988733');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
