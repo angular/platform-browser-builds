9b338b7efb6db661cca52b9893e5cef44f6bdc65
Miško Hevery <misko@hevery.com>
2017-02-10 21:09:46 +0000

    7c87c52 fix(upgrade): pass correct values to `ngOnChanges` for interpolation bindings (#14400)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.7-541de26
+ * @license Angular v2.4.7-7c87c52
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4673,7 +4673,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('2.4.7-541de26');
+    var /** @type {?} */ VERSION = new core.Version('2.4.7-7c87c52');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
