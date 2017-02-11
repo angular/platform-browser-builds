33dafedea8ce62b2e0062d7ab40feb932a715b80
GitHub <noreply@github.com>
2017-02-11 06:37:39 +0000

    2ca93e1 doc: update Node.js and npm version requirements

---
+++ src/browser/meta.js
@@ -0,0 +1,160 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Injectable } from '@angular/core/index';
+import { DomAdapter } from '../dom/dom_adapter';
+/**
+ * A service that can be used to get and add meta tags.
+ *
+ * \@experimental
+ */
+export class Meta {
+    /**
+     * @param {?} _dom
+     */
+    constructor(_dom) {
+        this._dom = _dom;
+    }
+    /**
+     * @param {?} tag
+     * @param {?=} forceCreation
+     * @return {?}
+     */
+    addTag(tag, forceCreation = false) {
+        if (!tag)
+            return null;
+        return this._getOrCreateElement(tag, forceCreation);
+    }
+    /**
+     * @param {?} tags
+     * @param {?=} forceCreation
+     * @return {?}
+     */
+    addTags(tags, forceCreation = false) {
+        if (!tags)
+            return [];
+        return tags.reduce((result, tag) => {
+            if (tag) {
+                result.push(this._getOrCreateElement(tag, forceCreation));
+            }
+            return result;
+        }, []);
+    }
+    /**
+     * @param {?} attrSelector
+     * @return {?}
+     */
+    getTag(attrSelector) {
+        if (!attrSelector)
+            return null;
+        return this._dom.query(`meta[${attrSelector}]`);
+    }
+    /**
+     * @param {?} attrSelector
+     * @return {?}
+     */
+    getTags(attrSelector) {
+        if (!attrSelector)
+            return [];
+        const /** @type {?} */ list /*NodeList*/ = this._dom.querySelectorAll(this._dom.defaultDoc(), `meta[${attrSelector}]`);
+        return list ? [].slice.call(list) : [];
+    }
+    /**
+     * @param {?} tag
+     * @param {?=} selector
+     * @return {?}
+     */
+    updateTag(tag, selector) {
+        if (!tag)
+            return null;
+        selector = selector || this._parseSelector(tag);
+        const /** @type {?} */ meta = this.getTag(selector);
+        if (meta) {
+            return this._setMetaElementAttributes(tag, meta);
+        }
+        return this._getOrCreateElement(tag, true);
+    }
+    /**
+     * @param {?} attrSelector
+     * @return {?}
+     */
+    removeTag(attrSelector) { this.removeTagElement(this.getTag(attrSelector)); }
+    /**
+     * @param {?} meta
+     * @return {?}
+     */
+    removeTagElement(meta) {
+        if (meta) {
+            this._dom.remove(meta);
+        }
+    }
+    /**
+     * @param {?} meta
+     * @param {?=} forceCreation
+     * @return {?}
+     */
+    _getOrCreateElement(meta, forceCreation = false) {
+        if (!forceCreation) {
+            const /** @type {?} */ selector = this._parseSelector(meta);
+            const /** @type {?} */ elem = this.getTag(selector);
+            // It's allowed to have multiple elements with the same name so it's not enough to
+            // just check that element with the same name already present on the page. We also need to
+            // check if element has tag attributes
+            if (elem && this._containsAttributes(meta, elem))
+                return elem;
+        }
+        const /** @type {?} */ element = (this._dom.createElement('meta'));
+        this._setMetaElementAttributes(meta, element);
+        const /** @type {?} */ head = this._dom.getElementsByTagName(this._dom.defaultDoc(), 'head')[0];
+        this._dom.appendChild(head, element);
+        return element;
+    }
+    /**
+     * @param {?} tag
+     * @param {?} el
+     * @return {?}
+     */
+    _setMetaElementAttributes(tag, el) {
+        Object.keys(tag).forEach((prop) => this._dom.setAttribute(el, prop, tag[prop]));
+        return el;
+    }
+    /**
+     * @param {?} tag
+     * @return {?}
+     */
+    _parseSelector(tag) {
+        const /** @type {?} */ attr = tag.name ? 'name' : 'property';
+        return `${attr}="${tag[attr]}"`;
+    }
+    /**
+     * @param {?} tag
+     * @param {?} elem
+     * @return {?}
+     */
+    _containsAttributes(tag, elem) {
+        return Object.keys(tag).every((key) => this._dom.getAttribute(elem, key) === tag[key]);
+    }
+}
+Meta.decorators = [
+    { type: Injectable },
+];
+/** @nocollapse */
+Meta.ctorParameters = () => [
+    { type: DomAdapter, },
+];
+function Meta_tsickle_Closure_declarations() {
+    /** @type {?} */
+    Meta.decorators;
+    /**
+     * @nocollapse
+     * @type {?}
+     */
+    Meta.ctorParameters;
+    /** @type {?} */
+    Meta.prototype._dom;
+}
+//# sourceMappingURL=meta.js.map
