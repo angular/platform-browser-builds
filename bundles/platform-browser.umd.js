9e9da8f07561429700db78f6b939fe9db7dfd5f7
Miško Hevery <misko@hevery.com>
2017-02-11 04:49:37 +0000

    8dd16bb test: add missing change to test-main.js

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-5279d06
+ * @license Angular v4.0.0-beta.7-8dd16bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4873,7 +4873,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-5279d06');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-8dd16bb');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
