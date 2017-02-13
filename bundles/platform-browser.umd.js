bb427b80fe5d6cdb30d2f1ce7ce0424de1c76a07
Miško Hevery <misko@hevery.com>
2017-02-13 23:08:19 +0000

    221b7a1 build: use `ts-api-guardian` v0.2.1 (#14408)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-d3f174a
+ * @license Angular v4.0.0-beta.7-221b7a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4873,7 +4873,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-d3f174a');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-221b7a1');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
