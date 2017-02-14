a264b0104c427b547c391ae4f27b95c600d41844
Igor Minar <iminar@google.com>
2017-02-14 22:36:57 +0000

    269cf42 build: add the `tslint` gulp task (#14481)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-e4e9dbe
+ * @license Angular v4.0.0-beta.7-269cf42
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4873,7 +4873,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-e4e9dbe');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-269cf42');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
