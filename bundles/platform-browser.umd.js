f06f6110c4a7c4866172d0d2cdfb31b73281ae61
GitHub <noreply@github.com>
2017-02-15 06:01:51 +0000

    e9ba7aa docs: update schedule by adding one more beta (#14494)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-bb9c7ae
+ * @license Angular v4.0.0-beta.7-e9ba7aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5139,7 +5139,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-bb9c7ae');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-e9ba7aa');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
