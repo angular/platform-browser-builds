9fb2818537c96756b5edf2145ef9aa79c2df6866
Igor Minar <iminar@google.com>
2017-02-16 20:31:05 +0000

    ba17dcb refactor(animations): multiple group animations should tally-up delays correctly

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-3dbd9a0
+ * @license Angular v4.0.0-beta.7-ba17dcb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5143,7 +5143,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-3dbd9a0');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-ba17dcb');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
