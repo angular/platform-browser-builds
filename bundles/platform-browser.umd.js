fba70d06bdf8be7d698d68a60c7bd1161dee7abe
Igor Minar <igor@angularjs.org>
2017-02-18 22:44:52 +0000

    0cf753b docs: add changelog for v2.4.8

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-45eac23
+ * @license Angular v4.0.0-beta.7-0cf753b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5200,7 +5200,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-45eac23');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.7-0cf753b');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
