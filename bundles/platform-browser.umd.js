2ada1a918b8b7f822cfbdbc5e48b5f469f2f35b1
GitHub <noreply@github.com>
2017-02-21 02:14:33 +0000

    fbe4b76 fix(http): Make ResponseOptionsArgs an interface (#14607)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-8742432
+ * @license Angular v4.0.0-beta.8-fbe4b76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5201,7 +5201,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.8-8742432');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-beta.8-fbe4b76');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
