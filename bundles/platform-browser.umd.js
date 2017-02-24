9749313f5542232fde45968e18860a8032746fad
Igor Minar <iminar@google.com>
2017-02-24 01:44:25 +0000

    a23634d fix(common): do not reference deprecated classes in providers (#14523) (#14523)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -4784,7 +4784,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-c53621b');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-a23634d');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
