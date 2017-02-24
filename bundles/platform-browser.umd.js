4620fa709c5e112fa24d957116bc8520a2ecab09
Igor Minar <iminar@google.com>
2017-02-24 04:24:58 +0000

    e99d721 feat(compiler): Add a `enableLegacyTemplate` option to support `<template>`

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -4784,7 +4784,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-ee747f7');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-e99d721');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
