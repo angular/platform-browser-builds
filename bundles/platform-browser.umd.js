34aae11962b942abad326526a45c8ef0c6a0b946
Igor Minar <iminar@google.com>
2017-02-24 05:36:22 +0000

    01907ba build(platform-server): update parse5 to v3 (#14538)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -4784,7 +4784,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-39f56fa');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-01907ba');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
