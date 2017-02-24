089fe2416a3603576143b720ab9b3db89377e9e8
Igor Minar <iminar@google.com>
2017-02-24 20:13:06 +0000

    5094aef fix(compiler-cli): fix flat module import on Windows (#14712)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -4784,7 +4784,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-93ddd38');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-5094aef');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
