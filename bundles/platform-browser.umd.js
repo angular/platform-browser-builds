cb35f246a93563077e314a3503a268ccb4c085e2
GitHub <noreply@github.com>
2017-02-25 02:44:10 +0000

    f32f4de docs: more changelog fixes

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -4785,7 +4785,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-26ed262');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-f32f4de');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
