ffb6a4b1587d60f4b1d5a195c375df329d34c810
GitHub <noreply@github.com>
2017-02-26 04:39:28 +0000

    213e210 docs: changelog - add anchor to the FESM section

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -4785,7 +4785,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-c868457');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-213e210');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
