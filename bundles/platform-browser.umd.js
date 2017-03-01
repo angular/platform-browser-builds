7102c3a348322d02745b53f8c0d8f65ef7e3ace7
Igor Minar <iminar@google.com>
2017-03-01 15:12:09 +0000

    968995a fix(upgrade): fix upgrade component Closure optimization. (#14801)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -4009,7 +4009,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-5ba55b0');
+  var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-968995a');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
