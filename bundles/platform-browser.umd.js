15a5cf2dfcdf4fcd39fe4ab27dbe3b05874643b3
Igor Minar <iminar@google.com>
2017-03-01 20:37:34 +0000

    79fc1e3 fix(compiler): apply element bindings before host bindings (#14823)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -4009,7 +4009,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-9402df9');
+  var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-79fc1e3');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
