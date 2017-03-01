5478eba1147e67213ace48eb859dcf6b1f0e56a7
Igor Minar <iminar@google.com>
2017-03-01 23:48:29 +0000

    dd49901 ci: add NGBUILDS_IO_KEY to .travis.yaml for ngbuilds.io integration

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -4009,7 +4009,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-a6996a9');
+  var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-dd49901');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
