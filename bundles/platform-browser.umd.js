2144473981544056507ea2c1fa73827351809b2b
Igor Minar <iminar@google.com>
2017-03-02 08:41:46 +0000

    207298c build(aio): generate marketing docs JSON files

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -4009,7 +4009,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-a24e652');
+  var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-207298c');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
