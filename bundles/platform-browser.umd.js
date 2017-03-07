3a4f0020a1af6a9b4d3265cebb05cd2ba81f4d63
Chuck Jazdzewski <chuckj@google.com>
2017-03-07 17:54:02 +0000

    07122f0 fix(upgrade): populate upgraded component's view before creating the controller (#14289)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -3884,7 +3884,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-ebd4463');
+  var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-07122f0');
 
   exports.BrowserModule = BrowserModule;
   exports.platformBrowser = platformBrowser;
