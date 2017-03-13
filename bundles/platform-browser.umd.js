0ab7ba3dcc279b919b0dba483029f4a5d423fdef
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:55:03 +0000

    75147ff fix(aio): remove invalid brace from CSS (#15122)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-018e5c9
+ * @license Angular v4.0.0-rc.3-75147ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4162,7 +4162,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-018e5c9');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-75147ff');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
