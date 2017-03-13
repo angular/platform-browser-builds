e1666119566a5739a17e1194bd32078a1db6ed20
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:40:19 +0000

    018e5c9 fix(platform-server): fix get/set title in parse5 adapter (#14965)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-e7dab7e
+ * @license Angular v4.0.0-rc.3-018e5c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4162,7 +4162,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-e7dab7e');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-018e5c9');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
