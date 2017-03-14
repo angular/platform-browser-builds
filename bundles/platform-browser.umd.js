456d518f398feb38d8f4fbaf53630312ae9eaf48
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 01:26:12 +0000

    1c1085b feat(aio): add google analytics (#15081)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-eedca09
+ * @license Angular v4.0.0-rc.3-1c1085b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4162,7 +4162,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-eedca09');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-1c1085b');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
