f3dc6c4de3c57cfa62c4263c2617b06a077d4ee6
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 23:01:23 +0000

    f093501 fix(platform-server): support svg elements with namespaced attributes (#15101)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-80649ea
+ * @license Angular v4.0.0-rc.3-f093501
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4162,7 +4162,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-80649ea');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-f093501');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
