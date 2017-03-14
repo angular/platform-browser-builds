eaa2701ad60d6aebaf8f2ca843911f4459cf6ff0
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 01:03:22 +0000

    eedca09 build(aio): generate the api-list.json file from the API docs

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ab40fcb
+ * @license Angular v4.0.0-rc.3-eedca09
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4162,7 +4162,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-ab40fcb');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-eedca09');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
