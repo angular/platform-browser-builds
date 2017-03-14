708154ede1baf01ca86f030a4600c797205eb475
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 23:44:14 +0000

    13686bb fix: element injector vs module injector (#15044)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-f093501
+ * @license Angular v4.0.0-rc.3-13686bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4162,7 +4162,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-f093501');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-13686bb');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
