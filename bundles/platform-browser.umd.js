19e4c8790522f298732cfc788bd27103cd1a0282
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 00:20:06 +0000

    ab40fcb docs(aio): fix relative links

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6497633
+ * @license Angular v4.0.0-rc.3-ab40fcb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4162,7 +4162,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-6497633');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-ab40fcb');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
