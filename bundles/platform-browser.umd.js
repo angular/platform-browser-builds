ad5eb801c51ed28ca45fccdb553fed356051db40
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 21:52:00 +0000

    2c5a671 fix: don't instantiate providers with ngOnDestroy eagerly. (#15070)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-0aad270
+ * @license Angular v4.0.0-rc.3-2c5a671
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4162,7 +4162,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-0aad270');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-2c5a671');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
