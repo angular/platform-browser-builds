f3a46846cd3405a5308ed4024e6701643d20f78c
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:30:41 +0000

    3c15916 fix(compiler): Improve error message for missing annotations (#14724)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-959a03a
+ * @license Angular v4.0.0-rc.3-3c15916
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4162,7 +4162,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-959a03a');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-3c15916');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
