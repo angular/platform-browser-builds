9917a68d070121a7856471ba8ca3626ee0376867
Igor Minar <igor@angularjs.org>
2017-03-15 04:25:54 +0000

    ec548ad feat(aio): hide search results when click outside search elements

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-423bfb0
+ * @license Angular v4.0.0-rc.3-ec548ad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4162,7 +4162,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-423bfb0');
+    var /** @type {?} */ VERSION = new core.Version('4.0.0-rc.3-ec548ad');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
