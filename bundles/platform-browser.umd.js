bf32acfadf0e281387ee45bb7bd42cc1d6d327ea
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 23:26:01 +0000

    b0ae464 ci: changing deployment token

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.9-80fe41a
+ * @license Angular v2.4.9-b0ae464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4672,7 +4672,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new core.Version('2.4.9-80fe41a');
+    var /** @type {?} */ VERSION = new core.Version('2.4.9-b0ae464');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
