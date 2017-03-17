ae17a3e7c25f66381b17d1638773697b35288a9b
Chuck Jazdzewski <chuckj@google.com>
2017-03-17 17:48:45 +0000

    2d78c8c docs: add changelog for 4.0.0-rc.5

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.4-7fb4528
+ * @license Angular v4.0.0-rc.5-2d78c8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4222,7 +4222,7 @@
     /**
      * \@stable
      */
-    var VERSION = new core.Version('4.0.0-rc.4-7fb4528');
+    var VERSION = new core.Version('4.0.0-rc.5-2d78c8c');
 
     exports.BrowserModule = BrowserModule;
     exports.platformBrowser = platformBrowser;
