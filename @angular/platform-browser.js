414cda6827f99dce1aa55a4e27c99193c983b6e6
Miško Hevery <misko@hevery.com>
2017-03-20 18:02:41 +0000

    5da021c feat(router): add `ParamMap.keys` to get a list of parameters

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-f634c62
+ * @license Angular v4.0.0-rc.5-5da021c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4285,7 +4285,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-f634c62');
+const VERSION = new Version('4.0.0-rc.5-5da021c');
 
 /**
  * @license
