3d7cb503b70be513032586fc1df550bd4f4d5281
Igor Minar <iminar@google.com>
2017-03-22 22:41:18 +0000

    ea49a95 fix(upgrade): component injectors should not link the module injector tree (#15385)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-a50d79d
+ * @license Angular v4.0.0-rc.5-ea49a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4285,7 +4285,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-a50d79d');
+const VERSION = new Version('4.0.0-rc.5-ea49a95');
 
 /**
  * @license
