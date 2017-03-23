e625872ab29ea61ca09728140a864cd2580768ff
Igor Minar <iminar@google.com>
2017-03-23 00:36:54 +0000

    98cb974 docs(router): fix typo in ParamMap api doc (#15397)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-9319b5f
+ * @license Angular v4.0.0-rc.5-98cb974
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4285,7 +4285,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-9319b5f');
+const VERSION = new Version('4.0.0-rc.5-98cb974');
 
 /**
  * @license
