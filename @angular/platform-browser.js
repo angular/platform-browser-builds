673ce678e02f471affc3f284588a5e092d8cce97
GitHub <noreply@github.com>
2017-03-24 01:01:40 +0000

    c65b754 docs: add the 4.0.0 code name to the changelog

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-db0dca3
+ * @license Angular v4.0.0-c65b754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-db0dca3');
+const VERSION = new Version('4.0.0-c65b754');
 
 /**
  * @license
