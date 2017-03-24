8d1893ccbfc9ea5b5f7f45d9a039e19e693a67c8
Victor Berchet <victor@suumit.com>
2017-03-24 21:55:09 +0000

    8785b2b fix(router): shouldn't execute CanLoad when a route has been loaded

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-ea848f7
+ * @license Angular v4.0.0-8785b2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-ea848f7');
+const VERSION = new Version('4.0.0-8785b2b');
 
 /**
  * @license
