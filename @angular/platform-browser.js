ff83b30c200f3e8c490eca5b2d81a8539870ad4e
Victor Berchet <victor@suumit.com>
2017-03-28 20:50:24 +0000

    d438b88 fix(compiler): ignore errors when evaluating base classes (#15560)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-6269d28
+ * @license Angular v4.0.0-d438b88
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4307,7 +4307,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-6269d28');
+const VERSION = new Version('4.0.0-d438b88');
 
 /**
  * @license
