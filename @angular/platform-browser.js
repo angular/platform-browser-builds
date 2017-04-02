cabd628d6609cdbc9ebd4dc26892118a5e159cee
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-02 10:56:05 +0000

    34a1990 fix(aio): trim code blocks before rendering

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-e4c3882
+ * @license Angular v4.1.0-beta.0-34a1990
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4307,7 +4307,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-e4c3882');
+const VERSION = new Version('4.1.0-beta.0-34a1990');
 
 /**
  * @license
