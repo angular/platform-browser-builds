77773fe3b1f59e21c83b325c8abcbf6689c1a1bb
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 16:21:46 +0000

    cb5a7ef fix(core): key-value differ changes iteration (#15968)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-8ad464d
+ * @license Angular v4.1.0-beta.1-cb5a7ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4307,7 +4307,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-8ad464d');
+const VERSION = new Version('4.1.0-beta.1-cb5a7ef');
 
 /**
  * @license
