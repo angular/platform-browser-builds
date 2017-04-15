e8c22f0b42d4f7c292f9d42a86c2294a06377015
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:52:41 +0000

    a2bdcc9 feat(aio): callout, code ex header styles

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-16673fa
+ * @license Angular v4.1.0-beta.1-a2bdcc9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4307,7 +4307,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-16673fa');
+const VERSION = new Version('4.1.0-beta.1-a2bdcc9');
 
 /**
  * @license
