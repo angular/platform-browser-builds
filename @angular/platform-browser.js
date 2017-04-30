fb6d25bd934244277607ffc7b64ee607eabbbd67
Miško Hevery <misko@hevery.com>
2017-04-30 16:30:45 +0000

    de8de84 test(aio): confirm ga tracks user’s # fragment clicks within a page (#16441)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-8d300ff
+ * @license Angular v4.1.0-de8de84
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4308,7 +4308,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-8d300ff');
+const VERSION = new Version('4.1.0-de8de84');
 
 /**
  * @license
