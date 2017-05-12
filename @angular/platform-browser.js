0a6b9a0bb399d1235d0b0759e8db6649c19a69e7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-12 13:37:05 +0000

    90bc5b2 fix(aio): only register ServiceWorker in production mode

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-37f1fcb
+ * @license Angular v4.2.0-beta.1-90bc5b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4316,7 +4316,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-37f1fcb');
+const VERSION = new Version('4.2.0-beta.1-90bc5b2');
 
 /**
  * @license
