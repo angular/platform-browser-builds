d1672b0630dbdb13d73ce1142efbab69bdce411e
Chuck Jazdzewski <chuckj@google.com>
2017-05-22 18:39:21 +0000

    85d4c4b feat(core): update zone.js to 0.8.10 and expose the flush method (#16860)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-6e41add
+ * @license Angular v4.2.0-rc.0-85d4c4b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-6e41add
+ * @license Angular v4.2.0-rc.0-85d4c4b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4448,7 +4448,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.0-6e41add');
+var VERSION = new _angular_core.Version('4.2.0-rc.0-85d4c4b');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
