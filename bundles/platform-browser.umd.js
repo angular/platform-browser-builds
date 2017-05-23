ae10503cd83459d7b421fa92fa43421783220d89
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 00:14:40 +0000

    e7d9fd8 fix(animations): repair flicker issues with WA polyfill (#16937)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-08dfe91
+ * @license Angular v4.2.0-rc.0-e7d9fd8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-08dfe91
+ * @license Angular v4.2.0-rc.0-e7d9fd8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4448,7 +4448,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.0-08dfe91');
+var VERSION = new _angular_core.Version('4.2.0-rc.0-e7d9fd8');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
