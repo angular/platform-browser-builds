58de65b6aee9dee8791497c31089cd32fdaf9efc
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 17:15:50 +0000

    5af143e refactor(compiler): allow sync AOT compilation (#16832).

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-3b28c75
+ * @license Angular v4.2.0-rc.0-5af143e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4318,7 +4318,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-3b28c75');
+const VERSION = new Version('4.2.0-rc.0-5af143e');
 
 /**
  * @license
