8196b24ebbe90d4ed99e8fe83e88b795d39424b0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-04 14:31:26 +0000

    f2d810f fix(aio): fix ToC styling

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-819514a
+ * @license Angular v4.2.0-rc.2-f2d810f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4318,7 +4318,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-819514a');
+const VERSION = new Version('4.2.0-rc.2-f2d810f');
 
 /**
  * @license
