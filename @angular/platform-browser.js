848c54620247c9f763a76eb4ea7e69b1955f4d59
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-09 23:16:13 +0000

    90b0713 refactor(compiler): don’t write summaries for jit by default

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-d56b7ed
+ * @license Angular v4.2.0-90b0713
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-d56b7ed');
+const VERSION = new Version('4.2.0-90b0713');
 
 /**
  * @license
