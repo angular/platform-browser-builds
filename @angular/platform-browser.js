bf4b67367071edb3e73ca98e2ae914285e429076
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 23:18:47 +0000

    234268e ci(aio): deploy commits on the stable branch to production

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-7a2d2ef
+ * @license Angular v4.2.1-234268e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-7a2d2ef');
+const VERSION = new Version('4.2.1-234268e');
 
 /**
  * @license
