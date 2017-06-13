18f22c1f718b3634f191de6af9968168091f749e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 21:08:16 +0000

    93d1b4e refactor(compiler): remove duplicate code

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-fa81c8e
+ * @license Angular v4.2.2-93d1b4e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-fa81c8e
+ * @license Angular v4.2.2-93d1b4e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-fa81c8e');
+var VERSION = new _angular_core.Version('4.2.2-93d1b4e');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
