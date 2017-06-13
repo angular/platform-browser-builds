d7a03111de8cc264c47489552930c2d268901410
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 22:57:35 +0000

    11dfb68 ci: update github templates (#17466)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4164369
+ * @license Angular v4.2.2-11dfb68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-4164369
+ * @license Angular v4.2.2-11dfb68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-4164369');
+var VERSION = new _angular_core.Version('4.2.2-11dfb68');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
