440115c1c3f1953bc5a867c7a7ecb85edda6ebd0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-14 15:04:08 +0000

    2bdf2fe test(aio): add async beforeEach to prevent Chrome disconnects

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-607fb1f
+ * @license Angular v4.2.2-2bdf2fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-607fb1f');
+const VERSION = new Version('4.2.2-2bdf2fe');
 
 /**
  * @license
