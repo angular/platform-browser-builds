de74497a1bc98ada85ba67868cf797c798de329e
Hans <hans@hansl.ca>
2017-06-15 22:11:16 +0000

    e096a85 fix(animations): remove duplicate license header

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-53f57d7
+ * @license Angular v4.2.2-e096a85
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-53f57d7
+ * @license Angular v4.2.2-e096a85
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-53f57d7');
+var VERSION = new _angular_core.Version('4.2.2-e096a85');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
