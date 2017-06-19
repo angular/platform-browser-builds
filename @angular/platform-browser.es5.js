25773589f2f9dd1a97d99274dff0847ede0c710d
Hans Larsen <hans@hansl.ca>
2017-06-19 22:13:02 +0000

    09512c7 docs(RELEASE_SCHEDULE): fix version numbers for August/September releases

--- @angular/platform-browser.es5.js
+++ @angular/platform-browser.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.3-3607a48
+ * @license Angular v4.2.3-09512c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4424,7 +4424,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.3-3607a48');
+var VERSION = new Version('4.2.3-09512c7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
