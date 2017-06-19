b114fb5a2d23caec664653c516f2be97e265d398
Hans <hans@hansl.ca>
2017-06-19 22:29:32 +0000

    8f5836c docs(TRIAGE_AND_LABELS): update labels to reflect the current state

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-319ce18
+ * @license Angular v4.2.2-8f5836c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-319ce18
+ * @license Angular v4.2.2-8f5836c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-319ce18');
+var VERSION = new _angular_core.Version('4.2.2-8f5836c');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
