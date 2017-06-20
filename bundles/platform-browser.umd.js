aab1e76c261674e9832d8aa19dda8388d4c59c7b
Hans <hans@hansl.ca>
2017-06-20 20:29:56 +0000

    00874c2 docs(aio): update resource listing

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-009651e
+ * @license Angular v4.2.2-00874c2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-009651e
+ * @license Angular v4.2.2-00874c2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-009651e');
+var VERSION = new _angular_core.Version('4.2.2-00874c2');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
