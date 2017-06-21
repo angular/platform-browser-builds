5384aa78c41bfbaf0cf0b6f0bd19f3cc3c9fdf2b
Hans Larsen <hans@hansl.ca>
2017-06-21 23:59:16 +0000

    74954f1 docs(aio): ward's changes

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-c112232
+ * @license Angular v4.2.3-74954f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-c112232');
+const VERSION = new Version('4.2.3-74954f1');
 
 /**
  * @license
