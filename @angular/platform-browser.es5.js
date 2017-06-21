17d9e1e4ee5849099dfb7aa3c01d3779755ab893
Hans <hans@hansl.ca>
2017-06-21 23:52:10 +0000

    37cdc4f docs(aio): ward's changes

--- @angular/platform-browser.es5.js
+++ @angular/platform-browser.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-4352dd2
+ * @license Angular v4.2.2-37cdc4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4424,7 +4424,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-4352dd2');
+var VERSION = new Version('4.2.2-37cdc4f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
