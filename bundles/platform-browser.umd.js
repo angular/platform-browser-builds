d0650bad98e956d51f7b6ca7c8aa2a659d536319
Hans <hans@hansl.ca>
2017-06-21 21:23:04 +0000

    bb804dd feat(aio): select contributor group with URL “about?group=gde”

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-1d93cf2
+ * @license Angular v4.2.2-bb804dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-1d93cf2
+ * @license Angular v4.2.2-bb804dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-1d93cf2');
+var VERSION = new _angular_core.Version('4.2.2-bb804dd');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
