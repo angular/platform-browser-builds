f6dda147ef3825e3adfefe66906556d3d834ac7d
Hans Larsen <hans@hansl.ca>
2017-06-22 00:17:32 +0000

    d203136 revert: feat(core): update zone.js to 0.8.12

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-74954f1
+ * @license Angular v4.2.3-d203136
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-74954f1');
+const VERSION = new Version('4.2.3-d203136');
 
 /**
  * @license
