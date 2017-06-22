46cafc1fc85409774b8f32c2ded066d40ca2fd9d
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:03:57 +0000

    5c576d3 docs: add documentation for LTS versions

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-68f939e
+ * @license Angular v4.3.0-beta.0-5c576d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-68f939e
+ * @license Angular v4.3.0-beta.0-5c576d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-68f939e');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-5c576d3');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
