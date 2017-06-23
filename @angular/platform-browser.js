c6a5759d99f9f87e861250337bcaa8309a5b8202
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 17:49:16 +0000

    9344c86 docs: fix “under to hood” typo in changelog.md

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-353de51
+ * @license Angular v4.2.4-9344c86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.4-353de51');
+const VERSION = new Version('4.2.4-9344c86');
 
 /**
  * @license
