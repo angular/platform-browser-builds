ecf6853a911a761cc10275c9bb3df770f1ba2e48
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:26:00 +0000

    6191d53 docs(aio): Update resources.json - fixed language error

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-414c7e9
+ * @license Angular v4.3.0-beta.0-6191d53
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-414c7e9
+ * @license Angular v4.3.0-beta.0-6191d53
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-414c7e9');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-6191d53');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
