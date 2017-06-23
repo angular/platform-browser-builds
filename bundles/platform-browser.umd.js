74eb874f4eabb5aa4132f9f4cc8e166b3b25e84a
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:08:54 +0000

    494a0d0 fix(aio): fix topbar nav-item focus style

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-7ba720a
+ * @license Angular v4.3.0-beta.0-494a0d0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-7ba720a
+ * @license Angular v4.3.0-beta.0-494a0d0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-7ba720a');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-494a0d0');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
