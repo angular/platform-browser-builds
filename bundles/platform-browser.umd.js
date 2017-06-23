d2ebccabd7425f36364f15e255050d86f6ed1871
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:49:53 +0000

    08ecfd8 build: No longer need to bazel build twice

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-3ce9d51
+ * @license Angular v4.3.0-beta.0-08ecfd8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-3ce9d51
+ * @license Angular v4.3.0-beta.0-08ecfd8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-3ce9d51');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-08ecfd8');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
