3cc3af6c5ca4e92ebc556c5258c7dd431a000040
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 18:45:05 +0000

    f365a0f docs(aio): animations typos fixed

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-233044e
+ * @license Angular v4.3.0-beta.0-f365a0f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-233044e
+ * @license Angular v4.3.0-beta.0-f365a0f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-233044e');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-f365a0f');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
