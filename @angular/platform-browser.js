7fd1cc401ea17bea8324a26f922756118fbeaa8b
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 20:54:33 +0000

    7205d7a fix(aio): build `scripts-js` before creating a new docker image for the preview server

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-ffbdf74
+ * @license Angular v4.2.4-7205d7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.4-ffbdf74');
+const VERSION = new Version('4.2.4-7205d7a');
 
 /**
  * @license
--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-ffbdf74
+ * @license Angular v4.2.4-7205d7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.4-ffbdf74
+ * @license Angular v4.2.4-7205d7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.4-ffbdf74');
+var VERSION = new _angular_core.Version('4.2.4-7205d7a');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
