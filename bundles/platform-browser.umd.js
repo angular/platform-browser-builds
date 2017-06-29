3d9f57ed566aa54e96039c97be3cb70022c89735
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 17:00:29 +0000

    8d01db4 docs: Updated router guide content and examples for paramMap

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-4268c82
+ * @license Angular v4.3.0-beta.0-8d01db4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-4268c82
+ * @license Angular v4.3.0-beta.0-8d01db4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-4268c82');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-8d01db4');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
