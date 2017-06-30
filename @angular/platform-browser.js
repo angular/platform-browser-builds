dc3fbede68d60ec0c6b7e846482d0788715c89ad
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:30:54 +0000

    7080fcc fix(aio): restore visible sidenav keyboard focus; shift TOC up

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-54f96bf
+ * @license Angular v4.2.5-7080fcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.5-54f96bf');
+const VERSION = new Version('4.2.5-7080fcc');
 
 /**
  * @license
