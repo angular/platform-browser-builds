efc61dfad49087fbd406b633993436b931f228cb
Matias Niemelä <matias@yearofmoo.com>
2017-06-30 01:02:29 +0000

    61e6618 docs: add changelog for 4.2.5

--- @angular/platform-browser.es5.js
+++ @angular/platform-browser.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.4-f31b0d6
+ * @license Angular v4.2.5-61e6618
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4424,7 +4424,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.4-f31b0d6');
+var VERSION = new Version('4.2.5-61e6618');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
