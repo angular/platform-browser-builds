1a809aec406a6e2313aa5f01c54cb0158197c1a1
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:53:05 +0000

    27b87e8 docs(aio): fix quickstart-cli bad HTML escape chars

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-7080fcc
+ * @license Angular v4.2.5-27b87e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.5-7080fcc
+ * @license Angular v4.2.5-27b87e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.5-7080fcc');
+var VERSION = new _angular_core.Version('4.2.5-27b87e8');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
