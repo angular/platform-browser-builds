fe9b838086fa26735f5b375b9ec4eefbf58288aa
GitHub <noreply@github.com>
2017-07-01 18:00:27 +0000

    b479ed9 perf(core): refactor NgZone, decrease size by 1.2Kb (#17773)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-d5dc53e
+ * @license Angular v4.3.0-beta.1-b479ed9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-d5dc53e
+ * @license Angular v4.3.0-beta.1-b479ed9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-d5dc53e');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-b479ed9');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
