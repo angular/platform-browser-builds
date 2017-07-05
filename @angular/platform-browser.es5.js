27bf50290cf4d1837a5e82bda6e354edad530335
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 22:02:23 +0000

    c6b75b0 docs: add animations package to CONTRIBUTING.md (#15413)

--- @angular/platform-browser.es5.js
+++ @angular/platform-browser.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-0d7e1a9
+ * @license Angular v4.3.0-beta.1-c6b75b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4424,7 +4424,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-0d7e1a9');
+var VERSION = new Version('4.3.0-beta.1-c6b75b0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
