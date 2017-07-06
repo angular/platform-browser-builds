5f0c403e9c590cae3c3f5aa4db94a2bef78882c9
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 19:28:28 +0000

    dd7c113 feat(compiler): update the schema by extracting from latest chrome (#17858)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-f2f61c9
+ * @license Angular v4.3.0-beta.1-dd7c113
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-f2f61c9
+ * @license Angular v4.3.0-beta.1-dd7c113
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-f2f61c9');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-dd7c113');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
