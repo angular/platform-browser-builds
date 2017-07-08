777d8da90a4c0f761cae3f46a5db3799d890cb94
Jason Aden <jasonaden@google.com>
2017-07-08 05:15:47 +0000

    b69f0fa docs: add changelog for 4.2.6

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-8b8b4cb
+ * @license Angular v4.2.6-b69f0fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.5-8b8b4cb
+ * @license Angular v4.2.6-b69f0fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.5-8b8b4cb');
+var VERSION = new _angular_core.Version('4.2.6-b69f0fa');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
