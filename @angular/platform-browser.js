18ef0f5a7516d8194edbcd20ec4cdf024329accc
Igor Minar <iminar@google.com>
2017-07-11 19:04:58 +0000

    e131f6b docs: various minor formatting changes (#18046)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e28f097
+ * @license Angular v4.3.0-rc.0-e131f6b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-e28f097');
+const VERSION = new Version('4.3.0-rc.0-e131f6b');
 
 /**
  * @license
