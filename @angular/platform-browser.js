430285ca46c3f0aba14ce842a3bbd8bf973416bb
Igor Minar <iminar@google.com>
2017-07-11 20:13:13 +0000

    bbf2133 docs: update ECMAScript info (#17801)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-4300439
+ * @license Angular v4.3.0-rc.0-bbf2133
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-4300439');
+const VERSION = new Version('4.3.0-rc.0-bbf2133');
 
 /**
  * @license
--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-4300439
+ * @license Angular v4.3.0-rc.0-bbf2133
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-4300439
+ * @license Angular v4.3.0-rc.0-bbf2133
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-4300439');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-bbf2133');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
