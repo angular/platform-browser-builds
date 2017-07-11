305400e3057cf29f2a18382533a66520077473f3
Igor Minar <iminar@google.com>
2017-07-11 20:36:23 +0000

    20127c1 docs: fix typos and grammar (#17818)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-a50d935
+ * @license Angular v4.3.0-rc.0-20127c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-a50d935');
+const VERSION = new Version('4.3.0-rc.0-20127c1');
 
 /**
  * @license
