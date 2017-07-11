6c05f832176cb0575abc9988ed4f83d1d08ce346
Igor Minar <iminar@google.com>
2017-07-11 19:22:05 +0000

    b9525ec docs: Unbreak README Formatting (#18043)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-d9b03be
+ * @license Angular v4.3.0-rc.0-b9525ec
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-d9b03be
+ * @license Angular v4.3.0-rc.0-b9525ec
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-d9b03be');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-b9525ec');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
