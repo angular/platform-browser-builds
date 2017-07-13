14d490ab643cda13f37f58fdeee96ff6b27afe1c
Igor Minar <iminar@google.com>
2017-07-13 19:49:37 +0000

    bc3b2ac docs(aio): Updated resources to UI Components (#18067)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-4f1e4ff
+ * @license Angular v4.3.0-rc.0-bc3b2ac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-4f1e4ff');
+const VERSION = new Version('4.3.0-rc.0-bc3b2ac');
 
 /**
  * @license
