e2ba0cd082d2d398b8b2d990fa9624a5c290c624
Igor Minar <iminar@google.com>
2017-07-13 22:28:58 +0000

    db96c96 fix(aio): add <hr> in between overloads

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-ce0f4f0
+ * @license Angular v4.3.0-rc.0-db96c96
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4315,7 +4315,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-ce0f4f0');
+const VERSION = new Version('4.3.0-rc.0-db96c96');
 
 /**
  * @license
--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-ce0f4f0
+ * @license Angular v4.3.0-rc.0-db96c96
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-ce0f4f0
+ * @license Angular v4.3.0-rc.0-db96c96
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-ce0f4f0');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-db96c96');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
