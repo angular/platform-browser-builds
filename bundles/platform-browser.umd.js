6abd06aa8d7a3a1e312f912387f401c3262c86c7
Igor Minar <iminar@google.com>
2017-07-13 22:42:20 +0000

    1bf7ba8 ci(aio): exclude changes in aio/content folder

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-db96c96
+ * @license Angular v4.3.0-rc.0-1bf7ba8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-db96c96
+ * @license Angular v4.3.0-rc.0-1bf7ba8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-db96c96');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-1bf7ba8');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
