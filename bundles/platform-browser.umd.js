2b35caa3739d5c8cb29aefa488b7838efe0eb76c
Igor Minar <iminar@google.com>
2017-07-13 19:46:14 +0000

    4f1e4ff fix(common): don't guess Content-Type for FormData bodies (#18104)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-f0beb4d
+ * @license Angular v4.3.0-rc.0-4f1e4ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-f0beb4d
+ * @license Angular v4.3.0-rc.0-4f1e4ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4458,7 +4458,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-f0beb4d');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-4f1e4ff');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
