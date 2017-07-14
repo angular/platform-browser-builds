e2278e6b64ace9abb489eff55ff01e7422697f08
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 20:39:54 +0000

    a0b06be docs: add changelog for 4.3.0

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e0fa727
+ * @license Angular v4.3.0-a0b06be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-e0fa727');
+const VERSION = new Version('4.3.0-a0b06be');
 
 /**
  * @license
