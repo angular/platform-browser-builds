43f51dd3a0b2a94c272dcb92468e3fd0dc38947b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 16:53:49 +0000

    8e56c3c docs: fix type error in HttpClient sample

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-7955cac
+ * @license Angular v4.3.0-rc.0-8e56c3c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-7955cac');
+const VERSION = new Version('4.3.0-rc.0-8e56c3c');
 
 /**
  * @license
