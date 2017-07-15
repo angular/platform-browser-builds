2f2e75cab0f4cb540a3b7ed3f286ca866d9b9495
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-15 08:26:05 +0000

    6e2ddcc build(aio): render type parameters of API function exports

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-55742e4
+ * @license Angular v4.3.0-6e2ddcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-55742e4');
+const VERSION = new Version('4.3.0-6e2ddcc');
 
 /**
  * @license
--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-55742e4
+ * @license Angular v4.3.0-6e2ddcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-55742e4
+ * @license Angular v4.3.0-6e2ddcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4441,7 +4441,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-55742e4');
+var VERSION = new _angular_core.Version('4.3.0-6e2ddcc');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
