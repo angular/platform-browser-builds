909c9b7f9247ec0ddf5bab4911e9e0d4ec0afc16
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:40:25 +0000

    5c62e30 fix(common): send flushed body as error instead of null

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-256bc8a
+ * @license Angular v4.3.0-5c62e30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-256bc8a');
+const VERSION = new Version('4.3.0-5c62e30');
 
 /**
  * @license
