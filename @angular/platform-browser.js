cde1f46a884c0197a99f1af7d097def12d367133
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:31:04 +0000

    37c626e fix(aio): remove title attribute from `CodeExampleComponent`

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-f0a1109
+ * @license Angular v4.3.0-37c626e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-f0a1109');
+const VERSION = new Version('4.3.0-37c626e');
 
 /**
  * @license
