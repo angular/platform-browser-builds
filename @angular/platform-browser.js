75512426c9a1f3aa2573c65a97a73572da29c5a0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 21:19:05 +0000

    a5205c6 fix(upgrade): allow accessing AngularJS injector from downgraded module

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-5c62e30
+ * @license Angular v4.3.0-a5205c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-5c62e30');
+const VERSION = new Version('4.3.0-a5205c6');
 
 /**
  * @license
