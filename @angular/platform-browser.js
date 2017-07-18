a91be4cfde226be5807409b9f29e3d10c1d9ebf3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:18:16 +0000

    3b588fe docs: fix typo

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-e03adb9
+ * @license Angular v4.3.0-3b588fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-e03adb9');
+const VERSION = new Version('4.3.0-3b588fe');
 
 /**
  * @license
