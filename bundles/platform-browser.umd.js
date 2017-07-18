7a8e6143a88a50cbfead1ffdf87a4126d4c68c31
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 17:47:51 +0000

    eb6fb5f fix(router): should navigate to the same url when config changes

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-ad3029e
+ * @license Angular v4.3.0-eb6fb5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-ad3029e
+ * @license Angular v4.3.0-eb6fb5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4441,7 +4441,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-ad3029e');
+var VERSION = new _angular_core.Version('4.3.0-eb6fb5f');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
