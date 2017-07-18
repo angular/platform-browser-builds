6961568afc4b2b628a22460370fdb239adeca605
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:34:55 +0000

    256bc8a docs(http): Make name of injected HttpTestingController consistent

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4441,7 +4441,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-59c23c7');
+var VERSION = new _angular_core.Version('4.3.0-256bc8a');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
