6a6e977a2c023963acb4b1ca03649d7a8ebffc4e
Miško Hevery <misko@hevery.com>
2017-07-25 22:55:00 +0000

    91ab39c docs(aio) - Fixed link to the glossary dash-case term (#18311)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-6279e50
+ * @license Angular v5.0.0-beta.0-91ab39c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-6279e50
+ * @license Angular v5.0.0-beta.0-91ab39c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3766,7 +3766,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-6279e50');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-91ab39c');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
