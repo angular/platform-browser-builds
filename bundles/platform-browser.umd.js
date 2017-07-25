a6fb555b3b56755ef90d2a6d2e351e4af14efb66
Miško Hevery <misko@hevery.com>
2017-07-25 23:11:38 +0000

    b582e2b docs(aio): update examples to 4.3

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-91ab39c
+ * @license Angular v5.0.0-beta.0-b582e2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-91ab39c
+ * @license Angular v5.0.0-beta.0-b582e2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3766,7 +3766,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-91ab39c');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-b582e2b');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
