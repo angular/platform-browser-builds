8234d50752754dca6d5bfc9c10acc1a2c535898d
Miško Hevery <misko@hevery.com>
2017-07-27 21:24:26 +0000

    8de2ace docs: add changelog for 4.3.2

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.1-12b8e1a
+ * @license Angular v4.3.2-8de2ace
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.1-12b8e1a');
+const VERSION = new Version('4.3.2-8de2ace');
 
 /**
  * @license
