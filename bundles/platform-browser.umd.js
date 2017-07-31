128567052b1644e0db193135461706456d1dea11
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 20:47:11 +0000

    da8bb1b docs(aio): fixed list format in FormArray section

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.2-8de2ace
+ * @license Angular v4.3.2-da8bb1b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.2-8de2ace
+ * @license Angular v4.3.2-da8bb1b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4441,7 +4441,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.2-8de2ace');
+var VERSION = new _angular_core.Version('4.3.2-da8bb1b');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
