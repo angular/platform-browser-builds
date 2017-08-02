3dd3b1aea1bedbe5fa76431b7d78aeddf7385266
Victor Berchet <victor@suumit.com>
2017-08-02 23:23:18 +0000

    b65fe3e ci: remove chromium fold reference (#18445)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-dbc5c58
+ * @license Angular v4.3.3-b65fe3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-dbc5c58');
+const VERSION = new Version('4.3.3-b65fe3e');
 
 /**
  * @license
