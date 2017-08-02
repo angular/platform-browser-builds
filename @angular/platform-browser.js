ce7784953263f7c5a7f28c29a234f6bf67633651
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-02 21:00:57 +0000

    1e1af7f docs: changelog for 5.0.0-beta.2 release

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-49cd851
+ * @license Angular v5.0.0-beta.2-1e1af7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3626,7 +3626,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.1-49cd851');
+const VERSION = new Version('5.0.0-beta.2-1e1af7f');
 
 /**
  * @fileoverview added by tsickle
