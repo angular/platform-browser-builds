ccb759345a0e9aa18640b9e01ee0c1e93859fc83
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-02 20:43:25 +0000

    24db1ed docs: add changelog for 4.3.3

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.2-da8bb1b
+ * @license Angular v4.3.3-24db1ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.2-da8bb1b');
+const VERSION = new Version('4.3.3-24db1ed');
 
 /**
  * @license
