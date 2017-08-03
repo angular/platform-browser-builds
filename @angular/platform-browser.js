89e52e1ef7ac1c50cdcf43ad804edfbf3defa832
Victor Berchet <victor@suumit.com>
2017-08-03 18:24:28 +0000

    e7e7622 fix(compiler): ignore @import in multi-line css (#18452)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-4176832
+ * @license Angular v4.3.3-e7e7622
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-4176832');
+const VERSION = new Version('4.3.3-e7e7622');
 
 /**
  * @license
