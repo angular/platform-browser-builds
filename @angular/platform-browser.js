bc5921a36e09a45032d9395809160630f634cb5c
Victor Berchet <victor@suumit.com>
2017-08-03 00:44:16 +0000

    1640d2a refactor(platform-browser): compiler platform-browser packages cleanly (#18464)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-5f501c7
+ * @license Angular v5.0.0-beta.2-1640d2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3626,7 +3626,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-5f501c7');
+const VERSION = new Version('5.0.0-beta.2-1640d2a');
 
 /**
  * @fileoverview added by tsickle
