1c2652f60a13283a7190b6eb985f8fded09210a2
Victor Berchet <victor@suumit.com>
2017-08-07 22:00:49 +0000

    685cc26 fix(common): don't recreate view when context shape doesn't change (#18277)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-5b7432b
+ * @license Angular v5.0.0-beta.2-685cc26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3626,7 +3626,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-5b7432b');
+const VERSION = new Version('5.0.0-beta.2-685cc26');
 
 /**
  * @fileoverview added by tsickle
