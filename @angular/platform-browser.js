3c632531d1a1c1bb978313bef61faa993cd2f20b
Victor Berchet <victor@suumit.com>
2017-08-07 19:25:14 +0000

    e0660b1 fix(animations): support persisting dynamic styles within animation states (#18468)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-3212f8c
+ * @license Angular v4.3.3-e0660b1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-3212f8c');
+const VERSION = new Version('4.3.3-e0660b1');
 
 /**
  * @license
