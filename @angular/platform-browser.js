17e840ff1e25c25638dce13115fe96759d687711
Victor Berchet <victor@suumit.com>
2017-08-09 22:01:40 +0000

    f4cb453 test(aio): fix running docs examples against local builds (#18520)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-3efc88f
+ * @license Angular v4.3.3-f4cb453
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-3efc88f');
+const VERSION = new Version('4.3.3-f4cb453');
 
 /**
  * @license
