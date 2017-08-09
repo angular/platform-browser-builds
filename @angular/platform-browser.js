e2dbbe9ad90ab0070197f8dd418bf49de2bdcfe3
Victor Berchet <victor@suumit.com>
2017-08-09 21:40:50 +0000

    e54bd59 fix(core): forbid destroyed views to be inserted or moved in VC (#18568)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-1e18331
+ * @license Angular v5.0.0-beta.2-e54bd59
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3626,7 +3626,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-1e18331');
+const VERSION = new Version('5.0.0-beta.2-e54bd59');
 
 /**
  * @fileoverview added by tsickle
