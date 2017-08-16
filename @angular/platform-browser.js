c9f8fca758cbead53cdd127cc6f0e0670d4c5587
Hans Larsen <hans@hansl.ca>
2017-08-16 00:33:20 +0000

    7662cef docs: remove TypeScript to JavaScript guide & sample

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-1cb6076
+ * @license Angular v4.3.4-7662cef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.4-1cb6076');
+const VERSION = new Version('4.3.4-7662cef');
 
 /**
  * @license
