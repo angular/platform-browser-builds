18aa7d810289e1066ca3cfe4c3f6ec46cfc76a15
Miško Hevery <misko@hevery.com>
2017-08-19 00:05:00 +0000

    3b571a4 build(aio): do not auto-link code elements already inside a link (#18776)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-f9b2905
+ * @license Angular v4.3.5-3b571a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.5-f9b2905
+ * @license Angular v4.3.5-3b571a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4441,7 +4441,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.5-f9b2905');
+var VERSION = new _angular_core.Version('4.3.5-3b571a4');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
