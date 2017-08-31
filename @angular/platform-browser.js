ae250171fba81b05ec67877ac5f83838ceab39e7
Jason Aden <jasonaden@google.com>
2017-08-31 01:41:11 +0000

    ba7d70e build: fix changelog to remove AIO line items (#18956)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.6-497e017');
+const VERSION = new Version('4.3.6-ba7d70e');
 
 /**
  * @license
