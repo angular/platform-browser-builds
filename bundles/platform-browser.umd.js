e8714407ebbb177a7d7dd605ce2c09e1cb9d2d2f
Misko Hevery <misko@hevery.com>
2017-09-02 05:48:10 +0000

    aeb98db docs: add changelog for 4.4.0-RC.0

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-8036d05
+ * @license Angular v4.4.0-RC.0-aeb98db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.0-RC.0-8036d05
+ * @license Angular v4.4.0-RC.0-aeb98db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4441,7 +4441,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.0-RC.0-8036d05');
+var VERSION = new _angular_core.Version('4.4.0-RC.0-aeb98db');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
