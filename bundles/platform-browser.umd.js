3a8891d26269c95721c68e3d3b6773248d6b1033
Miško Hevery <misko@hevery.com>
2017-09-02 05:20:01 +0000

    8036d05 release: cut the 4.4.0-RC.0 release

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-7d137d7
+ * @license Angular v4.4.0-RC.0-8036d05
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.6-7d137d7
+ * @license Angular v4.4.0-RC.0-8036d05
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4441,7 +4441,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.6-7d137d7');
+var VERSION = new _angular_core.Version('4.4.0-RC.0-8036d05');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
