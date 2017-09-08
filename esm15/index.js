70069f19ac5f04bed058a5f1d93e467fc209c993
Matias Niemelä <matias@yearofmoo.com>
2017-09-08 22:43:04 +0000

    a176cd6 build(aio): update package.json for angular-in-mem-web-api 0.4.0

--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-626555c
+ * @license Angular v5.0.0-beta.6-a176cd6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3740,7 +3740,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.6-626555c');
+const VERSION = new Version('5.0.0-beta.6-a176cd6');
 
 /**
  * @fileoverview added by tsickle
