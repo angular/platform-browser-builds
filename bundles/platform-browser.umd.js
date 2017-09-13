3513025034e95e5af99f979d6ccfd6be9ecfe8b3
Matias Niemelä <matias@yearofmoo.com>
2017-09-13 17:53:13 +0000

    bf94f87 refactor(compiler): use new ngc for i18n (#19095)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-c8f742e
+ * @license Angular v5.0.0-beta.6-bf94f87
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.6-c8f742e
+ * @license Angular v5.0.0-beta.6-bf94f87
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4916,7 +4916,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.6-c8f742e');
+var VERSION = new _angular_core.Version('5.0.0-beta.6-bf94f87');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
