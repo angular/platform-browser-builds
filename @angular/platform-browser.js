2f780a804cfc49b38856a6d5bb70bd8c8b216308
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:20:44 +0000

    ff87294 docs(changelog): fix the changelog link for comparing 4.4.1 to 4.3.6

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.1-0c72f7d
+ * @license Angular v4.4.1-ff87294
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.1-0c72f7d');
+const VERSION = new Version('4.4.1-ff87294');
 
 /**
  * @license
--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.1-0c72f7d
+ * @license Angular v4.4.1-ff87294
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.1-0c72f7d
+ * @license Angular v4.4.1-ff87294
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4441,7 +4441,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.1-0c72f7d');
+var VERSION = new _angular_core.Version('4.4.1-ff87294');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
