f1997e7791653d1cb659fd0f3e5eac4c0f444445
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 21:37:09 +0000

    b20c5d2 fix(upgrade): remove code setting id attribute. (#19182)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-0bafd03
+ * @license Angular v4.4.0-RC.0-b20c5d2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.0-RC.0-0bafd03');
+const VERSION = new Version('4.4.0-RC.0-b20c5d2');
 
 /**
  * @license
