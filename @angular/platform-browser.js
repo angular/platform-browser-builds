f0e3525d98dcca59bfdd1362f26e31a47d2c2814
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 21:11:10 +0000

    0bafd03 revert: test(packaging): added test for source map correctness

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-e8d1858
+ * @license Angular v4.4.0-RC.0-0bafd03
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.0-RC.0-e8d1858');
+const VERSION = new Version('4.4.0-RC.0-0bafd03');
 
 /**
  * @license
