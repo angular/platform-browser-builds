9560701b50e92432f057cf3d110bc8b0377e5858
Igor Minar <igor@angularjs.org>
2017-09-19 23:32:42 +0000

    9d2236a ci: update yarn to 1.0.2 (#19270)

--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e17128e
+ * @license Angular v5.0.0-beta.7-9d2236a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3738,7 +3738,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-e17128e');
+const VERSION = new Version('5.0.0-beta.7-9d2236a');
 
 /**
  * @fileoverview added by tsickle
