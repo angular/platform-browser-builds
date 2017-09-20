649eaff3a1589d3412e5a65369ff39873e6ccd8a
Igor Minar <igor@angularjs.org>
2017-09-20 23:45:26 +0000

    9e84719 test: fix cli-integration tests that got broken by yarn update (#19297)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-2c4107c
+ * @license Angular v5.0.0-beta.7-9e84719
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3734,7 +3734,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-2c4107c');
+const VERSION = new Version('5.0.0-beta.7-9e84719');
 
 /**
  * @fileoverview added by tsickle
