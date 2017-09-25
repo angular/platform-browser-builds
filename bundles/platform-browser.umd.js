3d3b141c5525dabf3e8c7fc7add356fc7d07a525
Victor Berchet <victor@suumit.com>
2017-09-25 20:03:25 +0000

    4767902 fix(compiler-cli): do not validate metadata from declaration files (#19324)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e224e3d
+ * @license Angular v5.0.0-beta.7-4767902
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-e224e3d
+ * @license Angular v5.0.0-beta.7-4767902
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5161,7 +5161,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-e224e3d');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-4767902');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
