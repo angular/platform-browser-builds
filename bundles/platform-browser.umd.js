044494fb0fb7c8bb00c4c883ab94517cb9c0a446
Victor Berchet <victor@suumit.com>
2017-09-25 19:34:53 +0000

    5c99b01 build(aio): run the upload server as a non-previleged user

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-04bc525
+ * @license Angular v4.4.3-5c99b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.3-04bc525
+ * @license Angular v4.4.3-5c99b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4441,7 +4441,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.3-04bc525');
+var VERSION = new _angular_core.Version('4.4.3-5c99b01');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
