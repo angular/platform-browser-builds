e0a79c05e0375c01f85d7cad9973189840b675e7
Victor Berchet <victor@suumit.com>
2017-09-25 22:00:22 +0000

    ffceae0 refactor(aio): remove unnecessary `--` from yarn commands

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-bb1665c
+ * @license Angular v5.0.0-beta.7-ffceae0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3922,7 +3922,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-bb1665c');
+const VERSION = new Version('5.0.0-beta.7-ffceae0');
 
 /**
  * @fileoverview added by tsickle
