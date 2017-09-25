25d050779de013e1a69155df1001cba7ce3bb150
Victor Berchet <victor@suumit.com>
2017-09-25 19:21:27 +0000

    4ae546b build(aio): ensure decorators with shared interface types are found (#19361)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-adb0b76
+ * @license Angular v5.0.0-beta.7-4ae546b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3922,7 +3922,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-adb0b76');
+const VERSION = new Version('5.0.0-beta.7-4ae546b');
 
 /**
  * @fileoverview added by tsickle
