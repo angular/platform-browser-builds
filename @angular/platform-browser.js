6b440f680f917a98816e7038b32d69f474286676
Victor Berchet <victor@suumit.com>
2017-09-27 21:08:59 +0000

    55a7443 docs(aio): edit summary and next step headers (#16962)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-bc81fbd
+ * @license Angular v4.4.3-55a7443
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.3-bc81fbd');
+const VERSION = new Version('4.4.3-55a7443');
 
 /**
  * @license
