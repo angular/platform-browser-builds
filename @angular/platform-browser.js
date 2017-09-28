20b564231eb74054282ea71207692d8bd3739bbc
Victor Berchet <victor@suumit.com>
2017-09-28 19:44:31 +0000

    d489ad5 docs: add changelog for 4.4.4

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-7fc2dce
+ * @license Angular v4.4.4-d489ad5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.3-7fc2dce');
+const VERSION = new Version('4.4.4-d489ad5');
 
 /**
  * @license
