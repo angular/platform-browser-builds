6536f0c10e9e188bf1de8e379648d2e2dac57e69
Victor Berchet <victor@suumit.com>
2017-09-28 19:26:00 +0000

    7fc2dce fix(compiler): do not consider a reference with members as a reference (#19466)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-c3b39ba
+ * @license Angular v4.4.3-7fc2dce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.3-c3b39ba');
+const VERSION = new Version('4.4.3-7fc2dce');
 
 /**
  * @license
