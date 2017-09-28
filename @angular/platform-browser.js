e3dab3632fdc5bb6c3e220a6f91537a386976281
Victor Berchet <victor@suumit.com>
2017-09-28 18:19:10 +0000

    c3b39ba fix(compiler): correctly map error message locations (#19424)

--- @angular/platform-browser.js
+++ @angular/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-2cd88bf
+ * @license Angular v4.4.3-c3b39ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4299,7 +4299,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.3-2cd88bf');
+const VERSION = new Version('4.4.3-c3b39ba');
 
 /**
  * @license
