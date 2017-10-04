b1d5b05fd05fc68d7ec0b838605ed725f3ce63b6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:21:00 +0000

    696af79 fix(compiler): properly work on windows

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-d4d9009
+ * @license Angular v5.0.0-rc.0-696af79
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.0-d4d9009
+ * @license Angular v5.0.0-rc.0-696af79
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5163,7 +5163,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.0-d4d9009');
+var VERSION = new _angular_core.Version('5.0.0-rc.0-696af79');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
