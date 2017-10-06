5d7e158a546854d753f81aa2aa0942f117353739
Chuck Jazdzewski <chuckj@google.com>
2017-10-06 22:57:23 +0000

    f83989b fix(compiler-cli): produce smaller source maps for templates (#19578)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-03227e6
+ * @license Angular v5.0.0-rc.1-f83989b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.1-03227e6
+ * @license Angular v5.0.0-rc.1-f83989b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5163,7 +5163,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.1-03227e6');
+var VERSION = new _angular_core.Version('5.0.0-rc.1-f83989b');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
