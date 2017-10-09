d663a480e64544dcde3acb4dc8a01d8fa54dbf14
Chuck Jazdzewski <chuckj@google.com>
2017-10-09 23:34:07 +0000

    79deeac docs(aio): add copy about ngNoForm to NgForm API doc (#19561)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-32ca685
+ * @license Angular v5.0.0-rc.1-79deeac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.1-32ca685
+ * @license Angular v5.0.0-rc.1-79deeac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5163,7 +5163,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.1-32ca685');
+var VERSION = new _angular_core.Version('5.0.0-rc.1-79deeac');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
