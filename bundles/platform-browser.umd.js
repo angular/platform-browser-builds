3848b457482be42df336a84eae30ec2e97a8ae9a
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 21:23:09 +0000

    734378c fix(compiler): `TestBed.overrideProvider` should keep imported `NgModule`s eager (#19624)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-3232125
+ * @license Angular v4.4.4-734378c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.4-3232125
+ * @license Angular v4.4.4-734378c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4437,7 +4437,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.4-3232125');
+var VERSION = new _angular_core.Version('4.4.4-734378c');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
