a6a54bff1e64e8aaf85b3782f9047e3029738996
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 22:07:16 +0000

    65ca7fd docs(aio): update banner ad (#19669)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-3e3f918
+ * @license Angular v4.4.4-65ca7fd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.4-3e3f918
+ * @license Angular v4.4.4-65ca7fd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4437,7 +4437,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.4-3e3f918');
+var VERSION = new _angular_core.Version('4.4.4-65ca7fd');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
