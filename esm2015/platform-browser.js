06506a63dcb29cad1c239e3574bf25ed00c4e5de
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:41:04 +0000

    910735d build: fix `yarn install` command (`--freeze-lockfile` --> `--frozen-lockfile`)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-820bb7b
+ * @license Angular v5.0.0-rc.3-910735d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3924,7 +3924,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-820bb7b');
+const VERSION = new Version('5.0.0-rc.3-910735d');
 
 /**
  * @fileoverview added by tsickle
