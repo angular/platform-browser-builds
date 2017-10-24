f67710a5f070e4e989f356f992b61cd2ea23e06f
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 05:17:43 +0000

    a0ae120 fix(compiler-cli): report all diagnostic error messages (#19886)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.4-bde5701
+ * @license Angular v5.0.0-rc.4-a0ae120
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3924,7 +3924,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.4-bde5701');
+const VERSION = new Version('5.0.0-rc.4-a0ae120');
 
 /**
  * @fileoverview added by tsickle
