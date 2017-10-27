bf488c00be6bf3d6d194c7005fe3ede53b44b51c
Matias Niemelä <matias@yearofmoo.com>
2017-10-27 03:16:24 +0000

    394d951 docs: add changelog for 5.0.0-rc.7

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-31797d3
+ * @license Angular v5.0.0-rc.7-394d951
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3924,7 +3924,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.6-31797d3');
+const VERSION = new Version('5.0.0-rc.7-394d951');
 
 /**
  * @fileoverview added by tsickle
