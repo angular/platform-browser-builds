c9ad0df7a2c5bed86f4970c1195397317b0fd421
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:27:20 +0000

    de24d54 fix(compiler): report errors properly in G3 in certain conditions (#20041)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-c295aee
+ * @license Angular v5.0.0-rc.8-de24d54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3924,7 +3924,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-c295aee');
+const VERSION = new Version('5.0.0-rc.8-de24d54');
 
 /**
  * @fileoverview added by tsickle
