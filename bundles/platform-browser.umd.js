c93584ca41cfbe609042504856f32e6fe9b8d63e
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:29:42 +0000

    e1fb65c docs(packaging): fix typo

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.9-5225fdb
+ * @license Angular v5.0.0-e1fb65c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.9-5225fdb
+ * @license Angular v5.0.0-e1fb65c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5163,7 +5163,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.9-5225fdb');
+var VERSION = new _angular_core.Version('5.0.0-e1fb65c');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
