b2a40c77dfeca6bbb33ea8aac30ebe0da97a7be5
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:37:09 +0000

    a363be6 docs(aio): upgrade to v5 and cli 1.5

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-e1fb65c
+ * @license Angular v5.0.0-a363be6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-e1fb65c
+ * @license Angular v5.0.0-a363be6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5163,7 +5163,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-e1fb65c');
+var VERSION = new _angular_core.Version('5.0.0-a363be6');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
