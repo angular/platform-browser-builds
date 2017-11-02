e1f8d93515e479b5c309a84b03e554fb6f35e512
Victor Berchet <victor@suumit.com>
2017-11-02 22:13:44 +0000

    17ed14f docs: fix changelog for v5 (#20097)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-7186c9c
+ * @license Angular v5.1.0-beta.0-17ed14f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5122,7 +5122,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-7186c9c');
+var VERSION = new Version('5.1.0-beta.0-17ed14f');
 
 /**
  * @fileoverview added by tsickle
