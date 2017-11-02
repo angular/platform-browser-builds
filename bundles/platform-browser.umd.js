62cdadf49f56a420cb10cff0500bb65030e1fb06
Victor Berchet <victor@suumit.com>
2017-11-02 23:16:23 +0000

    37a740b build: remove local yarn (#19981)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-067e926
+ * @license Angular v5.0.0-37a740b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-067e926
+ * @license Angular v5.0.0-37a740b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5163,7 +5163,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-067e926');
+var VERSION = new _angular_core.Version('5.0.0-37a740b');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
