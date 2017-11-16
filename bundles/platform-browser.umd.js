74fc2bb24a3896047fa5dc0857b779b1f5332be4
Miško Hevery <misko@hevery.com>
2017-11-16 22:38:43 +0000

    4064cbe docs: add changelog for 5.1.0-beta.1

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dcfffbf
+ * @license Angular v5.1.0-beta.1-4064cbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-dcfffbf
+ * @license Angular v5.1.0-beta.1-4064cbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5191,7 +5191,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-dcfffbf');
+var VERSION = new _angular_core.Version('5.1.0-beta.1-4064cbe');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
