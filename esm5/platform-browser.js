9ce07d84e2d23c5e0298d9f37976c5b9cb78e768
Miško Hevery <misko@hevery.com>
2017-11-21 05:26:47 +0000

    2a9d2ba docs: update changelog with missing feature in 5.1.0-beta.1 (#20508)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-69c53c3
+ * @license Angular v5.1.0-beta.1-2a9d2ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5185,7 +5185,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.1-69c53c3');
+var VERSION = new Version('5.1.0-beta.1-2a9d2ba');
 
 /**
  * @fileoverview added by tsickle
