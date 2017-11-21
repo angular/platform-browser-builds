194fda910ad8878987da9e4cfb1d0610549cf2c8
Miško Hevery <misko@hevery.com>
2017-11-21 17:07:59 +0000

    b53ead4 fix(compiler): support event bindings in `fullTemplateTypeCheck` (#20490)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-82aace6
+ * @license Angular v5.0.2-b53ead4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3956,7 +3956,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.2-82aace6');
+const VERSION = new Version('5.0.2-b53ead4');
 
 /**
  * @fileoverview added by tsickle
