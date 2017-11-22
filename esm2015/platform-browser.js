7c13200c25168fc8be77b62f55e9b52d2c611bf9
Miško Hevery <misko@hevery.com>
2017-11-22 21:24:00 +0000

    686c4ef docs(aio): add ngATL header (#20590)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-efce396
+ * @license Angular v5.0.2-686c4ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3967,7 +3967,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.2-efce396');
+const VERSION = new Version('5.0.2-686c4ef');
 
 /**
  * @fileoverview added by tsickle
