f7ce742a823fffd38710229da87da4c644b5e47e
Miško Hevery <misko@hevery.com>
2017-11-30 03:59:38 +0000

    b450c83 docs(aio): remove services plurality (#20696)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-647ca64
+ * @license Angular v5.0.3-b450c83
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.3-647ca64
+ * @license Angular v5.0.3-b450c83
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5209,7 +5209,7 @@ var By = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.3-647ca64');
+var VERSION = new _angular_core.Version('5.0.3-b450c83');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
