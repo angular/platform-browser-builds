cfc7c1c046359fa11980c2e8e9b0255b89507b7f
Igor Minar <igor@angularjs.org>
2017-12-06 06:15:46 +0000

    6d15dd7 docs(aio): architecture review for a11y (#17848)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-a2fde2b
+ * @license Angular v5.0.5-6d15dd7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3967,7 +3967,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-a2fde2b');
+const VERSION = new Version('5.0.5-6d15dd7');
 
 /**
  * @fileoverview added by tsickle
