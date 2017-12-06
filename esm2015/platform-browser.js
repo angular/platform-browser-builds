41e45783e3f6f4ee5f4c1e86cdbac532030c666b
Igor Minar <igor@angularjs.org>
2017-12-06 15:20:07 +0000

    d824cc2 ci: upgrade to node 8 and Bazel 0.8 (#20807)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-c91c0d4
+ * @license Angular v5.0.5-d824cc2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3967,7 +3967,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-c91c0d4');
+const VERSION = new Version('5.0.5-d824cc2');
 
 /**
  * @fileoverview added by tsickle
