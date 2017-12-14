b6ad31785cda0df7d931f5cafc519af2652cdf9b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 17:29:52 +0000

    49c45f3 ci(aio): move e2e tests to non-optional job

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-e3140ae
+ * @license Angular v5.1.1-49c45f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5196,7 +5196,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.1-e3140ae');
+var VERSION = new Version('5.1.1-49c45f3');
 
 /**
  * @fileoverview added by tsickle
