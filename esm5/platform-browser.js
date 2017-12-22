22b73fff89f13050ef84a1089ac6eb3829ced262
Igor Minar <igor@angularjs.org>
2017-12-22 04:30:39 +0000

    41abcc3 docs(service-worker): fix word wrap (#21114)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-4337768
+ * @license Angular v5.1.2-41abcc3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5196,7 +5196,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-4337768');
+var VERSION = new Version('5.1.2-41abcc3');
 
 /**
  * @fileoverview added by tsickle
