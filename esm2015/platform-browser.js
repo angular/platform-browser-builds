f6f84a319b943964984c095859016883aeeaa262
Igor Minar <igor@angular.io>
2017-12-23 05:58:40 +0000

    12702b2 docs(changelog): fix typo in 5.1.1 (#21007)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-5de91fe
+ * @license Angular v5.1.2-12702b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3995,7 +3995,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-5de91fe');
+const VERSION = new Version('5.1.2-12702b2');
 
 /**
  * @fileoverview added by tsickle
