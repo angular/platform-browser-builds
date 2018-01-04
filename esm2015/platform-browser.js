61a36ccf5557fffaf8d6484a5588ef2b59353cdb
Kara Erickson <karakara@google.com>
2018-01-04 23:00:34 +0000

    d79a0ba docs(aio): add missing imports to interceptor example (#21259)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-b7307c9
+ * @license Angular v5.1.3-d79a0ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3995,7 +3995,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-b7307c9');
+const VERSION = new Version('5.1.3-d79a0ba');
 
 /**
  * @fileoverview added by tsickle
