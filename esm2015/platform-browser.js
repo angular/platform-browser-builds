fff5f4cedba730f6ec00f3952c17481b353f309c
Kara Erickson <karakara@google.com>
2018-01-05 20:13:59 +0000

    17e85cd build(aio): fix plunker for custom plunkers (#21291)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-c809545
+ * @license Angular v5.1.3-17e85cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3995,7 +3995,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-c809545');
+const VERSION = new Version('5.1.3-17e85cd');
 
 /**
  * @fileoverview added by tsickle
