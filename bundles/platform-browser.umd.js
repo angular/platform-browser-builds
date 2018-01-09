ea4e1ce32aca181414e41a3ceb0971ad135c46f8
Kara Erickson <karakara@google.com>
2018-01-09 00:29:49 +0000

    a73b0b6 docs(aio): fix punctuation for clarity (#21325) PR Close #21325

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-fda3815
+ * @license Angular v5.1.3-a73b0b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-fda3815
+ * @license Angular v5.1.3-a73b0b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5237,7 +5237,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-fda3815');
+var VERSION = new _angular_core.Version('5.1.3-a73b0b6');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
