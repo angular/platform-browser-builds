4d97e3915b210274791c169c6f2387da7af5fdf2
Kara Erickson <karakara@google.com>
2018-01-09 20:42:54 +0000

    96efb4b ci: temporary work-around for Travis issue (#21422)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-7c34807
+ * @license Angular v5.1.3-96efb4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5196,7 +5196,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-7c34807');
+var VERSION = new Version('5.1.3-96efb4b');
 
 /**
  * @fileoverview added by tsickle
