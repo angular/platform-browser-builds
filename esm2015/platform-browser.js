57de7fbb2f171af228b3480b06490f4340fe3cea
Kara Erickson <karakara@google.com>
2018-01-09 19:27:06 +0000

    7c34807 build(aio): upgrade to zone.js@0.8.19 (#21349)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-84f6b14
+ * @license Angular v5.1.3-7c34807
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3995,7 +3995,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-84f6b14');
+const VERSION = new Version('5.1.3-7c34807');
 
 /**
  * @fileoverview added by tsickle
