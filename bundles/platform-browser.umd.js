a2630c9d1024f1a156be4b075cc91bb0da7cf176
Kara Erickson <karakara@google.com>
2018-01-09 19:12:43 +0000

    84f6b14 fix(bazel): Give correct module names for ES6 output (#21320)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-39f81b2
+ * @license Angular v5.1.3-84f6b14
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-39f81b2
+ * @license Angular v5.1.3-84f6b14
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5237,7 +5237,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-39f81b2');
+var VERSION = new _angular_core.Version('5.1.3-84f6b14');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
