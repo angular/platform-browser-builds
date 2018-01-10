cefde085960e5928accc5abcd39cba5962b8dbea
Alex Eagle <alexeagle@google.com>
2018-01-10 04:34:40 +0000

    a0166bd docs(bazel): document an installation issue

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-0d1611f
+ * @license Angular v5.2.0-a0166bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5191,7 +5191,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-0d1611f');
+var VERSION = new Version('5.2.0-a0166bd');
 
 /**
  * @fileoverview added by tsickle
