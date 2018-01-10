a0467a453f2a5d5d040f6390d78c065b97a2fe50
Alex Eagle <alexeagle@google.com>
2018-01-10 21:41:47 +0000

    7bdd9ae build: upgrade yarn to 1.3.2 (#21406)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-5ede67c
+ * @license Angular v5.2.0-7bdd9ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-5ede67c
+ * @license Angular v5.2.0-7bdd9ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5232,7 +5232,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-5ede67c');
+var VERSION = new _angular_core.Version('5.2.0-7bdd9ae');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
