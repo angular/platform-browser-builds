b6c031c09faa096fbee78b06f5299691f75dd358
Alex Eagle <alexeagle@google.com>
2018-01-10 22:16:42 +0000

    650f5fb build(bazel): fix merge conflict between c4f02e2 and ef956a2 (#21453)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c32e833
+ * @license Angular v5.2.0-650f5fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5191,7 +5191,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-c32e833');
+var VERSION = new Version('5.2.0-650f5fb');
 
 /**
  * @fileoverview added by tsickle
