9ad8eb1c18ee6665d60594a0ca62d2f8ecf4b3c0
Miško Hevery <misko@hevery.com>
2018-01-23 18:44:14 +0000

    21f7669 refactor(bazel): pass around tsconfig as a file, not a path (#21614)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-4b68fdc
+ * @license Angular v5.2.1-21f7669
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.1-4b68fdc
+ * @license Angular v5.2.1-21f7669
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5232,7 +5232,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.1-4b68fdc');
+var VERSION = new _angular_core.Version('5.2.1-21f7669');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
