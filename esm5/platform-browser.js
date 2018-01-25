9b2f6a8820e37f534ab56ddfdab41ce0bc3f97a9
Misko Hevery <misko@hevery.com>
2018-01-25 06:38:16 +0000

    3e03dbe refactor(ivy): flatten hooks and collapse LView hook properties (#21650)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-d08785d
+ * @license Angular v6.0.0-beta.1-3e03dbe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5191,7 +5191,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-d08785d');
+var VERSION = new Version('6.0.0-beta.1-3e03dbe');
 
 /**
  * @fileoverview added by tsickle
