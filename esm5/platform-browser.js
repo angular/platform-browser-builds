6ce83d02a99ab4dde06722b3a34b7965ffe4cd49
Jason Aden <jasonaden@google.com>
2018-01-31 18:43:54 +0000

    9dca5f2 refactor(ivy): save check methods separately (#21795) (#21905)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-06d6c76
+ * @license Angular v6.0.0-beta.1-9dca5f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5191,7 +5191,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-06d6c76');
+var VERSION = new Version('6.0.0-beta.1-9dca5f2');
 
 /**
  * @fileoverview added by tsickle
