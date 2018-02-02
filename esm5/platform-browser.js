71c6d52af0005aa24e6922d3762f7da5816d8a45
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:11:21 +0000

    965eecc build(aio): move zip and live-example generation to `yarn predocs` task (#21970)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c4fb696
+ * @license Angular v5.2.3-965eecc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5191,7 +5191,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-c4fb696');
+var VERSION = new Version('5.2.3-965eecc');
 
 /**
  * @fileoverview added by tsickle
