c91c9fa99adc5fc9af5f2f66b3c9d546414b76c7
Miško Hevery <misko@hevery.com>
2018-02-09 21:31:37 +0000

    bc1e229 docs(aio): several fix for ngmodule guides (#21517)

--- esm2015/platform-browser.js
+++ esm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-7007f51
+ * @license Angular v6.0.0-beta.3-bc1e229
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4108,7 +4108,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-7007f51');
+const VERSION = new Version('6.0.0-beta.3-bc1e229');
 
 /**
  * @fileoverview added by tsickle
