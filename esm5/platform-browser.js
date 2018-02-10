1ed0ebeed46723480a9da2caf4cb3e67e30506e9
Miško Hevery <misko@hevery.com>
2018-02-10 01:43:59 +0000

    d57fd0b fix(bazel): allow TS to read ambient typings (#21876)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-9b280ee
+ * @license Angular v5.2.4-d57fd0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5344,7 +5344,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-9b280ee');
+var VERSION = new Version('5.2.4-d57fd0b');
 
 /**
  * @fileoverview added by tsickle
