8fd678d2117337b035db03b8a1798f655cd15aa3
Miško Hevery <misko@hevery.com>
2018-02-12 18:34:01 +0000

    a616dd6 fix(aio): remove broken span closing tag (#22146)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-d57fd0b
+ * @license Angular v5.2.4-a616dd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.4-d57fd0b
+ * @license Angular v5.2.4-a616dd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5385,7 +5385,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-d57fd0b');
+var VERSION = new _angular_core.Version('5.2.4-a616dd6');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
