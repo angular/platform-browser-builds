5b381ad88282716b667eb3779737174a3da3f20e
Victor Berchet <victor@suumit.com>
2018-02-23 18:13:20 +0000

    aad4316 refactor(ivy): rename componentRefresh to directiveRefresh (#22395)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-a81d599
+ * @license Angular v6.0.0-beta.5-aad4316
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-a81d599
+ * @license Angular v6.0.0-beta.5-aad4316
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5391,7 +5391,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-a81d599');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-aad4316');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
