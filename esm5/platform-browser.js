46b7a5630a838a6afcdc1016d54ea9d3b2ca18e1
Kara Erickson <karakara@google.com>
2018-03-09 08:11:09 +0000

    b26a905 feat(ivy): support attaching and detaching views from change detection (#22670)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-b0b9ca3
+ * @license Angular v6.0.0-beta.7-b26a905
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-b0b9ca3');
+var VERSION = new Version('6.0.0-beta.7-b26a905');
 
 /**
  * @fileoverview added by tsickle
