f159bb6775cd4bc8d795f743a930909a9c1b1de0
Kara Erickson <karakara@google.com>
2018-03-09 21:28:31 +0000

    e8326e6 fix: overloading a function doesn't generate all of the signatures (#22569)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-b5be18f
+ * @license Angular v6.0.0-beta.7-e8326e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-b5be18f');
+var VERSION = new Version('6.0.0-beta.7-e8326e6');
 
 /**
  * @fileoverview added by tsickle
