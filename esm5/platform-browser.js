9b1de83fb1158bb926201419b6198cb3e2409c4e
Kara Erickson <karakara@google.com>
2018-03-12 17:32:47 +0000

    3f70aba fix(compiler-cli): disableTypeScriptVersionCheck should be applied even for older tsc versions (#22669)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-0bf6fa5
+ * @license Angular v6.0.0-beta.7-3f70aba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-0bf6fa5');
+var VERSION = new Version('6.0.0-beta.7-3f70aba');
 
 /**
  * @fileoverview added by tsickle
