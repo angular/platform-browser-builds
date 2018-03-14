5b3b9718d9f4cf780c2a38f04b49969f3dc2790d
Kara Erickson <karakara@google.com>
2018-03-14 19:49:00 +0000

    f5a98f4 fix(core): remove core animation import symbols (#22692)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-c09bd67
+ * @license Angular v6.0.0-beta.7-f5a98f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-c09bd67');
+var VERSION = new Version('6.0.0-beta.7-f5a98f4');
 
 /**
  * @fileoverview added by tsickle
