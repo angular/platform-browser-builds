1055d525eb177452c85ad33b8753efbd34b3dc94
Miško Hevery <misko@hevery.com>
2018-03-16 18:51:27 +0000

    bafdad9 fix(ivy): cache local names and support multiple locals with same value (#22807)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-cedc04c
+ * @license Angular v6.0.0-beta.7-bafdad9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-cedc04c');
+var VERSION = new Version('6.0.0-beta.7-bafdad9');
 
 /**
  * @fileoverview added by tsickle
