e73fdf81b018aeb355768981c6d025cbb9788c0d
Igor Minar <igor@angular.io>
2018-03-18 21:31:23 +0000

    0bede54 style: fix formatting issues on the master branch (#22854)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-e27cfd6
+ * @license Angular v6.0.0-beta.7-0bede54
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-e27cfd6');
+var VERSION = new Version('6.0.0-beta.7-0bede54');
 
 /**
  * @fileoverview added by tsickle
