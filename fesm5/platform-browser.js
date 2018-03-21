af119b24a1737c23315bec39594a26510c849e66
Igor Minar <igor@angular.io>
2018-03-21 17:17:41 +0000

    4f2c51f docs: switch over to the preview url for the rxjs upgrade guide in changelog

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-7cd8e8d
+ * @license Angular v6.0.0-rc.0-4f2c51f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-7cd8e8d');
+var VERSION = new Version('6.0.0-rc.0-4f2c51f');
 
 /**
  * @fileoverview added by tsickle
