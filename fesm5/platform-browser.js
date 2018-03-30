f83f9a08b354e92250554df6033cf68a273709f0
Igor Minar <igor@angular.io>
2018-03-30 02:17:50 +0000

    481b22e fix(bazel): downlevel decorators in fesm5 files (#23078)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-45e090b
+ * @license Angular v6.0.0-rc.0-481b22e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-45e090b');
+var VERSION = new Version('6.0.0-rc.0-481b22e');
 
 /**
  * @fileoverview added by tsickle
