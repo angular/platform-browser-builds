c5905358d3a4ba09103a9e6160a0f668a5d84a81
Igor Minar <igor@angular.io>
2018-03-30 05:49:52 +0000

    be10bf5 fix(bazel): don't try to do flatmoduleindex under Blaze (#23083)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-439030f
+ * @license Angular v6.0.0-rc.0-be10bf5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-rc.0-439030f
+ * @license Angular v6.0.0-rc.0-be10bf5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4746,7 +4746,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-439030f');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-be10bf5');
 
 exports.BrowserModule = BrowserModule;
 exports.platformBrowser = platformBrowser;
--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-439030f
+ * @license Angular v6.0.0-rc.0-be10bf5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-439030f');
+var VERSION = new Version('6.0.0-rc.0-be10bf5');
 
 /**
  * @fileoverview added by tsickle
