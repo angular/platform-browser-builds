c91d182bb7cb5a15fd13739a2f03f1efbe049f3d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 17:55:20 +0000

    32a41bc test(ivy): fixes in the TodoMVC example (#23161)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-f48e215
+ * @license Angular v6.0.0-rc.1-32a41bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3532,7 +3532,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.1-f48e215');
+const VERSION = new Version('6.0.0-rc.1-32a41bc');
 
 /**
  * @fileoverview added by tsickle
