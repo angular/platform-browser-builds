fbffcff3080e5ca77b10750a4975363786afcde0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 16:13:19 +0000

    9703079 test(router): fix typo in expectation (#23137)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-dd61595
+ * @license Angular v5.2.9-9703079
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5346,7 +5346,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-dd61595');
+var VERSION = new Version('5.2.9-9703079');
 
 /**
  * @fileoverview added by tsickle
