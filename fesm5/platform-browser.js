cf6f34823987aa737ad0d3957b4e535e37606feb
Igor Minar <igor@angular.io>
2018-04-05 18:07:08 +0000

    fc813f6 fix(ivy): fix issue with refreshing embedded views (#23164)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-3b607e6
+ * @license Angular v6.0.0-rc.1-fc813f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-3b607e6');
+var VERSION = new Version('6.0.0-rc.1-fc813f6');
 
 /**
  * @fileoverview added by tsickle
