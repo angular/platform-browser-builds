fa7720d1eceeb72ff9558d67acf5e73511acea6d
Igor Minar <igor@angular.io>
2018-04-09 22:39:53 +0000

    bda6908 docs(core): update directives documentation (#23255)

--- esm5/platform-browser.js
+++ esm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-a1231be
+ * @license Angular v5.2.9-bda6908
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5346,7 +5346,7 @@ var By = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-a1231be');
+var VERSION = new Version('5.2.9-bda6908');
 
 /**
  * @fileoverview added by tsickle
