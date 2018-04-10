cd4d40cb0a5a22768cff7ba50b1c3478cd04d003
Igor Minar <igor@angular.io>
2018-04-10 20:38:11 +0000

    8555a3a fix(compiler): Pretty print object instead of [Object object] (#22689)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-10ecdb1
+ * @license Angular v6.0.0-rc.3-8555a3a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3532,7 +3532,7 @@ class By {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.3-10ecdb1');
+const VERSION = new Version('6.0.0-rc.3-8555a3a');
 
 /**
  * @fileoverview added by tsickle
