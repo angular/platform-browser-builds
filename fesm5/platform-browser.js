f7913cc3a5390776762436a86541297465bf4f67
Igor Minar <igor@angular.io>
2018-04-13 21:43:07 +0000

    490772e docs(aio): Update file tree to match Angular CLI V6 structure (#23367)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-e7ef027
+ * @license Angular v6.0.0-rc.4-490772e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-e7ef027');
+var VERSION = new Version('6.0.0-rc.4-490772e');
 
 /**
  * @fileoverview added by tsickle
