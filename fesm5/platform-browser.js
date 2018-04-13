64fb19eb2e9783624ab99ee0a4942b369be1db43
Igor Minar <igor@angular.io>
2018-04-13 06:44:01 +0000

    75febe7 docs(upgrade): fix detail regarding bootstrapping order (#23225) (#23270)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-993eeab
+ * @license Angular v6.0.0-rc.4-75febe7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-993eeab');
+var VERSION = new Version('6.0.0-rc.4-75febe7');
 
 /**
  * @fileoverview added by tsickle
