09e8e84d5e1f927383e397175f3c9c023d1b610d
Igor Minar <igor@angular.io>
2018-04-13 20:58:40 +0000

    d5e7f60 refactor(ivy): misc (#23351)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-6e73300
+ * @license Angular v6.0.0-rc.4-d5e7f60
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-6e73300');
+var VERSION = new Version('6.0.0-rc.4-d5e7f60');
 
 /**
  * @fileoverview added by tsickle
