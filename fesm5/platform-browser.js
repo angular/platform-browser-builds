76fe0c680297f1bae5ef97d3d4abbe3ffc7aca58
Victor Berchet <victor@suumit.com>
2018-04-19 23:56:04 +0000

    9757347 feat(ivy): add an IVY local the compiler which avoids analyzeModule (#23441)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-1d1e75e
+ * @license Angular v6.0.0-rc.5-9757347
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-1d1e75e');
+var VERSION = new Version('6.0.0-rc.5-9757347');
 
 /**
  * @fileoverview added by tsickle
