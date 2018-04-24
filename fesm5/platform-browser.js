1cb0adff0d6982baa890b9ad339438301a614e6f
Victor Berchet <victor@suumit.com>
2018-04-24 21:59:23 +0000

    f721b06 style: format code

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-0bc8443
+ * @license Angular v6.0.0-rc.5-f721b06
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-0bc8443');
+var VERSION = new Version('6.0.0-rc.5-f721b06');
 
 /**
  * @fileoverview added by tsickle
