dc546757b41dc4388ae030ad4db60780d1a899c8
Igor Minar <igor@angular.io>
2018-05-03 21:49:42 +0000

    9027818 docs(aio): Upgrade server-side rendering example to Angular V6 (#23649)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-5bd8c68
+ * @license Angular v6.0.0-9027818
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4705,7 +4705,7 @@ var By = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-5bd8c68');
+var VERSION = new Version('6.0.0-9027818');
 
 /**
  * @fileoverview added by tsickle
