a86c8b34d8d7ce1eb81a1e9f2cda696aeef68984
Igor Minar <igor@angular.io>
2018-05-08 21:16:31 +0000

    fc03427 fix(core): call ngOnDestroy on all services that have it (#23755)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+140.sha-77ff72f
+ * @license Angular v6.0.0-rc.5+141.sha-fc03427
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2601,7 +2601,7 @@ var By = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+140.sha-77ff72f');
+var VERSION = new core.Version('6.0.0-rc.5+141.sha-fc03427');
 
 /**
  * @license
