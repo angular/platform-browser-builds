6b632a80a5bbd3eef623cc26ca8f71009a01b009
Victor Berchet <victor@suumit.com>
2018-06-06 03:33:00 +0000

    22b58a7 feat(ivy): add support of ApplicationRef.bootstrapModuleFactory (#23811)

--- src/browser.d.ts
+++ src/browser.d.ts
@@ -11,13 +11,14 @@ export declare const platformBrowser: (extraProviders?: StaticProvider[]) => Pla
 export declare function initDomAdapter(): void;
 export declare function errorHandler(): ErrorHandler;
 export declare function _document(): any;
+export declare const BROWSER_MODULE_PROVIDERS: StaticProvider[];
 /**
  * The ng module for the browser.
  *
  *
  */
 export declare class BrowserModule {
-    constructor(parentModule: BrowserModule);
+    constructor(parentModule: BrowserModule | null);
     /**
      * Configures a browser-based application to transition from a server-rendered app, if
      * one is present on the page. The specified parameters must include an application id,
