6e74b13f4fcd1d3f0e8b98d2097ec6b418a724fe
Victor Berchet <victor@suumit.com>
2018-06-06 21:04:10 +0000

    7de2ba0 Revert "feat(ivy): add namespace instructions for SVG and others (#23899)"

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0
+ * @license Angular v6.1.0-beta.0+11.sha-7de2ba0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1274,7 +1274,7 @@ var DefaultDomRenderer2 = /** @class */ (function () {
     DefaultDomRenderer2.prototype.destroy = function () { };
     DefaultDomRenderer2.prototype.createElement = function (name, namespace) {
         if (namespace) {
-            return document.createElementNS(NAMESPACE_URIS[namespace] || namespace, name);
+            return document.createElementNS(NAMESPACE_URIS[namespace], name);
         }
         return document.createElement(name);
     };
@@ -1304,8 +1304,14 @@ var DefaultDomRenderer2 = /** @class */ (function () {
     DefaultDomRenderer2.prototype.nextSibling = function (node) { return node.nextSibling; };
     DefaultDomRenderer2.prototype.setAttribute = function (el, name, value, namespace) {
         if (namespace) {
-            var namespaceUri = NAMESPACE_URIS[namespace] || namespace;
-            el.setAttributeNS(namespaceUri, name, value);
+            name = namespace + ":" + name;
+            var namespaceUri = NAMESPACE_URIS[namespace];
+            if (namespaceUri) {
+                el.setAttributeNS(namespaceUri, name, value);
+            }
+            else {
+                el.setAttribute(name, value);
+            }
         }
         else {
             el.setAttribute(name, value);
@@ -1313,8 +1319,13 @@ var DefaultDomRenderer2 = /** @class */ (function () {
     };
     DefaultDomRenderer2.prototype.removeAttribute = function (el, name, namespace) {
         if (namespace) {
-            var namespaceUri = NAMESPACE_URIS[namespace] || namespace;
-            el.removeAttributeNS(namespaceUri, name);
+            var namespaceUri = NAMESPACE_URIS[namespace];
+            if (namespaceUri) {
+                el.removeAttributeNS(namespaceUri, name);
+            }
+            else {
+                el.removeAttribute(namespace + ":" + name);
+            }
         }
         else {
             el.removeAttribute(name);
@@ -2538,7 +2549,7 @@ var By = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.0');
+var VERSION = new core.Version('6.1.0-beta.0+11.sha-7de2ba0');
 
 /**
  * @license
