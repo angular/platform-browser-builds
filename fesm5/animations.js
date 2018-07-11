997af4181d9e0b2286f37d008e5fb783918e21c7
Miško Hevery <misko@hevery.com>
2018-07-11 21:38:49 +0000

    b9e095a release: cut the v6.0.8 release

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+86.sha-81a9db2
+ * @license Angular v6.1.0-beta.3+88.sha-b9e095a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
