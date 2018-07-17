8cf782ef2eab6d606c5a09938fc7919032324b45
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:21:51 +0000

    0b28732 docs: typos in directives docs (#24665)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+119.sha-06a3398
+ * @license Angular v6.1.0-beta.3+120.sha-0b28732
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __param, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __extends, __param, __metadata, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
