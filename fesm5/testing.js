6cd586b922135e60b293f231ab49bc2030e61e23
Stefanie Fluin <sjtrimble@gmail.com>
2018-07-19 03:52:54 +0000

    8dfcc78 docs(aio): api page cleanup

---
+++ fesm5/testing.js
@@ -0,0 +1,166 @@
+/**
+ * @license Angular v6.1.0-beta.3+122.sha-8dfcc78
+ * (c) 2010-2018 Google, Inc. https://angular.io/
+ * License: MIT
+ */
+
+import { NgZone, ɵglobal, PLATFORM_INITIALIZER, createPlatformFactory, platformCore, ɵdefineNgModule, defineInjector, APP_ID } from '@angular/core';
+import { ɵgetDOM, ɵBrowserDomAdapter, BrowserModule, ɵELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser';
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+var BrowserDetection = /** @class */ (function () {
+    function BrowserDetection(ua) {
+        this._overrideUa = ua;
+    }
+    Object.defineProperty(BrowserDetection.prototype, "_ua", {
+        get: function () {
+            if (typeof this._overrideUa === 'string') {
+                return this._overrideUa;
+            }
+            return ɵgetDOM() ? ɵgetDOM().getUserAgent() : '';
+        },
+        enumerable: true,
+        configurable: true
+    });
+    BrowserDetection.setup = function () { };
+    Object.defineProperty(BrowserDetection.prototype, "isFirefox", {
+        get: function () { return this._ua.indexOf('Firefox') > -1; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(BrowserDetection.prototype, "isAndroid", {
+        get: function () {
+            return this._ua.indexOf('Mozilla/5.0') > -1 && this._ua.indexOf('Android') > -1 &&
+                this._ua.indexOf('AppleWebKit') > -1 && this._ua.indexOf('Chrome') == -1 &&
+                this._ua.indexOf('IEMobile') == -1;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(BrowserDetection.prototype, "isEdge", {
+        get: function () { return this._ua.indexOf('Edge') > -1; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(BrowserDetection.prototype, "isIE", {
+        get: function () { return this._ua.indexOf('Trident') > -1; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(BrowserDetection.prototype, "isWebkit", {
+        get: function () {
+            return this._ua.indexOf('AppleWebKit') > -1 && this._ua.indexOf('Edge') == -1 &&
+                this._ua.indexOf('IEMobile') == -1;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(BrowserDetection.prototype, "isIOS7", {
+        get: function () {
+            return (this._ua.indexOf('iPhone OS 7') > -1 || this._ua.indexOf('iPad OS 7') > -1) &&
+                this._ua.indexOf('IEMobile') == -1;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(BrowserDetection.prototype, "isSlow", {
+        get: function () { return this.isAndroid || this.isIE || this.isIOS7; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(BrowserDetection.prototype, "supportsNativeIntlApi", {
+        // The Intl API is only natively supported in Chrome, Firefox, IE11 and Edge.
+        // This detector is needed in tests to make the difference between:
+        // 1) IE11/Edge: they have a native Intl API, but with some discrepancies
+        // 2) IE9/IE10: they use the polyfill, and so no discrepancies
+        get: function () {
+            return !!ɵglobal.Intl && ɵglobal.Intl !== ɵglobal.IntlPolyfill;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(BrowserDetection.prototype, "isChromeDesktop", {
+        get: function () {
+            return this._ua.indexOf('Chrome') > -1 && this._ua.indexOf('Mobile Safari') == -1 &&
+                this._ua.indexOf('Edge') == -1;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(BrowserDetection.prototype, "isOldChrome", {
+        // "Old Chrome" means Chrome 3X, where there are some discrepancies in the Intl API.
+        // Android 4.4 and 5.X have such browsers by default (respectively 30 and 39).
+        get: function () {
+            return this._ua.indexOf('Chrome') > -1 && this._ua.indexOf('Chrome/3') > -1 &&
+                this._ua.indexOf('Edge') == -1;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return BrowserDetection;
+}());
+BrowserDetection.setup();
+function createNgZone() {
+    return new NgZone({ enableLongStackTrace: true });
+}
+
+function initBrowserTests() {
+    ɵBrowserDomAdapter.makeCurrent();
+    BrowserDetection.setup();
+}
+var _TEST_BROWSER_PLATFORM_PROVIDERS = [{ provide: PLATFORM_INITIALIZER, useValue: initBrowserTests, multi: true }];
+/**
+ * Platform for testing
+ *
+ *
+ */
+var platformBrowserTesting = createPlatformFactory(platformCore, 'browserTesting', _TEST_BROWSER_PLATFORM_PROVIDERS);
+/**
+ * NgModule for testing.
+ *
+ *
+ */
+var BrowserTestingModule = /** @class */ (function () {
+    function BrowserTestingModule() {
+    }
+    BrowserTestingModule.ngModuleDef = ɵdefineNgModule({ type: BrowserTestingModule, bootstrap: [], declarations: [], imports: [], exports: [BrowserModule] });
+    BrowserTestingModule.ngInjectorDef = defineInjector({ factory: function BrowserTestingModule_Factory() { return new BrowserTestingModule(); }, providers: [
+            { provide: APP_ID, useValue: 'a' },
+            ɵELEMENT_PROBE_PROVIDERS,
+            { provide: NgZone, useFactory: createNgZone },
+        ], imports: [BrowserModule] });
+    return BrowserTestingModule;
+}());
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+export { platformBrowserTesting, BrowserTestingModule };
+//# sourceMappingURL=testing.js.map
