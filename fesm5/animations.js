6465168e5ae64c98d442d2d27fd7e55822cd79a2
Victor Berchet <victor@suumit.com>
2018-07-23 19:07:32 +0000

    4d840cb docs: technical review incorporated (#24744)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+34.sha-ac7658e
+ * @license Angular v6.0.9+38.sha-4d840cb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
