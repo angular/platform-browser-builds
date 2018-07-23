e4bd52961e16741a5deb5bb74aef9c9f849e8bba
Victor Berchet <victor@suumit.com>
2018-07-23 17:27:32 +0000

    168c2a6 docs: add Truly-UI to resources (#24615) PR Close #24615

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+49.sha-bd576bb
+ * @license Angular v6.1.0-rc.3+48.sha-168c2a6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __param, __metadata, __spread, __read } from 'tslib';
+import { __extends, __decorate, __param, __metadata, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
