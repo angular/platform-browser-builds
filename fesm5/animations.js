3453015e632d8bf081198d72008009a6d33b0805
Victor Berchet <victor@suumit.com>
2018-07-25 06:04:07 +0000

    d4ac969 Revert "docs: refactor style guide example 03-06 (#24996)"

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+69.sha-c205516
+ * @license Angular v6.1.0-rc.3+70.sha-d4ac969
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __read } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
