90aa42cbff7d4df93dd432f278459015ebea2cec
Victor Berchet <victor@suumit.com>
2018-07-25 18:20:16 +0000

    0560751 fix(core): stop reusing provider definitions across NgModuleRef instances (#25088)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+52.sha-e32c1b6
+ * @license Angular v6.0.9+53.sha-0560751
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
