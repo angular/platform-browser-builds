e5b1cf9a290c4c59f18b98567388b0e30d21e6f3
Igor Minar <igor@angular.io>
2018-07-27 20:30:06 +0000

    3d81e06 refactor(ivy): do not deep import from ngtsc into ngcc

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0+28.sha-dad1d3d
+ * @license Angular v6.1.0+34.sha-3d81e06
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { __extends, __spread, __read } from 'tslib';
-import { ɵdefineNgModule, defineInjector, defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ViewEncapsulation } from '@angular/core';
+import { defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ɵdefineNgModule, defineInjector, ViewEncapsulation } from '@angular/core';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
 import { ɵAnimationEngine, AnimationDriver, ɵAnimationStyleNormalizer, ɵCssKeyframesDriver, ɵNoopAnimationDriver, ɵWebAnimationsDriver, ɵWebAnimationsStyleNormalizer, ɵsupportsWebAnimations } from '@angular/animations/browser';
