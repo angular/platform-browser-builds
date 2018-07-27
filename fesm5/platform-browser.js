b39bc220dfc69c220145f3797600f15e11402730
Igor Minar <igor@angular.io>
2018-07-27 00:44:25 +0000

    f2bf828 build(bazel): add comment about angular bazel rules API re-export from /index.bzl (#24663)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0
+ * @license Angular v6.1.0+5.sha-f2bf828
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread } from 'tslib';
+import { __assign, __extends, __spread } from 'tslib';
 import { ɵparseCookieValue, DOCUMENT, PlatformLocation, isPlatformServer, CommonModule, ɵPLATFORM_BROWSER_ID } from '@angular/common';
 import { ɵglobal, Inject, Injectable, APP_INITIALIZER, ApplicationInitStatus, InjectionToken, Injector, setTestabilityGetter, inject, APP_ID, NgModule, NgZone, Version, ApplicationRef, PLATFORM_ID, PLATFORM_INITIALIZER, Sanitizer, createPlatformFactory, platformCore, ErrorHandler, ɵAPP_ROOT, ɵConsole, Optional, RendererFactory2, Testability, ApplicationModule, SkipSelf, defineInjectable, getDebugNode, NgProbeToken, ViewEncapsulation, RendererStyleFlags2, SecurityContext, ɵ_sanitizeHtml, ɵ_sanitizeStyle, ɵ_sanitizeUrl } from '@angular/core';
 
@@ -2480,7 +2480,7 @@ var By = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0');
+var VERSION = new Version('6.1.0+5.sha-f2bf828');
 
 /**
  * @license
