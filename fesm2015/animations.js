03ca93cf476418eabf26c491bfc41518d0437566
Igor Minar <igor@angular.io>
2018-08-01 20:09:39 +0000

    a83b9f7 docs(changelog): remove reverted feature entry (#25206)

--- fesm2015/animations.js
+++ fesm2015/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+22.sha-d4bf2da
+ * @license Angular v6.1.0+26.sha-a83b9f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ɵdefineNgModule, defineInjector, ViewEncapsulation } from '@angular/core';
+import { ɵdefineNgModule, defineInjector, defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ViewEncapsulation } from '@angular/core';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
 import { ɵAnimationEngine, AnimationDriver, ɵAnimationStyleNormalizer, ɵCssKeyframesDriver, ɵNoopAnimationDriver, ɵWebAnimationsDriver, ɵWebAnimationsStyleNormalizer, ɵsupportsWebAnimations } from '@angular/animations/browser';
