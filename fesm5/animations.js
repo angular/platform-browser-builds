c1e9130146dcf23e5efe550c7a47fa7fbd1951e1
Igor Minar <igor@angular.io>
2018-08-02 00:05:20 +0000

    5ca35b3 docs: Update the link to the Jasmine docs (#25175)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+42.sha-0a6a3f3
+ * @license Angular v6.1.0+43.sha-5ca35b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __read } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
