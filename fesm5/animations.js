1882783e72f73e85cd7136b51aebe0eba30e29cb
Igor Minar <igor@angular.io>
2018-08-02 00:11:32 +0000

    3d7f555 ci: update pullapprove groups and add docs (#25257)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+85.sha-06af794
+ * @license Angular v6.1.0+86.sha-3d7f555
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __read } from 'tslib';
+import { __read, __extends, __spread } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
