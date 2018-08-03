a8c183039e12f2843066cf6fcc6ee1f70921032a
Kara Erickson <karakara@google.com>
2018-08-03 07:10:30 +0000

    b38931b fix(ivy): use devModeEqual in no change mode (#25252)

--- fesm2015/animations.js
+++ fesm2015/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.0+8.sha-728d98d
+ * @license Angular v7.0.0-beta.0+5.sha-b38931b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ɵdefineNgModule, defineInjector, ViewEncapsulation } from '@angular/core';
+import { ɵdefineNgModule, defineInjector, defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ViewEncapsulation } from '@angular/core';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
 import { ɵAnimationEngine, AnimationDriver, ɵAnimationStyleNormalizer, ɵCssKeyframesDriver, ɵNoopAnimationDriver, ɵWebAnimationsDriver, ɵWebAnimationsStyleNormalizer, ɵsupportsWebAnimations } from '@angular/animations/browser';
