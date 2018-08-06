0ddce14c473cd17038d2152460bbe7a66073e891
Kara Erickson <karakara@google.com>
2018-08-06 22:25:42 +0000

    a54c049 docs: replace npm with yarn in lockfile readme (#25309)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+11.sha-88f01f5
+ * @license Angular v6.1.1+13.sha-a54c049
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __param, __metadata, __spread, __read } from 'tslib';
+import { __extends, __decorate, __param, __metadata, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
