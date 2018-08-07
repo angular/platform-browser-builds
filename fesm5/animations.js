b3c48562131a5e547eba12add86b877b24c15a93
Kara Erickson <karakara@google.com>
2018-08-07 18:58:28 +0000

    054fbbe fix: add mappings for ngfactory & ngsummary files to their module names in aot summary resolver (#25335)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+24.sha-94a2ac7
+ * @license Angular v6.1.1+26.sha-054fbbe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
