819e6ac2ca86c9cbe552c1e83b5d168c54aa8d69
Kara Erickson <karakara@google.com>
2018-08-08 21:54:58 +0000

    638ff76 docs: add ngrx book to the docs (#23389)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+7.sha-a3f1e2c
+ * @license Angular v7.0.0-beta.1+4.sha-638ff76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __param, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __extends, __param, __metadata, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
