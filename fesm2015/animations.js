b18ba851fd792f4f93081ad5fd3c4326ef9ef350
Ben Lesh <ben@benlesh.com>
2018-08-14 05:40:22 +0000

    151e4b9 docs: add link to Yarn in README (#24856)

--- fesm2015/animations.js
+++ fesm2015/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.2+9.sha-cb05f9b
+ * @license Angular v6.1.2+11.sha-151e4b9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { ɵdefineNgModule, defineInjector, defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ViewEncapsulation } from '@angular/core';
+import { defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ɵdefineNgModule, defineInjector, ViewEncapsulation } from '@angular/core';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
 import { ɵAnimationEngine, AnimationDriver, ɵAnimationStyleNormalizer, ɵCssKeyframesDriver, ɵNoopAnimationDriver, ɵWebAnimationsDriver, ɵWebAnimationsStyleNormalizer, ɵsupportsWebAnimations } from '@angular/animations/browser';
