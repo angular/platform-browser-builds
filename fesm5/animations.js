54e2bde9e0f57d7b9901a372b319f48e49bfc98c
Ben Lesh <ben@benlesh.com>
2018-08-15 01:03:42 +0000

    be2cf4d docs(core): Correct spelling error in directives docs (#25377)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+47.sha-a0a29fd
+ * @license Angular v7.0.0-beta.1+50.sha-be2cf4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __param, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __extends, __param, __metadata, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
