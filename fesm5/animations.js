67f93baaa883556818b9487f1842f1f7bf3108d3
Jason Aden <jasonaden@google.com>
2018-08-16 22:17:41 +0000

    cc55d60 docs: add HttpClientModule import code to services tutorial (#24854)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v7.0.0-beta.2+15.sha-6482f6f
+ * @license Angular v7.0.0-beta.2+17.sha-cc55d60
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { __extends, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
-import { RendererFactory2, ViewEncapsulation, ɵdefineNgModule, defineInjector, defineInjectable, inject, InjectionToken, NgZone } from '@angular/core';
+import { RendererFactory2, ViewEncapsulation, defineInjectable, inject, InjectionToken, NgZone, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
 import { ɵAnimationEngine, AnimationDriver, ɵAnimationStyleNormalizer, ɵCssKeyframesDriver, ɵNoopAnimationDriver, ɵWebAnimationsDriver, ɵWebAnimationsStyleNormalizer, ɵsupportsWebAnimations } from '@angular/animations/browser';
 import { DOCUMENT as DOCUMENT$1 } from '@angular/common';
