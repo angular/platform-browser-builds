a86e8ab44be8018b084a25172a6b42cb8346aee1
Matias Niemelä <matias@yearofmoo.com>
2018-08-29 22:19:09 +0000

    f394ba0 fix(bazel): Cache fileNameToModuleName lookups (#25731)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.3+75.sha-268cf79
+ * @license Angular v7.0.0-beta.3+76.sha-f394ba0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __param, __metadata, __spread, __read } from 'tslib';
+import { __extends, __decorate, __param, __metadata, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
