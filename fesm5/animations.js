7995712c124024837a022e43c2284ebe314d1b08
Miško Hevery <misko@hevery.com>
2018-08-31 18:58:16 +0000

    88f7ddb build: fix bad merge onto patch branch (#25729)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.6+2.sha-ed6b68b
+ * @license Angular v6.1.6+8.sha-88f7ddb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { __extends, __spread, __read } from 'tslib';
-import { ɵdefineNgModule, defineInjector, defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ViewEncapsulation } from '@angular/core';
+import { defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ɵdefineNgModule, defineInjector, ViewEncapsulation } from '@angular/core';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
 import { ɵAnimationEngine, AnimationDriver, ɵAnimationStyleNormalizer, ɵCssKeyframesDriver, ɵNoopAnimationDriver, ɵWebAnimationsDriver, ɵWebAnimationsStyleNormalizer, ɵsupportsWebAnimations } from '@angular/animations/browser';
