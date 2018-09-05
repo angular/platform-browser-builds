b943b911f44215edb0c14ae1edc5cc2eee7507ba
Misko Hevery <misko@hevery.com>
2018-09-05 20:32:53 +0000

    82e0676 fix(upgrade): trigger `$destroy` event on upgraded component element (#25357)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,10 +1,9 @@
 /**
- * @license Angular v6.1.6+27.sha-3a0b735
+ * @license Angular v6.1.6+22.sha-82e0676
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import 'tslib';
 import { NgZone, ɵglobal, APP_ID, PLATFORM_INITIALIZER, createPlatformFactory, platformCore, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { ɵgetDOM, BrowserModule, ɵBrowserDomAdapter, ɵELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser';
 
