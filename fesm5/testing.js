fa934be62d0801b3c21c1db7cb24b4a047ec92aa
Miško Hevery <misko@hevery.com>
2018-09-06 02:31:57 +0000

    d1063c6 build(docs-infra): restore correct dependency versions in `package.json` (#25827)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v6.1.6+25.sha-2c1f550
+ * @license Angular v6.1.6+28.sha-d1063c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
+import { __decorate } from 'tslib';
 import { NgZone, ɵglobal, APP_ID, NgModule, PLATFORM_INITIALIZER, createPlatformFactory, platformCore } from '@angular/core';
 import { ɵgetDOM, BrowserModule, ɵBrowserDomAdapter, ɵELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser';
-import { __decorate } from 'tslib';
 
 /**
  * @license
