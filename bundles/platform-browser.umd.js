cb479c859085defa4058bbf168dc565247ecbcb3
Kara Erickson <karakara@google.com>
2018-09-24 16:41:17 +0000

    b8422b4 build(docs-infra): fail doc-gen if a content rule fails (#26039)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+37.sha-7f1cace
+ * @license Angular v7.0.0-beta.6+51.sha-b8422b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2487,7 +2487,8 @@
         /**
          * Match all elements.
          *
-         * ## Example
+         * @usageNotes
+         * ### Example
          *
          * {@example platform-browser/dom/debug/ts/by/by.ts region='by_all'}
          */
@@ -2495,7 +2496,8 @@
         /**
          * Match elements by the given CSS selector.
          *
-         * ## Example
+         * @usageNotes
+         * ### Example
          *
          * {@example platform-browser/dom/debug/ts/by/by.ts region='by_css'}
          */
@@ -2509,7 +2511,8 @@
         /**
          * Match elements that have the given directive present.
          *
-         * ## Example
+         * @usageNotes
+         * ### Example
          *
          * {@example platform-browser/dom/debug/ts/by/by.ts region='by_directive'}
          */
@@ -2534,7 +2537,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.6+37.sha-7f1cace');
+    var VERSION = new core.Version('7.0.0-beta.6+51.sha-b8422b4');
 
     /**
      * @license
