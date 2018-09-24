d107fda65496aa426ffe4fa9ebc1e5c94263b4f6
Kara Erickson <karakara@google.com>
2018-09-24 19:13:09 +0000

    82a14dc feat(ivy): provide groundwork for animations in core (#25234)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+25.sha-64aa670
+ * @license Angular v7.0.0-beta.6+35.sha-82a14dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3518,7 +3518,7 @@ class By {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+25.sha-64aa670');
+const VERSION = new Version('7.0.0-beta.6+35.sha-82a14dc');
 
 /**
  * @fileoverview added by tsickle
