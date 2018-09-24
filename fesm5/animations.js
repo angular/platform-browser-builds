460d34844c953ee099259727c059303b8ed58669
Kara Erickson <karakara@google.com>
2018-09-24 16:57:33 +0000

    47eb212 docs: fix Sajee info in contributors (#26063)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v7.0.0-beta.6+51.sha-b8422b4
+ * @license Angular v7.0.0-beta.6+52.sha-47eb212
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __extends, __spread } from 'tslib';
+import { __extends, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
-import { RendererFactory2, ViewEncapsulation, ɵdefineNgModule, defineInjector, defineInjectable, inject, NgZone, InjectionToken } from '@angular/core';
+import { RendererFactory2, ViewEncapsulation, ɵdefineNgModule, defineInjector, defineInjectable, inject, InjectionToken, NgZone } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
 import { ɵAnimationEngine, AnimationDriver, ɵAnimationStyleNormalizer, ɵCssKeyframesDriver, ɵNoopAnimationDriver, ɵWebAnimationsDriver, ɵWebAnimationsStyleNormalizer, ɵsupportsWebAnimations } from '@angular/animations/browser';
 import { DOCUMENT as DOCUMENT$1 } from '@angular/common';
