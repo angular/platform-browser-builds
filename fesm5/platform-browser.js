935ede7374f0f7488e7f345c0bc9f6185ed5e51e
Kara Erickson <karakara@google.com>
2018-09-25 18:45:45 +0000

    0c34471 feat(ivy): expose a series of helpful application inspection tools (#25919)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+35.sha-82a14dc
+ * @license Angular v7.0.0-beta.6+58.sha-0c34471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2318,7 +2318,8 @@ var By = /** @class */ (function () {
     /**
      * Match all elements.
      *
-     * ## Example
+     * @usageNotes
+     * ### Example
      *
      * {@example platform-browser/dom/debug/ts/by/by.ts region='by_all'}
      */
@@ -2326,7 +2327,8 @@ var By = /** @class */ (function () {
     /**
      * Match elements by the given CSS selector.
      *
-     * ## Example
+     * @usageNotes
+     * ### Example
      *
      * {@example platform-browser/dom/debug/ts/by/by.ts region='by_css'}
      */
@@ -2340,7 +2342,8 @@ var By = /** @class */ (function () {
     /**
      * Match elements that have the given directive present.
      *
-     * ## Example
+     * @usageNotes
+     * ### Example
      *
      * {@example platform-browser/dom/debug/ts/by/by.ts region='by_directive'}
      */
@@ -2365,7 +2368,7 @@ var By = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.6+35.sha-82a14dc');
+var VERSION = new Version('7.0.0-beta.6+58.sha-0c34471');
 
 /**
  * @license
