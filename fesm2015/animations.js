740a266941dbf2679b8a571747073ee24c9b274a
Jason Aden <jasonaden@google.com>
2018-10-05 23:22:59 +0000

    3886bfa docs: Rename 'QuickStart' into 'Getting Started' (#25762)

--- fesm2015/animations.js
+++ fesm2015/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+64.sha-2fe401d
+ * @license Angular v6.1.9+68.sha-3886bfa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { NgModule, Injectable, Inject, defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ɵdefineNgModule, defineInjector, ViewEncapsulation } from '@angular/core';
+import { NgModule, ɵdefineNgModule, defineInjector, Injectable, Inject, defineInjectable, inject, InjectionToken, RendererFactory2, NgZone, ViewEncapsulation } from '@angular/core';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
 import { ɵAnimationEngine, AnimationDriver, ɵAnimationStyleNormalizer, ɵCssKeyframesDriver, ɵNoopAnimationDriver, ɵWebAnimationsDriver, ɵWebAnimationsStyleNormalizer, ɵsupportsWebAnimations } from '@angular/animations/browser';
