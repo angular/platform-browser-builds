7d50574c15c0cf746513fd9b19ece64311eea71e
Jason Aden <jasonaden@google.com>
2018-10-08 21:16:29 +0000

    8a6f372 build(docs-infra): upgrade Angular to 7.0.0-rc.0 and TypeScript to 3.1.1 (#26306)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+73.sha-ed04e99
+ * @license Angular v6.1.9+78.sha-8a6f372
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2364,7 +2364,7 @@ class By {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.9+73.sha-ed04e99');
+const VERSION = new Version('6.1.9+78.sha-8a6f372');
 
 /**
  * @license
