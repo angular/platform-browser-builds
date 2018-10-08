ee6f61af60df5dcce4a9d3fd41cde6d9659cbf7d
Jason Aden <jasonaden@google.com>
2018-10-08 19:31:22 +0000

    5a31bde fix(upgrade): properly destroy upgraded component elements and descendants (#26209)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.0+63.sha-55d54c7
+ * @license Angular v7.0.0-rc.0+65.sha-5a31bde
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __read, __decorate, __metadata, __extends, __param, __spread } from 'tslib';
+import { __extends, __decorate, __param, __metadata, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
