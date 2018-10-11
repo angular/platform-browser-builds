9e8d6cf72a1cf0c207d665145ff98a2c6ca1a029
Miško Hevery <misko@hevery.com>
2018-10-11 21:50:49 +0000

    0a3f817 refactor(platform-browser): remove type assertion on console.profileEnd (#26303)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+18.sha-bd186c7
+ * @license Angular v7.0.0-rc.1+22.sha-0a3f817
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2279,10 +2279,6 @@
             }
             var end = getDOM().performanceNow();
             if (record && isProfilerAvailable) {
-                // need to cast to <any> because type checker thinks there's no argument
-                // while in fact there is:
-                //
-                // https://developer.mozilla.org/en-US/docs/Web/API/Console/profileEnd
                 win.console.profileEnd(profileName);
             }
             var msPerTick = (end - start) / numTicks;
@@ -2543,7 +2539,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-rc.1+18.sha-bd186c7');
+    var VERSION = new core.Version('7.0.0-rc.1+22.sha-0a3f817');
 
     /**
      * @license
