237220f9ac7955d71938fed870504f2bd7ff718d
Miško Hevery <misko@hevery.com>
2018-10-11 21:48:59 +0000

    1e69d60 fix(ivy): ensure that `ɵNgModuleDefWithMeta` is exported (#26082)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+6.sha-7d0e175
+ * @license Angular v7.0.0-rc.1+16.sha-1e69d60
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __param, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __extends, __param, __metadata, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
