684a0e68f70abbd32ca8bd96013ff4a091bef5ad
Misko Hevery <misko@hevery.com>
2018-10-17 04:10:26 +0000

    34aff0b docs(forms): change headings (#25900)

--- fesm2015/animations.js
+++ fesm2015/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+54.sha-7ac4b76
+ * @license Angular v7.0.0-rc.1+58.sha-34aff0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata } from 'tslib';
+import { __decorate, __metadata, __param } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
