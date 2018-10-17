5e09b5b7cf68ebb63f0bdc336c893c981bb9f880
Misko Hevery <misko@hevery.com>
2018-10-17 19:27:56 +0000

    65b2093 build(docs-infra): update git ref for cli command docs (#26497)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+75.sha-beebf7f
+ * @license Angular v7.0.0-rc.1+84.sha-65b2093
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __extends, __param, __metadata, __spread, __read } from 'tslib';
+import { __decorate, __read, __metadata, __extends, __param, __spread } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
 import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgZone, InjectionToken, NgModule } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
