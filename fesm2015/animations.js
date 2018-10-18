074c6dadd67ff3c8639bd83ac5422a43de0079ec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:47:08 +0000

    d0a1e42 ci: add forms-overview guide to forms approval list (#26549)

--- fesm2015/animations.js
+++ fesm2015/animations.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0+2.sha-32c018c
+ * @license Angular v7.0.0+6.sha-d0a1e42
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
-import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgModule, defineInjectable, inject, InjectionToken, NgZone, ɵdefineNgModule, defineInjector } from '@angular/core';
+import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgModule, ɵdefineNgModule, defineInjector, defineInjectable, inject, InjectionToken, NgZone } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
 import { ɵAnimationEngine, AnimationDriver, ɵAnimationStyleNormalizer, ɵCssKeyframesDriver, ɵNoopAnimationDriver, ɵWebAnimationsDriver, ɵWebAnimationsStyleNormalizer, ɵsupportsWebAnimations } from '@angular/animations/browser';
 import { DOCUMENT as DOCUMENT$1 } from '@angular/common';
