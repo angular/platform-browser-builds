ed0b3afd0f96e14be07ccb788ab450622083aaa6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-23 21:59:01 +0000

    929739c Revert "feat(router): add prioritizedGuardValue operator optimization and allowing UrlTree return from guard (#26478)" (#26700)

--- fesm2015/animations.js
+++ fesm2015/animations.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0+53.sha-c8adef2
+ * @license Angular v7.0.0+67.sha-929739c.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
-import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgModule, defineInjectable, inject, InjectionToken, NgZone, ɵdefineNgModule, defineInjector } from '@angular/core';
+import { Inject, Injectable, RendererFactory2, ViewEncapsulation, NgModule, ɵdefineNgModule, defineInjector, defineInjectable, inject, InjectionToken, NgZone } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
 import { ɵAnimationEngine, AnimationDriver, ɵAnimationStyleNormalizer, ɵCssKeyframesDriver, ɵNoopAnimationDriver, ɵWebAnimationsDriver, ɵWebAnimationsStyleNormalizer, ɵsupportsWebAnimations } from '@angular/animations/browser';
 import { DOCUMENT as DOCUMENT$1 } from '@angular/common';
