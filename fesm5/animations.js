9fb85ff42f4b78cc7dfba0d9a10ef9cdc90ac3e8
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 20:56:09 +0000

    634e9e9 docs: Removing duplicate lines (#26754)

--- fesm5/animations.js
+++ fesm5/animations.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v7.1.0-beta.0+70.sha-87a7f2d
+ * @license Angular v7.1.0-beta.0+72.sha-634e9e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { __extends, __spread, __read } from 'tslib';
 import { AnimationBuilder, AnimationFactory, sequence } from '@angular/animations';
-import { RendererFactory2, ViewEncapsulation, ɵdefineNgModule, defineInjector, defineInjectable, inject, InjectionToken, NgZone } from '@angular/core';
+import { RendererFactory2, ViewEncapsulation, defineInjectable, inject, InjectionToken, NgZone, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { DOCUMENT, ɵDomRendererFactory2, BrowserModule } from '@angular/platform-browser';
 import { ɵAnimationEngine, AnimationDriver, ɵAnimationStyleNormalizer, ɵCssKeyframesDriver, ɵNoopAnimationDriver, ɵWebAnimationsDriver, ɵWebAnimationsStyleNormalizer, ɵsupportsWebAnimations } from '@angular/animations/browser';
 import { DOCUMENT as DOCUMENT$1 } from '@angular/common';
