d89a5507621d95f58d56268b7c88215487c3e5f6
Misko Hevery <misko@hevery.com>
2018-11-16 20:39:37 +0000

    597d14f test(docs-infra): remove reflect-metadata polyfill from aio tests (#27130)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.4+1.sha-6ae8a3e
+ * @license Angular v7.0.4+2.sha-597d14f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2472,7 +2472,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.4+1.sha-6ae8a3e');
+var VERSION = new Version('7.0.4+2.sha-597d14f');
 
 /**
  * @license
