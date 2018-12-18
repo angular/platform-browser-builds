15b9e27131e700ec6bf28db58e9a4a20b9683b14
Miško Hevery <misko@hevery.com>
2018-12-18 01:16:10 +0000

    85866de fix(bazel): Set module_name and enable ng test (#27715)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+66.sha-c986d3d
+ * @license Angular v7.2.0-beta.2+67.sha-85866de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2472,7 +2472,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+66.sha-c986d3d');
+var VERSION = new Version('7.2.0-beta.2+67.sha-85866de');
 
 /**
  * @license
