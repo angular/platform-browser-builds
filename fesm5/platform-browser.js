2a01c447ddfdcd1bdc8245caafc2f29471c6b42a
Andrew Kushnir <akushnir@google.com>
2019-01-09 21:02:39 +0000

    91a8a4f docs(forms): remove duplicated link (#27884) PR Close #27884

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+72.sha-582395b
+ * @license Angular v7.2.0+73.sha-91a8a4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2472,7 +2472,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+72.sha-582395b');
+var VERSION = new Version('7.2.0+73.sha-91a8a4f');
 
 /**
  * @license
