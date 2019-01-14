8a9e57e62cba21ade8963fcd413b2e1a7263e775
Andrew Kushnir <akushnir@google.com>
2019-01-14 22:46:55 +0000

    f1fb62d fix(core): proper @internal and @nocollapse combined usage fix (#28138)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+169.sha-d12db4e
+ * @license Angular v7.2.0+170.sha-f1fb62d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2472,7 +2472,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+169.sha-d12db4e');
+var VERSION = new Version('7.2.0+170.sha-f1fb62d');
 
 /**
  * @license
