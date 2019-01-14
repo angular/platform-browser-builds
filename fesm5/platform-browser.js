b26b4212764aa2f14d9ca6ab9fef6aeac984331b
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:20:00 +0000

    6fff74e test(ivy): re-enable passing tests and misc cleanup (#28093)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+143.sha-da2880d
+ * @license Angular v7.2.0+146.sha-6fff74e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2472,7 +2472,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+143.sha-da2880d');
+var VERSION = new Version('7.2.0+146.sha-6fff74e');
 
 /**
  * @license
