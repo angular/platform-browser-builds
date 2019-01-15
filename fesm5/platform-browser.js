a09fb4f30c12ec936ce3b6ee94dc23a322cf8092
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:03:53 +0000

    6930451 refactor(ivy): remove def.attributes in favor of the `elementHostAttrs` instruction (#28089)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+178.sha-e62eeed
+ * @license Angular v7.2.0+179.sha-6930451
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2472,7 +2472,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+178.sha-e62eeed');
+var VERSION = new Version('7.2.0+179.sha-6930451');
 
 /**
  * @license
