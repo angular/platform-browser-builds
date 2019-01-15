9051a2b3c86fd36a8bca769edbf685d9c629a910
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:21:35 +0000

    cfb4145 docs: update testing doc example style for HostListener (#26372)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+170.sha-f1fb62d
+ * @license Angular v7.2.0+171.sha-cfb4145
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2472,7 +2472,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+170.sha-f1fb62d');
+var VERSION = new Version('7.2.0+171.sha-cfb4145');
 
 /**
  * @license
