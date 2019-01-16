4ca6ff8e93f90a9c37053dd99ad1913c73d9524c
Andrew Kushnir <akushnir@google.com>
2019-01-16 21:57:47 +0000

    1e5e62e docs: release notes for the v7.2.1 release

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+198.sha-bac71ef
+ * @license Angular v7.2.0+199.sha-1e5e62e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2472,7 +2472,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+198.sha-bac71ef');
+var VERSION = new Version('7.2.0+199.sha-1e5e62e');
 
 /**
  * @license
