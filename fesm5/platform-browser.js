f29ec66528909fdfd0a5e64dd6962797801927b3
Miško Hevery <misko@hevery.com>
2019-02-12 18:48:35 +0000

    2bf526b docs: add a README for third_party usage (#28651)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+46.sha-8bd7d5b
+ * @license Angular v7.2.4+47.sha-2bf526b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2472,7 +2472,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+46.sha-8bd7d5b');
+var VERSION = new Version('7.2.4+47.sha-2bf526b');
 
 /**
  * @license
