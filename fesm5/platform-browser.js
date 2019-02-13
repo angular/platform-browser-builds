d887b3e9a4bf351ad9a1e61e1df03119db5f327b
Miško Hevery <misko@hevery.com>
2019-02-13 04:53:17 +0000

    73f9db5 ci: downgrade to sauce-connect v4.5.1 (#28659)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+47.sha-2bf526b
+ * @license Angular v7.2.4+48.sha-73f9db5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2472,7 +2472,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+47.sha-2bf526b');
+var VERSION = new Version('7.2.4+48.sha-73f9db5');
 
 /**
  * @license
