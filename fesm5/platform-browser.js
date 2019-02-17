649602399f6bf64459d4a0460e8808dce11f52fe
Misko Hevery <misko@hevery.com>
2019-02-17 05:18:49 +0000

    bf25165 build(bazel): specify tsconfig-test dependency (#28696)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+83.sha-a2ba444
+ * @license Angular v7.2.5+1.sha-bf25165
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2472,7 +2472,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+83.sha-a2ba444');
+var VERSION = new Version('7.2.5+1.sha-bf25165');
 
 /**
  * @license
