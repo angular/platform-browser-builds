28eecc4311fb3d9281aa746b2f33af6a185c3dbb
Igor Minar <igor@angular.io>
2019-02-19 21:24:10 +0000

    5e68e35 docs(platform-browser): fix a grammatical error in the BrowserModule documentation (#28808)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+26.sha-745c9c5
+ * @license Angular v8.0.0-beta.4+27.sha-5e68e35
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2053,7 +2053,7 @@
     ];
     /**
      * Exports required infrastructure for all Angular apps.
-     * Included by defaults in all Angular apps created with the CLI
+     * Included by default in all Angular apps created with the CLI
      * `new` command.
      * Re-exports `CommonModule` and `ApplicationModule`, making their
      * exports and providers available to all apps.
@@ -2604,7 +2604,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.4+26.sha-745c9c5');
+    var VERSION = new i0.Version('8.0.0-beta.4+27.sha-5e68e35');
 
     /**
      * @license
--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+26.sha-745c9c5
+ * @license Angular v8.0.0-beta.4+27.sha-5e68e35
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1988,7 +1988,7 @@ var BROWSER_MODULE_PROVIDERS = [
 ];
 /**
  * Exports required infrastructure for all Angular apps.
- * Included by defaults in all Angular apps created with the CLI
+ * Included by default in all Angular apps created with the CLI
  * `new` command.
  * Re-exports `CommonModule` and `ApplicationModule`, making their
  * exports and providers available to all apps.
@@ -2539,7 +2539,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+26.sha-745c9c5');
+var VERSION = new Version('8.0.0-beta.4+27.sha-5e68e35');
 
 /**
  * @license
