b5c5316a74bf35c4dd6dccc4f1fcca6449bef95f
Kara Erickson <karakara@google.com>
2019-03-06 23:48:15 +0000

    79fb9d4 build(docs-infra): use pinned dependencies when possible in `ng-packages-installer` (#28510)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+20.sha-36cbfb1
+ * @license Angular v6.1.10+26.sha-79fb9d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2364,7 +2364,7 @@ class By {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.10+20.sha-36cbfb1');
+const VERSION = new Version('6.1.10+26.sha-79fb9d4');
 
 /**
  * @license
