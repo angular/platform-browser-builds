2c0fdc51687c8df2ca071afa78be381289fc1c79
Andrew Kushnir <akushnir@google.com>
2019-03-06 01:05:48 +0000

    84f3dfb test(service-worker): verify that `config/schema.json` is published to npm (#27859)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+86.sha-881807d.with-local-changes
+ * @license Angular v8.0.0-beta.6+88.sha-84f3dfb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2481,7 +2481,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+86.sha-881807d.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+88.sha-84f3dfb.with-local-changes');
 
 /**
  * @license
