cd05dadede816b8d0001100dcfe0d30ae3c4c32c
Kara Erickson <karakara@google.com>
2019-03-08 03:10:54 +0000

    014841d test(ivy): fix failing view ref test (#29178)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+39.sha-63d1806.with-local-changes
+ * @license Angular v8.0.0-beta.7+40.sha-014841d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2481,7 +2481,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+39.sha-63d1806.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+40.sha-014841d.with-local-changes');
 
 /**
  * @license
