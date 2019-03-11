75ae8534ec43c37052c91ddfe556f848d33418b0
Kara Erickson <karakara@google.com>
2019-03-11 22:54:39 +0000

    ed4675e docs: update Universal guide (#28956)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+61.sha-146256a.with-local-changes
+ * @license Angular v8.0.0-beta.7+63.sha-ed4675e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2481,7 +2481,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+61.sha-146256a.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+63.sha-ed4675e.with-local-changes');
 
 /**
  * @license
