491c9b919ae40500941ac3b87387d875a39f76e6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 20:50:27 +0000

    86a3f90 fix(compiler-cli): pass config path to ts.parseJsonConfigFileContent (#29872)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+26.sha-1794a8e.with-local-changes
+ * @license Angular v8.0.0-beta.12+25.sha-86a3f90.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4005,7 +4005,7 @@ const DOCUMENT = DOCUMENT$1;
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.12+26.sha-1794a8e.with-local-changes');
+const VERSION = new Version('8.0.0-beta.12+25.sha-86a3f90.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
