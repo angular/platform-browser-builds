26e44f39ed6b88e7a0e0151091eaa45a3c4f5320
Ben Lesh <ben@benlesh.com>
2019-04-24 21:03:51 +0000

    071ee64 refactor(common): rename LocationUpgradeService and remove angular/upgrade dependency (#30055)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+19.sha-3938563.with-local-changes
+ * @license Angular v8.0.0-beta.14+31.sha-071ee64.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -646,6 +646,26 @@
         BrowserPlatformLocation.prototype.onHashChange = function (fn) {
             getDOM().getGlobalEventTarget(this._doc, 'window').addEventListener('hashchange', fn, false);
         };
+        Object.defineProperty(BrowserPlatformLocation.prototype, "href", {
+            get: function () { return this.location.href; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(BrowserPlatformLocation.prototype, "protocol", {
+            get: function () { return this.location.protocol; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(BrowserPlatformLocation.prototype, "hostname", {
+            get: function () { return this.location.hostname; },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(BrowserPlatformLocation.prototype, "port", {
+            get: function () { return this.location.port; },
+            enumerable: true,
+            configurable: true
+        });
         Object.defineProperty(BrowserPlatformLocation.prototype, "pathname", {
             get: function () { return this.location.pathname; },
             set: function (newPath) { this.location.pathname = newPath; },
@@ -680,6 +700,7 @@
         };
         BrowserPlatformLocation.prototype.forward = function () { this._history.forward(); };
         BrowserPlatformLocation.prototype.back = function () { this._history.back(); };
+        BrowserPlatformLocation.prototype.getState = function () { return this._history.state; };
         BrowserPlatformLocation = __decorate([
             core.Injectable(),
             __param(0, core.Inject(common.DOCUMENT)),
@@ -2563,7 +2584,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.14+19.sha-3938563.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.14+31.sha-071ee64.with-local-changes');
 
     /**
      * @license
