e7208720e9ed570e4d45775add150a509e53697d
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:37:21 +0000

    f3ce8ee fix(ivy): property bindings use correct indices (#30129)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+9.sha-876ceb3.with-local-changes
+ * @license Angular v8.0.0-rc.0+10.sha-f3ce8ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2499,7 +2499,7 @@ var By = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+9.sha-876ceb3.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+10.sha-f3ce8ee.with-local-changes');
 
 /**
  * @license
