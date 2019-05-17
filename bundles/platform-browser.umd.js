682c71d38aba1f0a51055aac7f659b42c287e655
Jason Aden <jasonaden@google.com>
2019-05-17 03:41:37 +0000

    98ded94 fixup! test(ivy): make the test run with benchpress (#30449)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+233.sha-1f6fcb6.with-local-changes
+ * @license Angular v8.0.0-rc.0+238.sha-98ded94.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+233.sha-1f6fcb6.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+238.sha-98ded94.with-local-changes');
 
     /**
      * @license
