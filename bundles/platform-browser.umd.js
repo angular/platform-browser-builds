ff96fe0685671df5443db493d9816450652a4f5a
Jason Aden <jasonaden@google.com>
2019-05-22 23:36:34 +0000

    62c936a docs: update node and typescript versions mentioned in docs (#30601)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+45.sha-77e1b34.with-local-changes
+ * @license Angular v8.0.0-rc.4+46.sha-62c936a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.4+45.sha-77e1b34.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.4+46.sha-62c936a.with-local-changes');
 
     /**
      * @license
