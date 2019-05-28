869ce6b3ce77baffa9d6fe01b014050051a4f2d1
Igor Minar <igor@angular.io>
2019-05-28 23:57:19 +0000

    f3ee9a6 docs: add note to platform-webworker deprecation (#30705)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+1.sha-d076c51.with-local-changes
+ * @license Angular v8.0.0+2.sha-f3ee9a6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0+1.sha-d076c51.with-local-changes');
+    var VERSION = new i0.Version('8.0.0+2.sha-f3ee9a6.with-local-changes');
 
     /**
      * @license
