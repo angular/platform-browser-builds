75de4718b9546b671334deff748649088b7d902f
Miško Hevery <misko@hevery.com>
2019-05-31 17:04:17 +0000

    cdf586d build(docs-infra): upgrade cli command docs sources to e8bdfe6b9 (#30777)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+17.sha-0f352b6.with-local-changes
+ * @license Angular v8.0.0+19.sha-cdf586d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0+17.sha-0f352b6.with-local-changes');
+    var VERSION = new i0.Version('8.0.0+19.sha-cdf586d.with-local-changes');
 
     /**
      * @license
