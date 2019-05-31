40a499561e5aa2a7e6d52c47a1dbb4a7cf959a89
Miško Hevery <misko@hevery.com>
2019-05-31 17:23:31 +0000

    e82b45c docs(docs-infra): remove deprecated `Buffer` usage from stackblitz builder (#30369)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+20.sha-8422ef2.with-local-changes
+ * @license Angular v8.0.0+21.sha-e82b45c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0+20.sha-8422ef2.with-local-changes');
+    var VERSION = new i0.Version('8.0.0+21.sha-e82b45c.with-local-changes');
 
     /**
      * @license
