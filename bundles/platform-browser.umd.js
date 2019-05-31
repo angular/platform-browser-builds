7118bc0aea5ae5be1b4bae73aaee82701b230e83
Miško Hevery <misko@hevery.com>
2019-05-31 17:17:15 +0000

    8422ef2 fix(docs-infra): update tooltip of events link (#30643)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+19.sha-cdf586d.with-local-changes
+ * @license Angular v8.0.0+20.sha-8422ef2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0+19.sha-cdf586d.with-local-changes');
+    var VERSION = new i0.Version('8.0.0+20.sha-8422ef2.with-local-changes');
 
     /**
      * @license
