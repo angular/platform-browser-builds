e01df58e50473693ccbcbea70a68b44716410361
Misko Hevery <misko@hevery.com>
2019-06-04 19:37:35 +0000

    ccc1c27 refactor: Move away from index signature based "updateCache". (#30518)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+35.sha-19caace.with-local-changes
+ * @license Angular v8.0.0+34.sha-ccc1c27.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0+35.sha-19caace.with-local-changes');
+    var VERSION = new i0.Version('8.0.0+34.sha-ccc1c27.with-local-changes');
 
     /**
      * @license
