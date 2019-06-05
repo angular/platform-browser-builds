832ef4186a30dfbaf50a81d151abcbcc6e122fd8
Jeremy Elbourn <jelbourn@google.com>
2019-06-05 17:33:53 +0000

    95ad7f1 fix(bazel): exclude components schematics from build

---
+++ testing/testing.d.ts
@@ -0,0 +1,28 @@
+/**
+ * @license Angular v8.1.0-beta.0+24.sha-95ad7f1.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { NgZone } from '@angular/core';
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class BrowserTestingModule {
+}
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformBrowserTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+export declare function ɵangular_packages_platform_browser_testing_testing_a(): NgZone;
+
+export { }
