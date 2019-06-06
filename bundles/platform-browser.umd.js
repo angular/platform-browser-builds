e17887f4284b1bd23731141a7e90fcb28bb6aadf
Misko Hevery <misko@hevery.com>
2019-06-06 04:51:15 +0000

    65f2010 fix(ivy): ngcc - use spaces in overwritten `package.json` content for readability (#30831)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+38.sha-5a7bcd1.with-local-changes
+ * @license Angular v8.0.0+39.sha-65f2010.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0+38.sha-5a7bcd1.with-local-changes');
+    var VERSION = new i0.Version('8.0.0+39.sha-65f2010.with-local-changes');
 
     /**
      * @license
