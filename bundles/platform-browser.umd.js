889eade25074acf646f6be13df3e90f371a849b7
Misko Hevery <misko@hevery.com>
2019-06-07 16:11:41 +0000

    9aeef0a build(docs-infra): fail hard if the CLI source is not what we expect (#30901)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+42.sha-be82270.with-local-changes
+ * @license Angular v8.0.0+46.sha-9aeef0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0+42.sha-be82270.with-local-changes');
+    var VERSION = new i0.Version('8.0.0+46.sha-9aeef0a.with-local-changes');
 
     /**
      * @license
