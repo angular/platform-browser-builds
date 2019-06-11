9838c024d2c6fcb158ea22b26421fb5aaceeb64f
Andrew Kushnir <akushnir@google.com>
2019-06-11 21:47:33 +0000

    7e2ed89 docs: fix a grammar mistake (#30949)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+62.sha-338e58c.with-local-changes
+ * @license Angular v8.0.0+61.sha-7e2ed89.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0+62.sha-338e58c.with-local-changes');
+    var VERSION = new i0.Version('8.0.0+61.sha-7e2ed89.with-local-changes');
 
     /**
      * @license
