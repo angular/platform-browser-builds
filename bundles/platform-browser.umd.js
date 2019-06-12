a89c3737cd1f92769c54d277ea9adf25e9ec7c9c
Andrew Kushnir <akushnir@google.com>
2019-06-12 19:08:41 +0000

    77a5790 docs: add platform to glossary (#30731)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+66.sha-4ca401c.with-local-changes
+ * @license Angular v8.0.0+67.sha-77a5790.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0+66.sha-4ca401c.with-local-changes');
+    var VERSION = new i0.Version('8.0.0+67.sha-77a5790.with-local-changes');
 
     /**
      * @license
