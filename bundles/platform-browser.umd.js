f7c9492df69bbf428903006b9c76cccaaea13e6e
Andrew Kushnir <akushnir@google.com>
2019-06-12 19:04:47 +0000

    3dcd5eb fix(ivy): unable to bind to implicit receiver in embedded views (#30994)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+64.sha-49307f0.with-local-changes
+ * @license Angular v8.0.0+65.sha-3dcd5eb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0+64.sha-49307f0.with-local-changes');
+    var VERSION = new i0.Version('8.0.0+65.sha-3dcd5eb.with-local-changes');
 
     /**
      * @license
