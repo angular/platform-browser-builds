003d1bfe42322ff44830e1687693b615b91cbd16
Andrew Kushnir <akushnir@google.com>
2019-06-18 18:13:08 +0000

    48174010a docs: update group() link to point to the animations group API (#30618)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+30.sha-1460e46.with-local-changes
+ * @license Angular v8.0.1+31.sha-4817401.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.1+30.sha-1460e46.with-local-changes');
+    var VERSION = new i0.Version('8.0.1+31.sha-4817401.with-local-changes');
 
     /**
      * @license
