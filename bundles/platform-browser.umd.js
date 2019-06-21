816ec5d9ea468b34edec1153ddc27eef90ab11a0
Kara Erickson <karakara@google.com>
2019-06-21 17:46:17 +0000

    15d138fdd docs: fix typo in browser-support.md (ES1015 --> ES2015) (#31157)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+13.sha-ff184b5.with-local-changes
+ * @license Angular v8.0.2+12.sha-15d138f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.2+13.sha-ff184b5.with-local-changes');
+    var VERSION = new i0.Version('8.0.2+12.sha-15d138f.with-local-changes');
 
     /**
      * @license
