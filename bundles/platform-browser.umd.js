8c5ce3aa175526f34ca039f2062d03bacdb785fe
Kara Erickson <karakara@google.com>
2019-06-25 17:53:36 +0000

    1d193df36 ci: enable remote build caching for CI jobs (#31204)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+36.sha-8a1bd2c.with-local-changes
+ * @license Angular v8.0.2+34.sha-1d193df.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.2+36.sha-8a1bd2c.with-local-changes');
+    var VERSION = new i0.Version('8.0.2+34.sha-1d193df.with-local-changes');
 
     /**
      * @license
