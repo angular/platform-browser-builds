471b4e8a5c3f458cd503551a0c7863a1407c7f3d
Miško Hevery <misko@hevery.com>
2019-07-24 22:15:33 +0000

    53286d1ef fix(zone.js): patch shadydom (#31717)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+28.sha-c73b75f.with-local-changes
+ * @license Angular v8.1.2+30.sha-53286d1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.2+28.sha-c73b75f.with-local-changes');
+    var VERSION = new i0.Version('8.1.2+30.sha-53286d1.with-local-changes');
 
     /**
      * @license
