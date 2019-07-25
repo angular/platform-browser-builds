4b4866dad092f33fa4b63bf72bea52e3c11095cd
Misko Hevery <misko@hevery.com>
2019-07-25 23:24:18 +0000

    55fab0a5c build: lock material unit tests job to specific commit (#31569) (#31847)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+33.sha-fdf2f69.with-local-changes
+ * @license Angular v8.1.2+35.sha-55fab0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2583,7 +2583,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.2+33.sha-fdf2f69.with-local-changes');
+    var VERSION = new i0.Version('8.1.2+35.sha-55fab0a.with-local-changes');
 
     /**
      * @license
