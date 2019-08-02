06d942fab253ea1b6b6902a5be82cf04c7937c9b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 01:05:33 +0000

    44b5bf486 docs: add links to web.dev/angular guides (#31804)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+5.sha-04ebd59.with-local-changes
+ * @license Angular v8.2.0+7.sha-44b5bf4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2585,7 +2585,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0+5.sha-04ebd59.with-local-changes');
+    var VERSION = new i0.Version('8.2.0+7.sha-44b5bf4.with-local-changes');
 
     /**
      * @license
