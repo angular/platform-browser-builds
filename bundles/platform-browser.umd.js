ad4cb956c1d37a25104f4512c2ce9a91bc0d502c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 17:18:34 +0000

    2cabced9f build(docs-infra): fix StackBlitz projects and ZIPs for `testing` guide examples (#31937)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+10.sha-dde0a32.with-local-changes
+ * @license Angular v8.2.0+13.sha-2cabced.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2585,7 +2585,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0+10.sha-dde0a32.with-local-changes');
+    var VERSION = new i0.Version('8.2.0+13.sha-2cabced.with-local-changes');
 
     /**
      * @license
