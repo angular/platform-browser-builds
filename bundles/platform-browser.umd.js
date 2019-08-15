d9d27279f90e80ad2cca5706d5a983e8dc8df5de
Andrew Kushnir <akushnir@google.com>
2019-08-15 17:57:52 +0000

    adc869ff8 build: add `ngcc` as a valid commit message scope (#32144)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+16.sha-46f2dcc.with-local-changes
+ * @license Angular v8.2.2+17.sha-adc869f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2733,7 +2733,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.2+16.sha-46f2dcc.with-local-changes');
+    var VERSION = new i0.Version('8.2.2+17.sha-adc869f.with-local-changes');
 
     /**
      * @license
