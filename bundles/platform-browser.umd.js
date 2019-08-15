b9e87f884f3c66104db7c59082c9ecf29af3f112
Andrew Kushnir <akushnir@google.com>
2019-08-15 20:06:14 +0000

    699c705a8 docs: clarify hierarchical injectors (#28700)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+19.sha-6f2e8af.with-local-changes
+ * @license Angular v8.2.2+18.sha-699c705.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2733,7 +2733,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.2+19.sha-6f2e8af.with-local-changes');
+    var VERSION = new i0.Version('8.2.2+18.sha-699c705.with-local-changes');
 
     /**
      * @license
