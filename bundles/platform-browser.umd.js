bf3ce12ad4f8ca83957ebdd84266ab90e8b6937d
Andrew Kushnir <akushnir@google.com>
2019-08-21 00:06:01 +0000

    26dc82682 docs: change to global approvers (#31940)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+26.sha-287247e.with-local-changes
+ * @license Angular v8.2.2+27.sha-26dc826.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2733,7 +2733,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.2+26.sha-287247e.with-local-changes');
+    var VERSION = new i0.Version('8.2.2+27.sha-26dc826.with-local-changes');
 
     /**
      * @license
