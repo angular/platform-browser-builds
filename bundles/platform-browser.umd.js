0668968403907a32be99175afdd523818345a03a
Miško Hevery <misko@hevery.com>
2019-08-29 19:56:57 +0000

    9d2348b9a docs: fix typo in compiler options guide (#32348)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+4.sha-e7bca07.with-local-changes
+ * @license Angular v8.2.4+5.sha-9d2348b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2733,7 +2733,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.4+4.sha-e7bca07.with-local-changes');
+    var VERSION = new i0.Version('8.2.4+5.sha-9d2348b.with-local-changes');
 
     /**
      * @license
