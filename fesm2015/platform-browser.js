29ec391baddeea9e6a17286c5d938d1519e8750c
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 16:40:30 +0000

    a65d3fa1d fix(language-service): Return empty external files during project initialization (#32519)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+26.sha-df5924a.with-local-changes
+ * @license Angular v9.0.0-next.5+28.sha-a65d3fa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3386,7 +3386,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.5+26.sha-df5924a.with-local-changes');
+const VERSION = new Version('9.0.0-next.5+28.sha-a65d3fa.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
