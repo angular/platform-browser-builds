f6c73bc922bc5596d000726b13422eca59c0d855
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 17:29:51 +0000

    6e380cff8 ci: update i18n CODEOWNERS for @angular/localize package (#32570)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+15.sha-76a8470.with-local-changes
+ * @license Angular v8.2.5+16.sha-6e380cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2733,7 +2733,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.5+15.sha-76a8470.with-local-changes');
+    var VERSION = new i0.Version('8.2.5+16.sha-6e380cf.with-local-changes');
 
     /**
      * @license
