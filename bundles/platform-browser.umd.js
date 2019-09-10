eb0d267168de7ceb2abe7186cda1dd3923c4219e
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 21:47:11 +0000

    2565f6795 docs: add mdb angular boilerplate to docs resources (#30448)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+17.sha-38f4dcd.with-local-changes
+ * @license Angular v8.2.5+18.sha-2565f67.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2733,7 +2733,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.5+17.sha-38f4dcd.with-local-changes');
+    var VERSION = new i0.Version('8.2.5+18.sha-2565f67.with-local-changes');
 
     /**
      * @license
