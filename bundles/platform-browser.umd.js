e38faf7a17d2f77b387213d5a103a711337b75cc
Andrew Kushnir <akushnir@google.com>
2019-09-17 00:01:59 +0000

    0ee2b755e ci: use automatic strategy selection for AngularTemplateCompile and TypeScriptCompile (#32708)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+14.sha-f213c7a.with-local-changes
+ * @license Angular v8.2.6+15.sha-0ee2b75.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2733,7 +2733,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.6+14.sha-f213c7a.with-local-changes');
+    var VERSION = new i0.Version('8.2.6+15.sha-0ee2b75.with-local-changes');
 
     /**
      * @license
