f4483c24cb9bc3e1215a24287539dd023af3d855
atscott <atscott@google.com>
2019-09-27 17:52:33 +0000

    466c754cc docs: fix typo in deployment guide (#32777)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+6.sha-45fee06.with-local-changes
+ * @license Angular v8.2.8+7.sha-466c754.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2733,7 +2733,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.8+6.sha-45fee06.with-local-changes');
+    var VERSION = new i0.Version('8.2.8+7.sha-466c754.with-local-changes');
 
     /**
      * @license
