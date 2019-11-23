e3696f8f8dd69717822d60adc706cf87259c4958
Matias Niemelä <matias@yearofmoo.com>
2019-11-23 00:00:50 +0000

    7b77ead88f ci: add JoostK to @angular/fw-compiler (#33994)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+52.sha-9248dba.with-local-changes
+ * @license Angular v9.0.0-rc.3+55.sha-7b77ead.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1928,7 +1928,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.3+52.sha-9248dba.with-local-changes');
+var VERSION = new Version('9.0.0-rc.3+55.sha-7b77ead.with-local-changes');
 
 /**
  * @license
