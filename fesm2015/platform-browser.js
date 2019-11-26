1eee804857696c569ae3e3b1faf6d1a8681e9d4d
Matias Niemelä <matias@yearofmoo.com>
2019-11-26 04:00:32 +0000

    d655093123 docs: add Animations import in Router doc (#33740)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+266.sha-953365d.with-local-changes
+ * @license Angular v9.0.0-rc.1+267.sha-d655093.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3355,7 +3355,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+266.sha-953365d.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+267.sha-d655093.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
