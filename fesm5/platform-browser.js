93cf1f1c1ecfbf7f525a1d0c435c018e3e0c29bb
Miško Hevery <misko@hevery.com>
2019-12-02 19:58:55 +0000

    e7cc1d606a refactor(core): update missing-injectable migration description to match guide (#34125)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+321.sha-5a7f334.with-local-changes
+ * @license Angular v9.0.0-rc.1+320.sha-e7cc1d6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1928,7 +1928,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+321.sha-5a7f334.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+320.sha-e7cc1d6.with-local-changes');
 
 /**
  * @license
