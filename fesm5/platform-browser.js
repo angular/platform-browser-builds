2831eb45cdd3f02aada96145482022babb9a9188
Andrew Kushnir <akushnir@google.com>
2019-12-10 18:54:12 +0000

    46f9201e6f refactor(ivy): pull property read into a constant (#34290)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+44.sha-7c2bb37.with-local-changes
+ * @license Angular v9.0.0-rc.5+47.sha-46f9201.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1928,7 +1928,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.5+44.sha-7c2bb37.with-local-changes');
+var VERSION = new Version('9.0.0-rc.5+47.sha-46f9201.with-local-changes');
 
 /**
  * @license
