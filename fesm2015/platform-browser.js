24a3d18feb9004e71c89ad00da871a51d59beac5
Kara Erickson <karakara@google.com>
2019-12-17 23:03:13 +0000

    3e201181bb fix(ivy): correctly associate output bound events with directives (#31938)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+497.sha-7938ff3
+ * @license Angular v9.0.0-rc.1+498.sha-3e20118
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3355,7 +3355,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+497.sha-7938ff3');
+const VERSION = new Version('9.0.0-rc.1+498.sha-3e20118');
 
 /**
  * @fileoverview added by tsickle
