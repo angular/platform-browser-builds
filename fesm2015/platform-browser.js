065cebddae4dd038f4a6b573b161263722d0aeec
Kara Erickson <karakara@google.com>
2019-12-18 19:56:12 +0000

    23595272fe build: use vendored yarn version for bazel node toolchain (#34472)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+509.sha-9264f43
+ * @license Angular v9.0.0-rc.1+510.sha-2359527
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3355,7 +3355,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+509.sha-9264f43');
+const VERSION = new Version('9.0.0-rc.1+510.sha-2359527');
 
 /**
  * @fileoverview added by tsickle
