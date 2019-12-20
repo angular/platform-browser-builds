82edb95369064ee68003b0802060e84de5d458f7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-12-20 23:02:11 +0000

    9b9116c79d feat(language-service): Append symbol type to hover tooltip (#34515)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+520.sha-2dffe65
+ * @license Angular v9.0.0-rc.1+521.sha-9b9116c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3355,7 +3355,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+520.sha-2dffe65');
+const VERSION = new Version('9.0.0-rc.1+521.sha-9b9116c');
 
 /**
  * @fileoverview added by tsickle
