827034bbd4796385defb1cad99472c4c86ecfdf7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 20:30:27 +0000

    5b864ede13 fix(ivy): TestBed not unwrapping imports array function when overriding provider (#34629)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+530.sha-ba2fd31
+ * @license Angular v9.0.0-rc.1+533.sha-5b864ed
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3355,7 +3355,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+530.sha-ba2fd31');
+const VERSION = new Version('9.0.0-rc.1+533.sha-5b864ed');
 
 /**
  * @fileoverview added by tsickle
