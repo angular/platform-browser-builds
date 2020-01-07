e575ef772ba7e8c0e11f39c3a7681e3e27557205
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 18:51:00 +0000

    effb92dfae fix(ivy): skip field inheritance if InheritDefinitionFeature is present on parent def (#34244)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+533.sha-5b864ed
+ * @license Angular v9.0.0-rc.1+534.sha-effb92d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3355,7 +3355,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+533.sha-5b864ed');
+const VERSION = new Version('9.0.0-rc.1+534.sha-effb92d');
 
 /**
  * @fileoverview added by tsickle
