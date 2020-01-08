c0543cc207f7cd98d3b2ced2caa68adeed401761
Miško Hevery <misko@hevery.com>
2020-01-08 20:59:28 +0000

    d1c7ca7393 refactor(ivy): add `insertTStylingBinding` to keep track of bindings.

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+524.sha-f004195
+ * @license Angular v9.0.0-rc.1+558.sha-d1c7ca7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1928,7 +1928,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+524.sha-f004195');
+var VERSION = new Version('9.0.0-rc.1+558.sha-d1c7ca7');
 
 /**
  * @license
