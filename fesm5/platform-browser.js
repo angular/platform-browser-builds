a3d90fa2d05383756026fb69f7bfb57866586e02
atscott <atscott@google.com>
2020-01-11 00:24:36 +0000

    08c25818d8 fix(ivy): don't run decorator handlers against declaration files (#34557)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+75.sha-a7d8dcd
+ * @license Angular v9.0.0-rc.8+76.sha-08c2581
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1928,7 +1928,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.8+75.sha-a7d8dcd');
+var VERSION = new Version('9.0.0-rc.8+76.sha-08c2581');
 
 /**
  * @license
