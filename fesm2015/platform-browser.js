3832cc0ecfed5cee94ce3a75835837909118f6be
atscott <atscott@google.com>
2020-01-15 18:01:49 +0000

    b7385a77ad docs: release notes for the v9.0.0-rc.9 release

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+688.sha-bf8ba89
+ * @license Angular v9.0.0-rc.1+689.sha-b7385a7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3139,7 +3139,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+688.sha-bf8ba89');
+const VERSION = new Version('9.0.0-rc.1+689.sha-b7385a7');
 
 /**
  * @fileoverview added by tsickle
