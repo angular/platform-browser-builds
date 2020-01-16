122268623dd20abee8727bd3da3dd254ab6286d1
GitHub <noreply@github.com>
2020-01-16 11:32:58 +0000

    9d66345226 build: disable link to PUBLIC_API docs when running ts-api-guardian in angular repo

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+693.sha-1fd4d4f
+ * @license Angular v9.0.0-rc.1+695.sha-9d66345
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3139,7 +3139,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+693.sha-1fd4d4f');
+const VERSION = new Version('9.0.0-rc.1+695.sha-9d66345');
 
 /**
  * @fileoverview added by tsickle
