6c7354ceb6c41e011f5c48c8a3463d7370eb1a1a
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 20:17:53 +0000

    61ad50d40a docs: add message on click of hero in toh-4 (#34496)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+746.sha-e7cf37d
+ * @license Angular v9.0.0-rc.1+747.sha-61ad50d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3139,7 +3139,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+746.sha-e7cf37d');
+const VERSION = new Version('9.0.0-rc.1+747.sha-61ad50d');
 
 /**
  * @fileoverview added by tsickle
