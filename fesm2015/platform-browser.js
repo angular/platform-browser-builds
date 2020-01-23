636173600bfc6969bf2cdbb27ad0b42df1d204a6
Andrew Kushnir <akushnir@google.com>
2020-01-23 20:02:36 +0000

    6a4186c017 build: vendor yarn using the .yarnrc yarn-path value (#34902)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+34.sha-e69d02c
+ * @license Angular v9.0.0-rc.10+37.sha-6a4186c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.10+34.sha-e69d02c');
+const VERSION = new Version('9.0.0-rc.10+37.sha-6a4186c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+34.sha-e69d02c
+ * @license Angular v9.0.0-rc.10+37.sha-6a4186c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.10+34.sha-e69d02c');
+var VERSION = new Version('9.0.0-rc.10+37.sha-6a4186c');
 
 /**
  * @license
