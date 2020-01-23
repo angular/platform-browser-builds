4d15d3151aeb766343bb8eb96cdb9bf183d73f05
Andrew Kushnir <akushnir@google.com>
2020-01-23 21:58:01 +0000

    5aa0507f6a docs(ivy): move incremental package README file to the correct location (#34912)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+780.sha-fa4ea34
+ * @license Angular v9.0.0-rc.1+782.sha-5aa0507
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+780.sha-fa4ea34');
+var VERSION = new Version('9.0.0-rc.1+782.sha-5aa0507');
 
 /**
  * @license
