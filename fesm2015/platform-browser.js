d0d634c1c53d580cab661b97f5b397c3cfd57c20
Andrew Kushnir <akushnir@google.com>
2020-01-24 20:42:34 +0000

    622737cee3 feat(common): expose getLocaleCurrencyCode publicly (#34946)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+52.sha-65354fb
+ * @license Angular v9.0.0-rc.10+54.sha-622737c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.10+52.sha-65354fb');
+const VERSION = new Version('9.0.0-rc.10+54.sha-622737c');
 
 /**
  * @fileoverview added by tsickle
