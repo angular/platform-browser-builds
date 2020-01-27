08446e06efadba98309995f77f9793531a0d6d10
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:09:27 +0000

    09869af90f fix(language-service): remove repeated symbol definitions for structural directive (#34847)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+819.sha-b3bd6ca
+ * @license Angular v9.0.0-rc.1+823.sha-09869af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+819.sha-b3bd6ca');
+var VERSION = new Version('9.0.0-rc.1+823.sha-09869af');
 
 /**
  * @license
