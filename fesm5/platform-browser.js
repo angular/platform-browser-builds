5d0e675631738c020d447bcf9f94c21c7bbc8573
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:34:29 +0000

    7ca74375cb ci: add missing codeowners for `aio/` files (#34988)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+857.sha-a892054
+ * @license Angular v9.0.0-rc.1+860.sha-7ca7437
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+857.sha-a892054');
+var VERSION = new Version('9.0.0-rc.1+860.sha-7ca7437');
 
 /**
  * @license
