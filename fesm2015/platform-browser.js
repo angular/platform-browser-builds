1ebc0e4d1235a2732765b495c47118abe4ece775
Andrew Kushnir <akushnir@google.com>
2020-01-29 23:37:00 +0000

    0390d20d60 build: remove unneeded style code for ngClass and ngStyle (#34998)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+58.sha-9b7d703
+ * @license Angular v9.0.0-rc.11+59.sha-0390d20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.11+58.sha-9b7d703');
+const VERSION = new Version('9.0.0-rc.11+59.sha-0390d20');
 
 /**
  * @fileoverview added by tsickle
