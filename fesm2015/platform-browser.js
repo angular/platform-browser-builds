d5392301a26524d4dde02a13f6bd54439df42108
Andrew Kushnir <akushnir@google.com>
2020-01-29 21:29:39 +0000

    7e0a031543 docs: remove named node version (#35014)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+53.sha-eaa4a5a
+ * @license Angular v9.0.0-rc.11+54.sha-7e0a031
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.11+53.sha-eaa4a5a');
+const VERSION = new Version('9.0.0-rc.11+54.sha-7e0a031');
 
 /**
  * @fileoverview added by tsickle
