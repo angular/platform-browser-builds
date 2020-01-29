70b97dbd8514a78f12f7392909502a2d8b85418a
Andrew Kushnir <akushnir@google.com>
2020-01-29 20:43:40 +0000

    519e9e1ae8 ci: minor PullApprove fixes/improvements (#35015)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+850.sha-8fd2a97
+ * @license Angular v9.0.0-rc.1+855.sha-519e9e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+850.sha-8fd2a97');
+var VERSION = new Version('9.0.0-rc.1+855.sha-519e9e1');
 
 /**
  * @license
