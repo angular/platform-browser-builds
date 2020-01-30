ec88a2498308ef2b2555bd6c73d89e3e64a6b91d
Andrew Kushnir <akushnir@google.com>
2020-01-30 19:50:28 +0000

    cd9ae66b35 refactor(ivy): remove markDirty from global utilities (#34453)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+867.sha-d72cfc9
+ * @license Angular v9.0.0-rc.1+869.sha-cd9ae66
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+867.sha-d72cfc9');
+const VERSION = new Version('9.0.0-rc.1+869.sha-cd9ae66');
 
 /**
  * @fileoverview added by tsickle
