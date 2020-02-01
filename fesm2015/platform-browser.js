028ddbe82790692f5f71321eec8a353f59852533
Misko Hevery <misko@hevery.com>
2020-02-01 01:23:26 +0000

    58dbe662ff build: temporarily disable components-repo-unit-tests (#35079)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.12+17.sha-7f96fbb
+ * @license Angular v9.0.0-rc.12+24.sha-58dbe66
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.12+17.sha-7f96fbb');
+const VERSION = new Version('9.0.0-rc.12+24.sha-58dbe66');
 
 /**
  * @fileoverview added by tsickle
