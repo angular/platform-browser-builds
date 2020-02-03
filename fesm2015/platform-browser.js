066bb3377814bb49edd288140cbdea91cbaea31e
Miško Hevery <misko@hevery.com>
2020-02-03 17:08:09 +0000

    3d4067a464 fix(ngcc): do not lock if the target is not compiled by Angular (#35057)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+897.sha-a3c7ab9
+ * @license Angular v9.0.0-rc.1+898.sha-3d4067a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3361,7 +3361,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+897.sha-a3c7ab9');
+const VERSION = new Version('9.0.0-rc.1+898.sha-3d4067a');
 
 /**
  * @fileoverview added by tsickle
