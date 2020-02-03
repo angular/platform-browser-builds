2f13114af5a6305c51f9a0b299d257ef8841e721
Miško Hevery <misko@hevery.com>
2020-02-03 22:22:44 +0000

    ab931cf872 fix(ivy): host-styling throws assert exception inside *ngFor (#35133)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+902.sha-a8609ba
+ * @license Angular v9.0.0-rc.1+903.sha-ab931cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+902.sha-a8609ba');
+const VERSION = new Version('9.0.0-rc.1+903.sha-ab931cf');
 
 /**
  * @fileoverview added by tsickle
