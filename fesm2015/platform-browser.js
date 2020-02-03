c37ee0e7c04f8196432117c799c4d7ec3fe99cb7
Miško Hevery <misko@hevery.com>
2020-02-03 17:20:46 +0000

    01308e4c7c refactor(ivy): Remove `TNode.directives` in favor of `TData` (#35050)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+899.sha-65dbd50
+ * @license Angular v9.0.0-rc.1+900.sha-01308e4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3361,7 +3361,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+899.sha-65dbd50');
+const VERSION = new Version('9.0.0-rc.1+900.sha-01308e4');
 
 /**
  * @fileoverview added by tsickle
