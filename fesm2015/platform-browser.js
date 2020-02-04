335db4f1c49ea3fc2b583f2bce21fd9b3cf13915
Miško Hevery <misko@hevery.com>
2020-02-04 18:46:00 +0000

    e6cb60ee22 docs: update typescript version reference (#35120)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+908.sha-0e3eeb4
+ * @license Angular v9.0.0-rc.1+910.sha-e6cb60e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3361,7 +3361,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+908.sha-0e3eeb4');
+const VERSION = new Version('9.0.0-rc.1+910.sha-e6cb60e');
 
 /**
  * @fileoverview added by tsickle
