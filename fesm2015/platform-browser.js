c055bb7cb7cafb39c734640241217d8b23c30e2a
Miško Hevery <misko@hevery.com>
2020-02-04 19:05:18 +0000

    5cada5cce1 fix(ivy): recompile on template change in ngc watch mode on Windows (#34015)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+914.sha-f7d4cc9
+ * @license Angular v9.0.0-rc.1+915.sha-5cada5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3361,7 +3361,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+914.sha-f7d4cc9');
+const VERSION = new Version('9.0.0-rc.1+915.sha-5cada5c');
 
 /**
  * @fileoverview added by tsickle
