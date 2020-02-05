682cd9c93bfca399100ea12a7c43cdf2935d37d9
Miško Hevery <misko@hevery.com>
2020-02-05 00:19:52 +0000

    aed5c7caad feat(docs-infra): add stable to the list of api statuses (#34981)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+925.sha-491654d
+ * @license Angular v9.0.0-rc.1+922.sha-aed5c7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+925.sha-491654d');
+var VERSION = new Version('9.0.0-rc.1+922.sha-aed5c7c');
 
 /**
  * @license
