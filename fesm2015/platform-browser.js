ae2c5cf646f1b538c941344aeddb798280dd8e63
Misko Hevery <misko@hevery.com>
2020-02-06 01:13:20 +0000

    7de87728e2 ci: temporary disable the Android 7 SauceLab due to timeout (#35174)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.14+20.sha-cf3071f
+ * @license Angular v9.0.0-rc.14+21.sha-7de8772
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.14+20.sha-cf3071f');
+const VERSION = new Version('9.0.0-rc.14+21.sha-7de8772');
 
 /**
  * @fileoverview added by tsickle
