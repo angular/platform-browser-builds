1bcd5f91419402691d7b0e70e5afce259b191a49
Misko Hevery <misko@hevery.com>
2020-02-06 23:59:40 +0000

    6fb538ed1d fix(bazel): update ibazel to 0.11.1 (#35158)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+944.sha-3c69442
+ * @license Angular v9.0.0-rc.1+948.sha-6fb538e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+944.sha-3c69442');
+const VERSION = new Version('9.0.0-rc.1+948.sha-6fb538e');
 
 /**
  * @fileoverview added by tsickle
