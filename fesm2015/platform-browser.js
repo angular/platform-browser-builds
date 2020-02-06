055c0687c70e63fe406d82e7c7d7a41470618a8d
Misko Hevery <misko@hevery.com>
2020-02-06 20:04:16 +0000

    22acdc2728 docs: consilidate release notes for the v9.0.0 release

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+936.sha-548b0e5
+ * @license Angular v9.0.0-rc.1+937.sha-22acdc2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3361,7 +3361,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+936.sha-548b0e5');
+const VERSION = new Version('9.0.0-rc.1+937.sha-22acdc2');
 
 /**
  * @fileoverview added by tsickle
