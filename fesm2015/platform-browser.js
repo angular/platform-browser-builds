2dbd225786d4b2c8bcb84fcc611bb6a762e59fd6
Kara Erickson <karakara@google.com>
2020-02-07 22:28:49 +0000

    26b17aee14 docs: add asset info to library guide (#34217)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+967.sha-5de5b52
+ * @license Angular v9.0.0-rc.1+968.sha-26b17ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3361,7 +3361,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+967.sha-5de5b52');
+const VERSION = new Version('9.0.0-rc.1+968.sha-26b17ae');
 
 /**
  * @fileoverview added by tsickle
