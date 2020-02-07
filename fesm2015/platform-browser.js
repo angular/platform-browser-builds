24a9ccdc0aef290e5fa1a809322f19bc06895c91
Kara Erickson <karakara@google.com>
2020-02-07 21:28:20 +0000

    dea1b962c7 fix(ivy): repeat template guards to narrow types in event handlers (#35193)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+30.sha-dce230e
+ * @license Angular v9.0.0+31.sha-dea1b96
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0+30.sha-dce230e');
+const VERSION = new Version('9.0.0+31.sha-dea1b96');
 
 /**
  * @fileoverview added by tsickle
