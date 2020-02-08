9129bc1a665382377b77116f42be7cb0ca2d903b
Kara Erickson <karakara@google.com>
2020-02-08 00:38:37 +0000

    18ed9dcd83 refactor(benchpress): delete outdated/unused folder (#35147)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+32.sha-5b5dc81
+ * @license Angular v9.0.0+36.sha-18ed9dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0+32.sha-5b5dc81');
+const VERSION = new Version('9.0.0+36.sha-18ed9dc');
 
 /**
  * @fileoverview added by tsickle
