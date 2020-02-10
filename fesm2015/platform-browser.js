ce7a157ea34a824c81b2e6450d575e9fcf929f9f
Kara Erickson <karakara@google.com>
2020-02-10 19:07:34 +0000

    d9fcfd3b24 docs(forms): update the ngForm deprecation notice (#35263)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+40.sha-bfcf6d0
+ * @license Angular v9.0.0+41.sha-d9fcfd3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0+40.sha-bfcf6d0');
+const VERSION = new Version('9.0.0+41.sha-d9fcfd3');
 
 /**
  * @fileoverview added by tsickle
