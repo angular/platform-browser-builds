5e6d61c939a00245e167ee947cfe534d2f988cda
Kara Erickson <karakara@google.com>
2020-02-11 00:54:53 +0000

    a92d97cda7 fix(compiler): report errors for missing binding names (#34595)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+981.sha-e1160f1
+ * @license Angular v9.0.0-rc.1+982.sha-a92d97c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+981.sha-e1160f1');
+var VERSION = new Version('9.0.0-rc.1+982.sha-a92d97c');
 
 /**
  * @license
