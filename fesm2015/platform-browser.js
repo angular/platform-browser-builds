e7683806648053bbc928a11e74272d1e04401fb5
Kara Erickson <karakara@google.com>
2020-02-12 19:20:14 +0000

    4aeb6c4e78 docs: release notes for the v9.0.1 release

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+990.sha-eef047b
+ * @license Angular v9.0.0-rc.1+991.sha-4aeb6c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+990.sha-eef047b');
+const VERSION = new Version('9.0.0-rc.1+991.sha-4aeb6c4');
 
 /**
  * @fileoverview added by tsickle
