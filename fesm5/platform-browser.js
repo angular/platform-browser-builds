64cfeea051e6bd52bcb1c861be711e05a9028ba7
Kara Erickson <karakara@google.com>
2020-02-13 01:10:35 +0000

    a84093a971 docs: remove service from region where it was added before it was created (#35354)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1
+ * @license Angular v9.0.1+6.sha-a84093a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.1');
+var VERSION = new Version('9.0.1+6.sha-a84093a');
 
 /**
  * @license
