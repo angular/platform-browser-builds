c04f9bbdd8446d54fd148713475cb2789713558b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:42:49 +0000

    2ca7984d55 fix(core): remove support for `Map`/`Set` in `[class]`/`[style]` bindings (#35392)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+21.sha-b9b512f
+ * @license Angular v9.1.0-next.0+23.sha-2ca7984
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.0+21.sha-b9b512f');
+const VERSION = new Version('9.1.0-next.0+23.sha-2ca7984');
 
 /**
  * @fileoverview added by tsickle
