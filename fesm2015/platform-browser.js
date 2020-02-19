b7f407151f2237d9fe35232465c7385bce0c36c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 01:47:15 +0000

    5fbfe6996a fix(ivy): wrong context passed to ngOnDestroy when resolved multiple times (#35249)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+37.sha-183a862
+ * @license Angular v9.1.0-next.0+38.sha-5fbfe69
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3361,7 +3361,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.0+37.sha-183a862');
+const VERSION = new Version('9.1.0-next.0+38.sha-5fbfe69');
 
 /**
  * @fileoverview added by tsickle
