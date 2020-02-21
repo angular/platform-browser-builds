1920ca74bec6c1abb1c48f9f1f1a54d09a0a5a1f
Miško Hevery <misko@hevery.com>
2020-02-21 17:01:31 +0000

    824d9a8cbf build: update cli packages to latest versions (#35608)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+10.sha-72664ca
+ * @license Angular v9.0.2+19.sha-824d9a8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.2+10.sha-72664ca');
+const VERSION = new Version('9.0.2+19.sha-824d9a8');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+10.sha-72664ca
+ * @license Angular v9.0.2+19.sha-824d9a8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.2+10.sha-72664ca');
+var VERSION = new Version('9.0.2+19.sha-824d9a8');
 
 /**
  * @license
