4d74cd6c04ddb9a052dbc1de285d842b8422597d
Miško Hevery <misko@hevery.com>
2020-02-24 17:31:03 +0000

    71b5970450 fix(ngcc): capture path-mapped entry-points that start with same string (#35592)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+43.sha-a7d5c55
+ * @license Angular v9.1.0-next.1+45.sha-71b5970
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+43.sha-a7d5c55');
+const VERSION = new Version('9.1.0-next.1+45.sha-71b5970');
 
 /**
  * @fileoverview added by tsickle
