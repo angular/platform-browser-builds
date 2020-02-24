427ca2ad9852c907ef5107e826f6d9c850b07d03
Miško Hevery <misko@hevery.com>
2020-02-24 17:33:35 +0000

    3893799183 docs: add amazon s3 builder package (#34783)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+44.sha-53f059e
+ * @license Angular v9.1.0-next.1+46.sha-3893799
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+44.sha-53f059e');
+var VERSION = new Version('9.1.0-next.1+46.sha-3893799');
 
 /**
  * @license
