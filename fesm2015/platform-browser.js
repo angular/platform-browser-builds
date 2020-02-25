acce77f7e61d85b25ede3ff30f3257339b59eb56
Miško Hevery <misko@hevery.com>
2020-02-25 01:50:29 +0000

    8cac5fec20 ci: move saucelabs_ivy & saucelabs_view_engine to the daily monitoring CircleCI workflow (#35516)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+41.sha-89f6b34
+ * @license Angular v9.0.2+47.sha-8cac5fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.2+41.sha-89f6b34');
+const VERSION = new Version('9.0.2+47.sha-8cac5fe');
 
 /**
  * @fileoverview added by tsickle
