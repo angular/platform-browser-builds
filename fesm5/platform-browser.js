4c117d4ba28aeec3822ebbf3ffccf11ae0322f53
Miško Hevery <misko@hevery.com>
2020-02-26 21:20:11 +0000

    2a8dd4758c test(core): check dependency in extended child (#34767)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+65.sha-8fbb966
+ * @license Angular v9.1.0-next.1+68.sha-2a8dd47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+65.sha-8fbb966');
+var VERSION = new Version('9.1.0-next.1+68.sha-2a8dd47');
 
 /**
  * @license
