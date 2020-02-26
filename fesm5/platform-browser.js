93edf08d12803315685215f2c7234a2d6db19b3f
Miško Hevery <misko@hevery.com>
2020-02-26 21:13:00 +0000

    8fbb966ca7 docs(core): describe your change... (#34513)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+64.sha-6f68eae
+ * @license Angular v9.1.0-next.1+65.sha-8fbb966
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+64.sha-6f68eae');
+var VERSION = new Version('9.1.0-next.1+65.sha-8fbb966');
 
 /**
  * @license
