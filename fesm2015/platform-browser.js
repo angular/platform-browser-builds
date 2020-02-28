524dc0c8ac5232f4aa2ef9cd447bc370cc364323
Joey Perrott <josephperrott.github@gmail.com>
2020-02-28 20:32:55 +0000

    3b32bc7b41 ci: move public-api goldens to goldens directory

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+22.sha-7bfcc35
+ * @license Angular v9.1.0-next.2+22.sha-3b32bc7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+22.sha-7bfcc35');
+const VERSION = new Version('9.1.0-next.2+22.sha-3b32bc7');
 
 /**
  * @fileoverview added by tsickle
