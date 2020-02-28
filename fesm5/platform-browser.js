e7f02818e94d4840359f3d39823fd28f4d2216f8
atscott <atscott@google.com>
2020-02-28 15:35:10 +0000

    73ee57a6e3 test(language-service): differentiate feature and internal infra tests (#35688)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+17.sha-5e38056
+ * @license Angular v9.1.0-next.2+18.sha-73ee57a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+17.sha-5e38056');
+var VERSION = new Version('9.1.0-next.2+18.sha-73ee57a');
 
 /**
  * @license
