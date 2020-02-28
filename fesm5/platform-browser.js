ba6a38887fcf963c9b02cf24e2cccae33dc8cd42
atscott <atscott@google.com>
2020-02-28 20:51:12 +0000

    e13fcbaa6f perf(core): use multiple directives in host bindings micro benchmark (#35736)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+10.sha-fcad075
+ * @license Angular v9.0.4+14.sha-e13fcba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.4+10.sha-fcad075');
+var VERSION = new Version('9.0.4+14.sha-e13fcba');
 
 /**
  * @license
