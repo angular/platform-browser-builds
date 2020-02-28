ca7c3f39967ca91c791936dbc95fa69f4e6230ea
atscott <atscott@google.com>
2020-02-28 00:29:21 +0000

    52fc08754d fix(ivy): support dynamic query tokens in AOT mode (#35307)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+3.sha-287d841
+ * @license Angular v9.0.4+4.sha-52fc087
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.4+3.sha-287d841');
+const VERSION = new Version('9.0.4+4.sha-52fc087');
 
 /**
  * @fileoverview added by tsickle
