f4478f37bde713c579018a7f4286075e0a3ca443
atscott <atscott@google.com>
2020-03-02 16:45:45 +0000

    40da51f641 fix(compiler): support i18n attributes on `<ng-template>` tags (#35681)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+32.sha-35c9f0d
+ * @license Angular v9.1.0-next.2+33.sha-40da51f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+32.sha-35c9f0d');
+const VERSION = new Version('9.1.0-next.2+33.sha-40da51f');
 
 /**
  * @fileoverview added by tsickle
