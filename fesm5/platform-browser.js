725257d7c3daf254547a33b3798860f7c3b1e0ac
atscott <atscott@google.com>
2020-03-02 21:55:55 +0000

    54288401f2 docs: fix docs and associated code snippets for enabling more macro tasks in `fakeAsync()` (#35778)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+25.sha-710fca2
+ * @license Angular v9.0.4+27.sha-5428840
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.4+25.sha-710fca2');
+var VERSION = new Version('9.0.4+27.sha-5428840');
 
 /**
  * @license
