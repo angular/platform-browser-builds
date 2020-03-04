91051b90a4af30cb61bd4d0616cad7c8828fc4e8
atscott <atscott@google.com>
2020-03-04 17:04:04 +0000

    2e728f7fff docs: remove `ivy` and mention `ve` label in docs (#35809)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+61.sha-d543b13
+ * @license Angular v9.1.0-next.2+62.sha-2e728f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3361,7 +3361,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+61.sha-d543b13');
+const VERSION = new Version('9.1.0-next.2+62.sha-2e728f7');
 
 /**
  * @fileoverview added by tsickle
