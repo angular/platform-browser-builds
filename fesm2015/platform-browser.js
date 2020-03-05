ed22d95eba4435cf38946a7155d7835ea4e5b8d8
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:22:40 +0000

    e0a35e13d5 perf(ngcc): reduce directory traversing (#35756)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+75.sha-a1d0396
+ * @license Angular v9.1.0-next.2+77.sha-e0a35e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+75.sha-a1d0396');
+const VERSION = new Version('9.1.0-next.2+77.sha-e0a35e1');
 
 /**
  * @fileoverview added by tsickle
