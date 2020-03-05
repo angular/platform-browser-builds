ce10b3f75c9d9a2df2da6e13d9d2caac0b95a6f4
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:35:58 +0000

    2a9b254ca5 refactor(compiler): Break up parseTemplateBindings() for microsyntax (#35812)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+1.sha-773d7b8
+ * @license Angular v9.0.5+5.sha-2a9b254
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.5+1.sha-773d7b8');
+var VERSION = new Version('9.0.5+5.sha-2a9b254');
 
 /**
  * @license
