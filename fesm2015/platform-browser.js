8f795cf59e9235fdca9bf69d4cc0eeb85c666f5a
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 20:58:58 +0000

    a1d0396c55 build: fix integration tests flakes using local yarn cache for bazel-schematics & ng_elements_schematics demos (#35877)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+74.sha-de67978
+ * @license Angular v9.1.0-next.2+75.sha-a1d0396
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3361,7 +3361,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+74.sha-de67978');
+const VERSION = new Version('9.1.0-next.2+75.sha-a1d0396');
 
 /**
  * @fileoverview added by tsickle
