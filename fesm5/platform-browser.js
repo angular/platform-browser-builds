8b750ff3e0e9ae5c2ab034cf9f7546e012674546
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:09:56 +0000

    eaf5b5856d fix(core): undecorated-classes-with-di migration should handle libraries generated with CLI versions past v6.2.0 (#35824)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+6.sha-2cccf59
+ * @license Angular v9.0.5+10.sha-eaf5b58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.5+6.sha-2cccf59');
+var VERSION = new Version('9.0.5+10.sha-eaf5b58');
 
 /**
  * @license
