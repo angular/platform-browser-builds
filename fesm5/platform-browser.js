7affc68c1423a50eca885fb360df5ea26ca9eb06
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 18:48:02 +0000

    fc4c3c3eb5 fix(localize): merge translation from all  XLIFF `<file>` elements (#35936)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+111.sha-9ff9a07
+ * @license Angular v9.1.0-next.2+110.sha-fc4c3c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+111.sha-9ff9a07');
+var VERSION = new Version('9.1.0-next.2+110.sha-fc4c3c3');
 
 /**
  * @license
