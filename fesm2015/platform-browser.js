c54d19966316cdff58dc1a8ab8ae0765ac4d573c
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:15:23 +0000

    3d46a45fa8 fix(language-service): resolve the variable from the template context first (#35982)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+117.sha-81cb54f
+ * @license Angular v9.1.0-next.2+116.sha-3d46a45
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3361,7 +3361,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+117.sha-81cb54f');
+const VERSION = new Version('9.1.0-next.2+116.sha-3d46a45');
 
 /**
  * @fileoverview added by tsickle
