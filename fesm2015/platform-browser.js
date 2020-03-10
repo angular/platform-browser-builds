8a2b8043e21d10a5ad5d3675b07b9e0f7c1f408e
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:15:23 +0000

    287bfefade ci: update components-repo-unit-tests job commit (#35961)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+116.sha-3d46a45
+ * @license Angular v9.1.0-next.2+120.sha-287bfef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+116.sha-3d46a45');
+const VERSION = new Version('9.1.0-next.2+120.sha-287bfef');
 
 /**
  * @fileoverview added by tsickle
