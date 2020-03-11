798672524749b9bd9482ff21ee12739a5965a16f
Andrew Kushnir <akushnir@google.com>
2020-03-11 22:35:50 +0000

    7af0648207 build: remove legacy integration test runner (#35985)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+17.sha-e262962
+ * @license Angular v9.0.6+24.sha-7af0648
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.6+17.sha-e262962');
+var VERSION = new Version('9.0.6+24.sha-7af0648');
 
 /**
  * @license
