f2b10e9cd6ef4c81705eaaa5cdbfc0a6ea3290b5
Andrew Kushnir <akushnir@google.com>
2020-03-11 23:35:54 +0000

    0d806c84e2 docs: add developer survey 2020 (#35655)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+35.sha-2b6028b
+ * @license Angular v9.1.0-next.4+36.sha-0d806c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3357,7 +3357,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+35.sha-2b6028b');
+const VERSION = new Version('9.1.0-next.4+36.sha-0d806c8');
 
 /**
  * @fileoverview added by tsickle
