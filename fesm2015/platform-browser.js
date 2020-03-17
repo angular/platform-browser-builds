b15abaede6b5115c00663e330c1618e89f5ab13b
Andrew Kushnir <akushnir@google.com>
2020-03-17 18:41:20 +0000

    70425d2a3c Revert "build: fix ts-api-guardian golden approval not working on windows (#36096)" (#36113)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+46.sha-35d94e9
+ * @license Angular v9.0.6+47.sha-70425d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.6+46.sha-35d94e9');
+const VERSION = new Version('9.0.6+47.sha-70425d2');
 
 /**
  * @fileoverview added by tsickle
