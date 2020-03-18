122a2be2a1a54ca98cb858262382a94f1f4ecf53
Andrew Kushnir <akushnir@google.com>
2020-03-18 23:19:17 +0000

    6d4f437973 build: ng_rollup_bundle internal rule should support ngcc (#36044)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.7
+ * @license Angular v9.0.7+2.sha-6d4f437
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3145,7 +3145,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.7');
+const VERSION = new Version('9.0.7+2.sha-6d4f437');
 
 /**
  * @fileoverview added by tsickle
