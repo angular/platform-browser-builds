2c8b644c43b2fe6b485463d08be4ce5d7b024867
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:49:40 +0000

    0baab0f3db docs: update release nots with blog post (#36268)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+2.sha-294c629
+ * @license Angular v9.1.0+14.sha-0baab0f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3141,7 +3141,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0+2.sha-294c629');
+const VERSION = new Version('9.1.0+14.sha-0baab0f');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+2.sha-294c629
+ * @license Angular v9.1.0+14.sha-0baab0f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+2.sha-294c629');
+var VERSION = new Version('9.1.0+14.sha-0baab0f');
 
 /**
  * @license
