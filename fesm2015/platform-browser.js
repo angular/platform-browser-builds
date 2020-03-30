06e7c3309ed3569b983302e5153320d32c739eca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 18:59:58 +0000

    5ac308060d refactor(ngcc): rename `workerCount` to `maxWorkerCount` (#36298)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+58.sha-c6dd900
+ * @license Angular v9.1.0-rc.0+57.sha-5ac3080
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3357,7 +3357,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+58.sha-c6dd900');
+const VERSION = new Version('9.1.0-rc.0+57.sha-5ac3080');
 
 /**
  * @fileoverview added by tsickle
