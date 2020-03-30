129099c1770b1e4678016ec7bcd72e2de00cf814
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 19:21:10 +0000

    15cffa210c build: update to rules_nodejs 1.5.0 (#36307)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+16.sha-c3b297a
+ * @license Angular v9.1.0+20.sha-15cffa2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+16.sha-c3b297a');
+var VERSION = new Version('9.1.0+20.sha-15cffa2');
 
 /**
  * @license
