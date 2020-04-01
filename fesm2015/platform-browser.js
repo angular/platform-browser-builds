192b61d0a45d6e04cab9e12780a1cd249f4afe75
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:56:39 +0000

    326240eb91 fix(ngcc): allow ngcc configuration to match pre-release versions of packages (#36370)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+81.sha-cc4b813
+ * @license Angular v9.1.0-rc.0+84.sha-326240e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3357,7 +3357,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+81.sha-cc4b813');
+const VERSION = new Version('9.1.0-rc.0+84.sha-326240e');
 
 /**
  * @fileoverview added by tsickle
