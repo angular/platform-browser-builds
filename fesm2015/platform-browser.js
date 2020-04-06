849a8029838119e39f75c2bcf39c77fd522f287b
Kara Erickson <karakara@google.com>
2020-04-06 20:46:52 +0000

    d43c30688a fix(core): avoid migration error when non-existent symbol is imported (#36367)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+121.sha-ee70a18
+ * @license Angular v9.1.0-rc.0+122.sha-d43c306
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3357,7 +3357,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+121.sha-ee70a18');
+const VERSION = new Version('9.1.0-rc.0+122.sha-d43c306');
 
 /**
  * @fileoverview added by tsickle
