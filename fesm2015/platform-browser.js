eeaa6715bea2df27e3ece2edd8f939bca5617f94
atscott <atscott@google.com>
2020-04-08 22:12:33 +0000

    cbed582a1a style(compiler): reformat of codebase with new clang-format version (#36520)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+9.sha-33eee43
+ * @license Angular v9.1.1+11.sha-cbed582
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3141,7 +3141,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.1+9.sha-33eee43');
+const VERSION = new Version('9.1.1+11.sha-cbed582');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+9.sha-33eee43
+ * @license Angular v9.1.1+11.sha-cbed582
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+9.sha-33eee43');
+var VERSION = new Version('9.1.1+11.sha-cbed582');
 
 /**
  * @license
