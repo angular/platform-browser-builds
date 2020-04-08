0193eb526381507dbb9eacc4f4ef7f92b91dc6fc
atscott <atscott@google.com>
2020-04-08 21:50:42 +0000

    33eee43263 fix(ngcc): do not warn if `paths` mapping does not exist (#36525)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+8.sha-72053b0
+ * @license Angular v9.1.1+9.sha-33eee43
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+8.sha-72053b0');
+var VERSION = new Version('9.1.1+9.sha-33eee43');
 
 /**
  * @license
