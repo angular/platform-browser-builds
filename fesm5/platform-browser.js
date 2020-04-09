b606384571fc7df885bfb620cf63cc4112bdb91b
atscott <atscott@google.com>
2020-04-09 23:50:12 +0000

    fee316161d build: update `REQUIRED_BASE_SHA` in merge script to clang 1.4.0 upgrade commit (#36547)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+18.sha-2e4244d
+ * @license Angular v10.0.0-next.1+19.sha-fee3161
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+18.sha-2e4244d');
+var VERSION = new Version('10.0.0-next.1+19.sha-fee3161');
 
 /**
  * @license
