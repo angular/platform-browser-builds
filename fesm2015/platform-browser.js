02ba3d263df10c085c03f98f62dde0b450a0cd1c
atscott <atscott@google.com>
2020-04-09 18:52:32 +0000

    078b0be4dc fix(common): `locales/global/*.js` are not ES5 compliant (#36342)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+11.sha-99b93e5
+ * @license Angular v10.0.0-next.1+13.sha-078b0be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3357,7 +3357,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+11.sha-99b93e5');
+const VERSION = new Version('10.0.0-next.1+13.sha-078b0be');
 
 /**
  * @fileoverview added by tsickle
