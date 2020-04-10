7cbb8852edccb3eb2c5f8917a61e4ab23d4d541b
atscott <atscott@google.com>
2020-04-10 16:30:16 +0000

    6ab43d7335 fix(ngcc): correctly detect external files from nested `node_modules/` (#36559)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+19.sha-fee3161
+ * @license Angular v10.0.0-next.1+20.sha-6ab43d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1934,7 +1934,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+19.sha-fee3161');
+var VERSION = new Version('10.0.0-next.1+20.sha-6ab43d7');
 
 /**
  * @license
