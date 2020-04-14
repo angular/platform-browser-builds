5d1d434e8b0c7b3ae0bf282b6e760cd72e746be1
atscott <atscott@google.com>
2020-04-14 17:38:57 +0000

    d50cb30443 test: disable failing saucelabs tests (#36620)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+30.sha-29122c5
+ * @license Angular v10.0.0-next.1+31.sha-d50cb30
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3357,7 +3357,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+30.sha-29122c5');
+const VERSION = new Version('10.0.0-next.1+31.sha-d50cb30');
 
 /**
  * @fileoverview added by tsickle
