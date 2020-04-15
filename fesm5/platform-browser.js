a6130e8b35ffdefe57ac9ce5679df1e9220ed6da
atscott <atscott@google.com>
2020-04-15 23:39:35 +0000

    5c3774cfe6 Revert "fix(common): `locales/global/*.js` are not ES5 compliant (#36342)" (#36648)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+45.sha-12266b2
+ * @license Angular v9.1.1+46.sha-5c3774c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2028,7 +2028,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+45.sha-12266b2');
+var VERSION = new Version('9.1.1+46.sha-5c3774c');
 
 /**
  * @license
