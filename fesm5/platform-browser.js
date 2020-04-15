3552b1ff8b38172ce402f49c2869146e459a1f9a
atscott <atscott@google.com>
2020-04-15 23:37:13 +0000

    074266b896 release: cut the v10.0.0-next.2 release

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+39.sha-c4dc642
+ * @license Angular v10.0.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2028,7 +2028,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+39.sha-c4dc642');
+var VERSION = new Version('10.0.0-next.2');
 
 /**
  * @license
