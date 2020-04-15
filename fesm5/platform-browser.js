6a95258b67c9d93a01b868e0b3424a3de01d70bd
atscott <atscott@google.com>
2020-04-15 16:45:32 +0000

    663b768780 fix(ngcc): force ngcc to exit on error (#36622)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+35.sha-0147de9
+ * @license Angular v10.0.0-next.1+36.sha-663b768
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2065,7 +2065,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+35.sha-0147de9');
+var VERSION = new Version('10.0.0-next.1+36.sha-663b768');
 
 /**
  * @license
