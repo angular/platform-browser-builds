5e36eee512171ef0ecbe35622b488027b6c97135
atscott <atscott@google.com>
2020-04-15 16:44:35 +0000

    12266b2042 fix(ngcc): display output from the unlocker process on Windows (#36569)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+42.sha-c5e7251
+ * @license Angular v9.1.1+45.sha-12266b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2028,7 +2028,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+42.sha-c5e7251');
+var VERSION = new Version('9.1.1+45.sha-12266b2');
 
 /**
  * @license
