f74b2e0961cf6f252c7b23829b5a0aa31b6c6464
atscott <atscott@google.com>
2020-04-16 18:32:25 +0000

    bfda7ca5bd docs(changelog): Add new line between changes (#36653)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+18.sha-305b059
+ * @license Angular v10.0.0-next.2+19.sha-bfda7ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2065,7 +2065,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+18.sha-305b059');
+var VERSION = new Version('10.0.0-next.2+19.sha-bfda7ca');
 
 /**
  * @license
