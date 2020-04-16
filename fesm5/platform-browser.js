9e984143914a49f7b5621c4ccf5f283b5148a5a9
atscott <atscott@google.com>
2020-04-16 00:10:06 +0000

    793a001d7c release: cut the v9.1.2 release

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+46.sha-5c3774c
+ * @license Angular v9.1.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2028,7 +2028,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+46.sha-5c3774c');
+var VERSION = new Version('9.1.2');
 
 /**
  * @license
