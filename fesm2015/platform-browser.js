9b5dcb77ba391354d48364cd523a12a61103bed2
atscott <atscott@google.com>
2020-04-16 17:56:33 +0000

    305b059809 docs: refactor routing doc (#35566)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+17.sha-297b7da
+ * @license Angular v10.0.0-next.2+18.sha-305b059
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3243,7 +3243,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+17.sha-297b7da');
+const VERSION = new Version('10.0.0-next.2+18.sha-305b059');
 
 /**
  * @fileoverview added by tsickle
