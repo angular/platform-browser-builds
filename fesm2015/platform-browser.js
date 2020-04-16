49d970a84ff9c1e280a9c8e46b120f6c189b901d
atscott <atscott@google.com>
2020-04-16 17:03:19 +0000

    03ff380e13 docs: add " in architecture page (#36564)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+1.sha-c6e5fc4
+ * @license Angular v10.0.0-next.2+2.sha-03ff380
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3243,7 +3243,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+1.sha-c6e5fc4');
+const VERSION = new Version('10.0.0-next.2+2.sha-03ff380');
 
 /**
  * @fileoverview added by tsickle
