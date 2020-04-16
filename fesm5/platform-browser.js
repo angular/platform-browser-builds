4b85835df44af2e222f156114229de91ce509ed1
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 21:35:06 +0000

    4480ba3e29 ci: remove reliance on Github API for CI setup (#36500)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+31.sha-5e79799
+ * @license Angular v10.0.0-next.2+32.sha-4480ba3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2028,7 +2028,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+31.sha-5e79799');
+var VERSION = new Version('10.0.0-next.2+32.sha-4480ba3');
 
 /**
  * @license
