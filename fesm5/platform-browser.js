5fe20ccf66a291e5bd003a28bee4ed1d3b534dff
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:51:21 +0000

    acf6075ca9 fix(core): do not use unbound attributes as inputs to structural directives (#36441)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+54.sha-28995db
+ * @license Angular v10.0.0-next.2+55.sha-acf6075
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2028,7 +2028,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+54.sha-28995db');
+var VERSION = new Version('10.0.0-next.2+55.sha-acf6075');
 
 /**
  * @license
