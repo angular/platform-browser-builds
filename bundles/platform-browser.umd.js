edc3cb77550a0a4e817c8a6d01acc1e3debb1a2c
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:39:51 +0000

    f898c9ab57 build: require a commit body in commit messages (#36632)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+9.sha-7743c43
+ * @license Angular v9.1.3+15.sha-f898c9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2243,7 +2243,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.3+9.sha-7743c43');
+    var VERSION = new i0.Version('9.1.3+15.sha-f898c9a');
 
     /**
      * @license
