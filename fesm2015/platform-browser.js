d2fab46405ea31afd99728a83f56a154715d174e
Andrew Kushnir <akushnir@google.com>
2020-04-27 22:19:23 +0000

    f27deea003 fix(core): handle synthetic props in Directive host bindings correctly (#35568)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+35.sha-00e6cb1
+ * @license Angular v10.0.0-next.3+36.sha-f27deea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3243,7 +3243,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+35.sha-00e6cb1');
+const VERSION = new Version('10.0.0-next.3+36.sha-f27deea');
 
 /**
  * @fileoverview added by tsickle
