f6210dcbf79424ce2c9fcbaefc5daee78ec818d0
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:52:49 +0000

    c8c2272a9f fix(core): Refresh transplanted views at insertion point only (#35968)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4
+ * @license Angular v9.1.4+14.sha-c8c2272
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3243,7 +3243,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.4');
+const VERSION = new Version('9.1.4+14.sha-c8c2272');
 
 /**
  * @fileoverview added by tsickle
