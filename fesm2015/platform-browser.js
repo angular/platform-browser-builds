697ecea62a175e356af9c0f2dc289282146af1f4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:11:58 +0000

    db4c59dad9 fix(ngcc): support TS 3.9 wrapped ES2015 classes (#36884)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+38.sha-58ea040
+ * @license Angular v10.0.0-next.4+39.sha-db4c59d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3243,7 +3243,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+38.sha-58ea040');
+const VERSION = new Version('10.0.0-next.4+39.sha-db4c59d');
 
 /**
  * @fileoverview added by tsickle
