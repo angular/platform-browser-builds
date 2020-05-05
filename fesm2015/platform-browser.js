37dc5d01690b4433f6a8b119e02cda04316ca8cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:36:44 +0000

    e30e1325f3 fix(core): properly get root nodes from embedded views with <ng-content> (#36051)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+20.sha-fc6c3ae
+ * @license Angular v10.0.0-next.5+21.sha-e30e132
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3243,7 +3243,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+20.sha-fc6c3ae');
+const VERSION = new Version('10.0.0-next.5+21.sha-e30e132');
 
 /**
  * @fileoverview added by tsickle
