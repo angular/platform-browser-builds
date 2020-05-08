95b6607e3def4ffb698c783bbd4212dbd4ee7e8e
Misko Hevery <misko@hevery.com>
2020-05-08 21:58:54 +0000

    c184de175d build: migrate to typescript file for ng-dev config (#37017)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+8.sha-de0c71c
+ * @license Angular v10.0.0-next.6+10.sha-c184de1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3243,7 +3243,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+8.sha-de0c71c');
+const VERSION = new Version('10.0.0-next.6+10.sha-c184de1');
 
 /**
  * @fileoverview added by tsickle
