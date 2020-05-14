6d376381e3f098bb843cd44bff9615527e26f252
Kara Erickson <karakara@google.com>
2020-05-14 20:25:28 +0000

    719ca5283b fix(dev-infra): do not require a commit body for release commits (#37110)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+35.sha-3813f54
+ * @license Angular v9.1.6+36.sha-719ca52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3243,7 +3243,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.6+35.sha-3813f54');
+const VERSION = new Version('9.1.6+36.sha-719ca52');
 
 /**
  * @fileoverview added by tsickle
