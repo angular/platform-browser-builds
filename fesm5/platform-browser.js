79df1b1b0155f46648f7c8e24fac24de0ae437fb
Kara Erickson <karakara@google.com>
2020-05-15 18:07:58 +0000

    9548c7cb94 release: cut the v9.1.7 release (#37116)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+47.sha-5ffa4f1
+ * @license Angular v9.1.6+49.sha-9548c7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2028,7 +2028,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.6+47.sha-5ffa4f1');
+var VERSION = new Version('9.1.6+49.sha-9548c7c');
 
 /**
  * @license
