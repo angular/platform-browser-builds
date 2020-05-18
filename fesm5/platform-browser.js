f4b99666d9d5eb9374d5f45fd8f224bb5e2f1393
Kara Erickson <karakara@google.com>
2020-05-18 23:45:33 +0000

    c4234577db docs(router): corrected minor typo (#37166)

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+70.sha-178a78f
+ * @license Angular v9.1.6+74.sha-c423457
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2028,7 +2028,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.6+70.sha-178a78f');
+var VERSION = new Version('9.1.6+74.sha-c423457');
 
 /**
  * @license
