d5ade3279a75cedab04474153172cc9d65480a5d
Kara Erickson <karakara@google.com>
2020-05-18 17:51:31 +0000

    593e07bd21 build(docs-infra): upgrade cli command docs sources to dbde51375 (#37154)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+7.sha-2ca2c8f
+ * @license Angular v10.0.0-next.8+9.sha-593e07b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3613,7 +3613,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+7.sha-2ca2c8f');
+const VERSION = new Version('10.0.0-next.8+9.sha-593e07b');
 
 /**
  * @fileoverview added by tsickle
