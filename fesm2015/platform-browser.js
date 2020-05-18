74a6e5745d38e2186c0398745167c1fcd6b54f4f
Kara Erickson <karakara@google.com>
2020-05-18 19:16:11 +0000

    c2c14c2a1f fix(dev-infra): incorrect base revision for merge script autosquash (#37138)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+10.sha-22d80a6
+ * @license Angular v10.0.0-next.8+15.sha-c2c14c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3613,7 +3613,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+10.sha-22d80a6');
+const VERSION = new Version('10.0.0-next.8+15.sha-c2c14c2');
 
 /**
  * @fileoverview added by tsickle
