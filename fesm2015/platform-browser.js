4599deb2ae7dd01802f38d208cfc0c6f44bab2dd
Kara Erickson <karakara@google.com>
2020-05-19 22:57:44 +0000

    211d33ad59 build: fix deprecation notice in scripts/github/merge-pr script (#37208)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+29.sha-383f04b
+ * @license Angular v10.0.0-next.8+30.sha-211d33a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3613,7 +3613,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+29.sha-383f04b');
+const VERSION = new Version('10.0.0-next.8+30.sha-211d33a');
 
 /**
  * @fileoverview added by tsickle
