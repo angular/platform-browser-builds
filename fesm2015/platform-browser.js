79a34919b1c5cd2610d08f6554d93a40e62fe066
Kara Erickson <karakara@google.com>
2020-05-20 00:06:26 +0000

    325ad7eb53 release: cut the v10.0.0-next.9 release

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+31.sha-362fba4
+ * @license Angular v10.0.0-next.9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3397,7 +3397,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+31.sha-362fba4');
+const VERSION = new Version('10.0.0-next.9');
 
 /**
  * @fileoverview added by tsickle
