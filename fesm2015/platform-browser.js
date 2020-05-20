671a46779300e076d6f2d9ba587464aacb3fa6ff
Kara Erickson <karakara@google.com>
2020-05-20 23:18:42 +0000

    b797913d10 release: cut the v9.1.9 release

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.8+19.sha-1465372
+ * @license Angular v9.1.9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3243,7 +3243,7 @@ function elementMatches(n, selector) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.8+19.sha-1465372');
+const VERSION = new Version('9.1.9');
 
 /**
  * @fileoverview added by tsickle
