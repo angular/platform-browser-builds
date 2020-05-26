5d5b39c86581f4113a0e9dadcc3ba2c75a5f8c04
Matias Niemelä <matias@yearofmoo.com>
2020-05-26 18:54:53 +0000

    ecce99470a build: deprecate old merge script (#37247)

--- bundles/platform-browser-animations.umd.js
+++ bundles/platform-browser-animations.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+17.sha-d1ea1f4
+ * @license Angular v10.0.0-rc.0+15.sha-ecce994
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -657,7 +657,7 @@
 
     /**
      * @license
-     * Copyright Google LLC All Rights Reserved.
+     * Copyright Google Inc. All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
@@ -704,7 +704,7 @@
 
     /**
      * @license
-     * Copyright Google LLC All Rights Reserved.
+     * Copyright Google Inc. All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
@@ -712,7 +712,7 @@
 
     /**
      * @license
-     * Copyright Google LLC All Rights Reserved.
+     * Copyright Google Inc. All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
@@ -720,7 +720,7 @@
 
     /**
      * @license
-     * Copyright Google LLC All Rights Reserved.
+     * Copyright Google Inc. All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
@@ -728,7 +728,7 @@
 
     /**
      * @license
-     * Copyright Google LLC All Rights Reserved.
+     * Copyright Google Inc. All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
--- bundles/platform-browser-testing.umd.js
+++ bundles/platform-browser-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+17.sha-d1ea1f4
+ * @license Angular v10.0.0-rc.0+15.sha-ecce994
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -240,7 +240,7 @@
 
     /**
      * @license
-     * Copyright Google LLC All Rights Reserved.
+     * Copyright Google Inc. All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
@@ -535,7 +535,7 @@
 
     /**
      * @license
-     * Copyright Google LLC All Rights Reserved.
+     * Copyright Google Inc. All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
@@ -582,7 +582,7 @@
 
     /**
      * @license
-     * Copyright Google LLC All Rights Reserved.
+     * Copyright Google Inc. All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
@@ -590,7 +590,7 @@
 
     /**
      * @license
-     * Copyright Google LLC All Rights Reserved.
+     * Copyright Google Inc. All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
@@ -598,7 +598,7 @@
 
     /**
      * @license
-     * Copyright Google LLC All Rights Reserved.
+     * Copyright Google Inc. All Rights Reserved.
      *
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
