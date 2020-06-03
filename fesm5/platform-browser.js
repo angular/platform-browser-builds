919ec7517a24f88688f5e2943aacec6948b30348
Igor Minar <igor@angular.io>
2020-06-03 00:56:19 +0000

    6855396449 docs: update aio in support for #BlackLivesMatter

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.9+13.sha-0b96908
+ * @license Angular v9.1.9+14.sha-6855396
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2028,7 +2028,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.9+13.sha-0b96908');
+var VERSION = new Version('9.1.9+14.sha-6855396');
 
 /**
  * @license
