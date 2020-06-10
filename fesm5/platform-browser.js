1be5aaf4eedef33ae5ce9481d4f9c565719ee40b
atscott <atscott01@gmail.com>
2020-06-10 18:23:22 +0000

    dc9da17f37 Revert "fix(elements): fire custom element output events during component initialization"

--- fesm5/platform-browser.js
+++ fesm5/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.10
+ * @license Angular v9.1.10+1.sha-dc9da17
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2028,7 +2028,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.10');
+var VERSION = new Version('9.1.10+1.sha-dc9da17');
 
 /**
  * @license
