da21e6c1d8c43a899ce9323dfdd6fa35601e1dda
atscott <atscott@google.com>
2020-06-10 15:44:03 +0000

    9d397eb5a1 Revert "build: remove wombot proxy registry from package.jsons for release (#37378)" (#37495)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+13.sha-6114cd2
+ * @license Angular v10.0.0-rc.3+14.sha-9d397eb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.3+13.sha-6114cd2');
+const VERSION = new Version('10.0.0-rc.3+14.sha-9d397eb');
 
 /**
  * @license
