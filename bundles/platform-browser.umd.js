5296ed4defbf1e7ece37a04c27d7f6a03fbb9dd7
Misko Hevery <misko@hevery.com>
2020-06-11 23:26:07 +0000

    3a43cdefe8 release: cut the v10.0.0-rc.5 release

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.4+14.sha-38c48be
+ * @license Angular v10.0.0-rc.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2279,7 +2279,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.4+14.sha-38c48be');
+    var VERSION = new i0.Version('10.0.0-rc.5');
 
     /**
      * @license
