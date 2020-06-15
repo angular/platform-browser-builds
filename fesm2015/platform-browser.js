ddbc13cd5186917c382901e81d7394e692e0291b
Misko Hevery <misko@hevery.com>
2020-06-15 21:54:36 +0000

    87a679b210 build: import in-memory-web-api project (#37182)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+185.sha-2a145f2
+ * @license Angular v10.0.0-rc.0+182.sha-87a679b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2048,7 +2048,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+185.sha-2a145f2');
+const VERSION = new Version('10.0.0-rc.0+182.sha-87a679b');
 
 /**
  * @license
