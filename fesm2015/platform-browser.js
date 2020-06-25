ba30ce4ef36de8eebe28ad20f5397341a635cf5c
Misko Hevery <misko@hevery.com>
2020-06-25 03:13:51 +0000

    0d2cdf6165 docs: add v9.angular.io to the angular.io version picker (#37719)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+2.sha-96891a0
+ * @license Angular v10.0.0+4.sha-0d2cdf6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2048,7 +2048,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0+2.sha-96891a0');
+const VERSION = new Version('10.0.0+4.sha-0d2cdf6');
 
 /**
  * @license
