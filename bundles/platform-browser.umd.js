3718d9e0bd67dc4f02aaaf1a2d6842acdec88f7d
Igor Minar <igor@angular.io>
2020-06-25 21:08:07 +0000

    0feb560136 fixup! feat(docs-infra): update deploy-to-firebase.sh script to support v9 multisite setup

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+1.sha-6c1ab47
+ * @license Angular v9.1.11+3.sha-0feb560
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2243,7 +2243,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.11+1.sha-6c1ab47');
+    var VERSION = new i0.Version('9.1.11+3.sha-0feb560');
 
     /**
      * @license
