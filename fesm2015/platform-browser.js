e3c99b220825b58223012baa84ecd8b6d8274178
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:38:03 +0000

    a5d5f67be7 fix(http): avoid abort a request when fetch operation is completed (#37367)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+4.sha-0d2cdf6
+ * @license Angular v10.0.0+15.sha-a5d5f67
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2048,7 +2048,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0+4.sha-0d2cdf6');
+const VERSION = new Version('10.0.0+15.sha-a5d5f67');
 
 /**
  * @license
