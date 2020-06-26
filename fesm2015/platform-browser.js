5b3300605e22419101dd8412e8acb617b7ab08dd
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:22:18 +0000

    c942662d79 fix(language-service): reinstate getExternalFiles() (#37750)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+273.sha-f954ab6
+ * @license Angular v10.0.0-rc.0+275.sha-c942662
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2048,7 +2048,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+273.sha-f954ab6');
+const VERSION = new Version('10.0.0-rc.0+275.sha-c942662');
 
 /**
  * @license
