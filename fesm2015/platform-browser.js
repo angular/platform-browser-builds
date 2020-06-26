de51b7ff8f91b7270aaffe367bef5c798d4750a5
Andrew Kushnir <akushnir@google.com>
2020-06-26 20:54:01 +0000

    07b5df3a19 release: cut the v10.0.1 release

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+34.sha-e702372
+ * @license Angular v10.0.1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2048,7 +2048,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0+34.sha-e702372');
+const VERSION = new Version('10.0.1');
 
 /**
  * @license
