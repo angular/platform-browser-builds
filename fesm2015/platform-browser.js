8a7d174631f77bac15858e8512d409344ce47f9b
Andrew Kushnir <akushnir@google.com>
2020-06-29 19:44:31 +0000

    7a91b23cb5 fix(core): fake_async_fallback should have the same logic with fake-async (#37680)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+10.sha-89e16ed
+ * @license Angular v10.0.1+16.sha-7a91b23
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2048,7 +2048,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.1+10.sha-89e16ed');
+const VERSION = new Version('10.0.1+16.sha-7a91b23');
 
 /**
  * @license
