394f5f42bcadfbf73a49b3abe83fdfe6e1fe4b87
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-07 19:43:11 +0000

    cb2ab26296 feat(dev-infra): merge script should link to original commit when cherry-picking with API strategy (#37889)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+320.sha-ec32eba
+ * @license Angular v10.0.0-rc.0+324.sha-cb2ab26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2105,7 +2105,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+320.sha-ec32eba');
+const VERSION = new Version('10.0.0-rc.0+324.sha-cb2ab26');
 
 /**
  * @license
