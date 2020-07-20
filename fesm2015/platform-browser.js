b382636fdd5796050d067894b306e1d90f56ee2a
Andrew Kushnir <akushnir@google.com>
2020-07-20 17:25:28 +0000

    e80278cf02 fix(compiler): properly associate source spans for implicitly closed elements (#38126)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+23.sha-8e305e7
+ * @license Angular v10.0.4+27.sha-e80278c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2048,7 +2048,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4+23.sha-8e305e7');
+const VERSION = new Version('10.0.4+27.sha-e80278c');
 
 /**
  * @license
