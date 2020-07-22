4d93784c122f87e5aad02fa69cf0f331dfeb8750
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:41:29 +0000

    6ec7297e43 build(docs-infra): remove boilerplate file listings in `example-boilerplate.js` (#38173)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5
+ * @license Angular v10.0.5+7.sha-6ec7297
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2048,7 +2048,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5');
+const VERSION = new Version('10.0.5+7.sha-6ec7297');
 
 /**
  * @license
