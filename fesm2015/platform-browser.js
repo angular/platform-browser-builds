bae54f1cb368e5316b8a01361fe4df97eb80a024
Misko Hevery <misko@hevery.com>
2020-07-24 22:42:06 +0000

    7467fd36b9 fix(zone.js): clearTimeout/clearInterval should call on object global (#37858)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+18.sha-aca0198
+ * @license Angular v10.0.5+19.sha-7467fd3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2048,7 +2048,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5+18.sha-aca0198');
+const VERSION = new Version('10.0.5+19.sha-7467fd3');
 
 /**
  * @license
