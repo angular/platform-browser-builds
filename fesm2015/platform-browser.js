3a777109aa4d92fb6a0eb9e32f04f4c620d78f28
Misko Hevery <misko@hevery.com>
2020-07-24 15:39:33 +0000

    b8af10902f docs: fixed that class attribute is not closed (#38219)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+15.sha-f411c9e
+ * @license Angular v10.0.5+16.sha-b8af109
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2048,7 +2048,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5+15.sha-f411c9e');
+const VERSION = new Version('10.0.5+16.sha-b8af109');
 
 /**
  * @license
