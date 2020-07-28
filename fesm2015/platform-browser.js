0bda6bf8f691f9c5698c7b62a0d08ff1edc4a54d
Misko Hevery <misko@hevery.com>
2020-07-28 22:30:34 +0000

    28c3e2d15d docs: release notes for the v10.0.6 release

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+58.sha-2a45b93
+ * @license Angular v10.1.0-next.2+59.sha-28c3e2d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2045,7 +2045,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+58.sha-2a45b93');
+const VERSION = new Version('10.1.0-next.2+59.sha-28c3e2d');
 
 /**
  * @license
