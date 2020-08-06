03bf07ce05c0f8c046dae2d646973d1eaf7e0d94
Andrew Kushnir <akushnir@google.com>
2020-08-06 16:28:13 +0000

    016a41b14d fix(compiler-cli): mark eager `NgModuleFactory` construction as not side effectful (#38320)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+10.sha-58f4b3a
+ * @license Angular v10.0.8+14.sha-016a41b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+10.sha-58f4b3a');
+const VERSION = new Version('10.0.8+14.sha-016a41b');
 
 /**
  * @license
