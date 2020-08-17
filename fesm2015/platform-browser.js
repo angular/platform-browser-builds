acd5bee0a1fc29a3b3feb572e3bd6f341168df31
atscott <atscott01@gmail.com>
2020-08-17 22:56:50 +0000

    e472f5f688 refactor(ngcc): update yargs and typings for yargs (#38470)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6
+ * @license Angular v10.1.0-next.6+3.sha-e472f5f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6');
+const VERSION = new Version('10.1.0-next.6+3.sha-e472f5f');
 
 /**
  * @license
