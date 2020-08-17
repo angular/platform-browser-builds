9b6d030a2026489e7872814638410d4158b3d593
Andrew Scott <atscott@google.com>
2020-08-17 18:54:58 +0000

    84d1ba792b refactor(language-service): [Ivy] remove temporary compiler (#38310)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+17.sha-b32126c
+ * @license Angular v10.0.9+18.sha-84d1ba7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.9+17.sha-b32126c');
+const VERSION = new Version('10.0.9+18.sha-84d1ba7');
 
 /**
  * @license
