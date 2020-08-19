bab9aacde88cdea62cd2887dbc62ec19e176981f
atscott <atscott01@gmail.com>
2020-08-19 16:39:04 +0000

    a3f3082bf0 release: cut the v10.0.11 release

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10+9.sha-0570c24
+ * @license Angular v10.0.11
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.10+9.sha-0570c24');
+const VERSION = new Version('10.0.11');
 
 /**
  * @license
