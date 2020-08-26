49920becc47ac4bfabd8d7a1772fc0144d9bbfdb
Misko Hevery <misko@hevery.com>
2020-08-26 19:15:31 +0000

    7d8dce11c0 release: cut the v10.0.14 release

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.12+4.sha-9168919
+ * @license Angular v10.0.14
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.12+4.sha-9168919');
+const VERSION = new Version('10.0.14');
 
 /**
  * @license
