2ff25e4cd20f5ca6a846503f2989cd21418920d3
atscott <atscott01@gmail.com>
2020-09-08 22:13:37 +0000

    ba95b79a21 perf(compiler-cli): optimize computation of type-check scope information (#38539)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+43.sha-4360eed
+ * @license Angular v11.0.0-next.0+45.sha-ba95b79
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+43.sha-4360eed');
+const VERSION = new Version('11.0.0-next.0+45.sha-ba95b79');
 
 /**
  * @license
