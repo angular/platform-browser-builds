ae031ebdcfd5f04e2318325b692732b07bb54d27
Andrew Kushnir <akushnir@google.com>
2020-09-14 20:30:30 +0000

    ebede67433 perf(compiler-cli): optimize computation of type-check scope information (#38843)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+20.sha-c62d1cb
+ * @license Angular v10.1.1+22.sha-ebede67
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+20.sha-c62d1cb');
+const VERSION = new Version('10.1.1+22.sha-ebede67');
 
 /**
  * @license
