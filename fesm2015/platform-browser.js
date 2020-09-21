faa05a30349fecc1f35147cf09432f63209ee4de
Misko Hevery <misko@hevery.com>
2020-09-21 23:52:42 +0000

    3082f7378b test(compiler-cli): make typescript_ast_factory_spec tests resilient to line-endings (#38925)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+47.sha-297b123
+ * @license Angular v11.0.0-next.2+48.sha-3082f73
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+47.sha-297b123');
+const VERSION = new Version('11.0.0-next.2+48.sha-3082f73');
 
 /**
  * @license
