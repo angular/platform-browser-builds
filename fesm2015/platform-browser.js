c6795dcc59a17328153b351ea65acb18f4624edf
Misko Hevery <misko@hevery.com>
2020-09-22 22:38:09 +0000

    75610505c6 refactor(zone.js): remove usages of blacklist related to UNPATCHED_EVENTS (#38930)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+54.sha-b168252
+ * @license Angular v11.0.0-next.2+53.sha-7561050
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+54.sha-b168252');
+const VERSION = new Version('11.0.0-next.2+53.sha-7561050');
 
 /**
  * @license
