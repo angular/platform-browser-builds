5a4b5c5c2f4343f80f57faf6eb6fdad4ac517540
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 21:02:11 +0000

    875fcfe5a9 ci: note Paul and Pawel as OOO in pullapprove (#39028)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+13.sha-5bf5513
+ * @license Angular v10.1.3+49.sha-875fcfe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.3+13.sha-5bf5513');
+const VERSION = new Version('10.1.3+49.sha-875fcfe');
 
 /**
  * @license
