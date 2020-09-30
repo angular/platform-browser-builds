c64d30a6d2a5ba3124de0f681830ee7d2a78452c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 14:16:10 +0000

    a1bf0de711 release: cut the zone.js-0.11.2 release (#39032)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+60.sha-69302ad
+ * @license Angular v10.1.3+72.sha-a1bf0de
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.3+60.sha-69302ad');
+const VERSION = new Version('10.1.3+72.sha-a1bf0de');
 
 /**
  * @license
