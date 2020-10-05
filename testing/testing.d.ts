d9cb4a0963c48bc20fce7430c23a1ef88f5debf7
sajeetharan <sajeefx@gmail.com>
2020-10-05 19:32:24 +0000

    4bd836c87e updated sajee's profile

---
+++ testing/testing.d.ts
@@ -0,0 +1,28 @@
+/**
+ * @license Angular v11.0.0-next.3+23.sha-4bd836c
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { NgZone } from '@angular/core';
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class BrowserTestingModule {
+}
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformBrowserTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+export declare function ɵangular_packages_platform_browser_testing_testing_a(): NgZone;
+
+export { }
