5e76e912e770321cb7ff27039dd680e96590f4da
Joey Perrott <josephperrott@gmail.com>
2020-10-31 01:26:54 +0000

    71d0063285 fix(compiler-cli): report missing pipes when `fullTemplateTypeCheck` is disabled (#39320)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+14.sha-85d5242
+ * @license Angular v10.2.1+15.sha-71d0063
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.1+14.sha-85d5242');
+const VERSION = new Version('10.2.1+15.sha-71d0063');
 
 /**
  * @license
