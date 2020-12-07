f990f4e06db1754f1f1933b5e454d20df672749b
Misko Hevery <misko@hevery.com>
2020-12-07 18:08:48 +0000

    a8aeb74714 test(upgrade): run tests against AngularJS v1.8.x as well (#39972)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+49.sha-7954c8d
+ * @license Angular v11.1.0-next.1+50.sha-a8aeb74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2119,7 +2119,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+49.sha-7954c8d');
+const VERSION = new Version('11.1.0-next.1+50.sha-a8aeb74');
 
 /**
  * @license
