47a3fa65f3bb3a07f5f4cd756b6c8632d4e95ed7
Misko Hevery <misko@hevery.com>
2020-12-08 17:20:03 +0000

    25e3dc594d refactor(dev-infra): separate retrieving targeted branches from printing them (#39897)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+65.sha-e8d2348
+ * @license Angular v11.1.0-next.1+68.sha-25e3dc5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2119,7 +2119,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+65.sha-e8d2348');
+const VERSION = new Version('11.1.0-next.1+68.sha-25e3dc5');
 
 /**
  * @license
