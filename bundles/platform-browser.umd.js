df6233cca0a3ccdc11c7907c78ec6cd78e043139
Andrew Kushnir <akushnir@google.com>
2021-01-20 23:26:19 +0000

    59680c2644 release: cut the v11.1.0 release

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+39.sha-34092a5
+ * @license Angular v11.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2387,7 +2387,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-rc.0+39.sha-34092a5');
+    var VERSION = new i0.Version('11.1.0');
 
     /**
      * @license
