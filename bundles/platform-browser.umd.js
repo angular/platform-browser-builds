0ec7ea1103fd6cf6de512ba87021e7cd7d39bda5
Misko Hevery <misko@hevery.com>
2021-02-01 17:38:34 +0000

    caafac2248 fix(compiler-cli): preserve user line endings in diagnostic template parse (#40597)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.0+15.sha-4c5f18d
+ * @license Angular v11.2.0-next.0+18.sha-caafac2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2387,7 +2387,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.0-next.0+15.sha-4c5f18d');
+    var VERSION = new i0.Version('11.2.0-next.0+18.sha-caafac2');
 
     /**
      * @license
