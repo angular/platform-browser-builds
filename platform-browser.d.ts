3afbd005d53f46655f1836a5b0ade9cb2d4eccf9
atscott <atscott01@gmail.com>
2021-02-19 17:27:26 +0000

    d0b6270990 docs: edit Component lifecycle title (#40894)

--- platform-browser.d.ts
+++ platform-browser.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+12.sha-aed2782
+ * @license Angular v12.0.0-next.1+15.sha-d0b6270
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -806,6 +806,7 @@ export declare class ɵHammerGesturesPlugin extends ɵangular_packages_platform_
     private _config;
     private console;
     private loader?;
+    private _loaderPromise;
     constructor(doc: any, _config: HammerGestureConfig, console: ɵConsole, loader?: HammerLoader | null | undefined);
     supports(eventName: string): boolean;
     addEventListener(element: HTMLElement, eventName: string, handler: Function): Function;
