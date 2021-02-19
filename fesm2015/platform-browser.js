fca057170a002361c29be29e18387d72139ccfab
atscott <atscott01@gmail.com>
2021-02-19 22:57:20 +0000

    d1d1dadb41 refactor(core): use RuntimeError to throw provider not found error (#40901)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+34.sha-f340a5b
+ * @license Angular v12.0.0-next.1+35.sha-d1d1dad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2127,7 +2127,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+34.sha-f340a5b');
+const VERSION = new Version('12.0.0-next.1+35.sha-d1d1dad');
 
 /**
  * @license
