93a96227c6d39f64d6fac140c36a173371d085ca
atscott <atscott01@gmail.com>
2021-02-23 19:06:18 +0000

    ad38cbbe09 perf(language-service): Skip Angular analysis when quick info requested outside a template (#40956)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+53.sha-bd6f377
+ * @license Angular v12.0.0-next.1+49.sha-ad38cbb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2070,7 +2070,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+53.sha-bd6f377');
+const VERSION = new Version('12.0.0-next.1+49.sha-ad38cbb');
 
 /**
  * @license
