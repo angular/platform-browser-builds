364d93f3b423c70ed8527785c3f923cf719bede4
Zach Arend <zachzach@google.com>
2021-02-26 16:51:23 +0000

    89ac771652 fix(docs-infra): add links to glossary for terms on the "Press kit" page (#40945)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+4.sha-55eb7b5
+ * @license Angular v11.2.3+10.sha-89ac771
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2070,7 +2070,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.3+4.sha-55eb7b5');
+const VERSION = new Version('11.2.3+10.sha-89ac771');
 
 /**
  * @license
