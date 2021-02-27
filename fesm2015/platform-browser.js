94c786901db96b44e947d698b331e8df6602724a
Zach Arend <zachzach@google.com>
2021-02-27 00:02:11 +0000

    2e0dea6710 docs: improve description, examples of DecimalPipe's digitsInfo parameter (#40714)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+10.sha-89ac771
+ * @license Angular v11.2.3+28.sha-2e0dea6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2070,7 +2070,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.3+10.sha-89ac771');
+const VERSION = new Version('11.2.3+28.sha-2e0dea6');
 
 /**
  * @license
