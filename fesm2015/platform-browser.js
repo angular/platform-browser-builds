0ee95e07f104a2341bcac0e44dd994a38452432f
Zach Arend <zachzach@google.com>
2021-03-03 23:13:09 +0000

    2028a77dc8 release: cut the v11.2.4 release

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+49.sha-9322e6a
+ * @license Angular v11.2.4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2071,7 +2071,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.3+49.sha-9322e6a');
+const VERSION = new Version('11.2.4');
 
 /**
  * @license
