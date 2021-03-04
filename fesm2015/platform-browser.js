a8fef153fb7e7e267e6e5560693e9913e63597b0
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:28:04 +0000

    1d4fc94801 refactor(compiler): remove unused `__BUILD_OPTIMIZER_*` constants (#41040)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+3.sha-4f4454d
+ * @license Angular v11.2.4+11.sha-1d4fc94
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2071,7 +2071,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.4+3.sha-4f4454d');
+const VERSION = new Version('11.2.4+11.sha-1d4fc94');
 
 /**
  * @license
