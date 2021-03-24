bcb11d8da53e3495142208d26be9c5aff6813113
Joey Perrott <josephperrott@gmail.com>
2021-03-24 15:49:49 +0000

    81a0e7145b ci: fix `elements` test on older browsers (#41324)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+38.sha-a92a89b
+ * @license Angular v12.0.0-next.5+39.sha-81a0e71
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2081,7 +2081,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+38.sha-a92a89b');
+const VERSION = new Version('12.0.0-next.5+39.sha-81a0e71');
 
 /**
  * @license
