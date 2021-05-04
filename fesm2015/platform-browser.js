a95d6024b5d42d77823da9760a5a8b240a51e0d0
Misko Hevery <misko@hevery.com>
2021-05-04 15:37:12 +0000

    6d7d659919 docs: add new migration guide to v12 documentation that describes how to migrate older localization ids to new ones (#41828)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+318.sha-b7db98f
+ * @license Angular v12.0.0-next.8+319.sha-6d7d659
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2042,7 +2042,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+318.sha-b7db98f');
+const VERSION = new Version('12.0.0-next.8+319.sha-6d7d659');
 
 /**
  * @license
