dedeb03310a7601403c2abe3a5011a91c4791545
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 23:30:38 +0000

    5f7a401fef release: cut the v11.2.14 release (#42070)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.13+29.sha-5f7a401
+ * @license Angular v11.2.14
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2081,7 +2081,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.13+29.sha-5f7a401');
+const VERSION = new Version('11.2.14');
 
 /**
  * @license
