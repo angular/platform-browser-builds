ad39211d2f76934e8ebe3f42b23c0013bb90b46f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 18:23:27 +0000

    a7ff02f014 build(docs-infra): show a dist-tag when installing the CLI on non-stable versions (#42040)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.5+1.sha-318e7b8
+ * @license Angular v10.2.5+2.sha-a7ff02f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2051,7 +2051,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.5+1.sha-318e7b8');
+const VERSION = new Version('10.2.5+2.sha-a7ff02f');
 
 /**
  * @license
