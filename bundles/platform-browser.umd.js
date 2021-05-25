50846d46d254a5e5698b9afd39afceaab289a624
Zach Arend <zachzach@google.com>
2021-05-25 18:27:44 +0000

    c01da2176d docs: edit copy for tick() in testing and api docs (#35697)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+38.sha-12b1e87
+ * @license Angular v12.0.1+66.sha-c01da21
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2392,7 +2392,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.1+38.sha-12b1e87');
+    var VERSION = new i0.Version('12.0.1+66.sha-c01da21');
 
     /**
      * @license
