13969d4042c1b87adc971bed3a7096c6a3a18a5b
Zach Arend <zachzach@google.com>
2021-05-25 03:04:24 +0000

    1c3fe69f92 docs: change Zeit -> Vercel (#42285)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+38.sha-a391698
+ * @license Angular v12.1.0-next.2+40.sha-1c3fe69
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2108,7 +2108,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+38.sha-a391698');
+const VERSION = new Version('12.1.0-next.2+40.sha-1c3fe69');
 
 /**
  * @license
