83cdbe29bb325a71bb080173fe79d1ed143e3b9f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:04:53 +0000

    a77ec5bcab fix(router): fix `serializeQueryParams` logic (#42481)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+44.sha-cba68e3
+ * @license Angular v12.0.3+50.sha-a77ec5b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2392,7 +2392,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.3+44.sha-cba68e3');
+    var VERSION = new i0.Version('12.0.3+50.sha-a77ec5b');
 
     /**
      * @license
