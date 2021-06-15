e53c7c6e3fd6526fd33301658a9312197fe83af2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-15 20:11:23 +0000

    11e0f53352 fix(language-service): Use last child end span for parent without close tag (#42554)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+19.sha-18d8322
+ * @license Angular v12.0.4+21.sha-11e0f53
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2052,7 +2052,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.4+19.sha-18d8322');
+const VERSION = new Version('12.0.4+21.sha-11e0f53');
 
 /**
  * @license
