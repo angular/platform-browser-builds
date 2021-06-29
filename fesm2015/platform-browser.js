a2ab19eabd7315b1c4c2a6331c34ec1fd6043daf
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 16:46:43 +0000

    ace0a50646 docs: remove duplicate link to `guide/inputs-outputs.md` (#42654)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0+12.sha-0f27f89
+ * @license Angular v12.1.0+13.sha-ace0a50
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2057,7 +2057,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0+12.sha-0f27f89');
+const VERSION = new Version('12.1.0+13.sha-ace0a50');
 
 /**
  * @license
