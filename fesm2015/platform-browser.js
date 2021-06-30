db5ce4000a7b9ae80b5d381b6bc121d6c3022722
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 19:04:04 +0000

    aab41ca42f build: update API goldens to reflect new tool (#42688)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0+18.sha-d19ddd1
+ * @license Angular v12.1.0+22.sha-aab41ca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2057,7 +2057,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0+18.sha-d19ddd1');
+const VERSION = new Version('12.1.0+22.sha-aab41ca');
 
 /**
  * @license
