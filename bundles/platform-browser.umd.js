cc9843bb71cf4834ca68b47d7db086131673da77
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 16:33:55 +0000

    c380d56c6d docs: setting context value inside an interceptor (#42699)

--- bundles/platform-browser.umd.js
+++ bundles/platform-browser.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0+16.sha-f84d826
+ * @license Angular v12.1.0+17.sha-c380d56
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2403,7 +2403,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0+16.sha-f84d826');
+    var VERSION = new i0.Version('12.1.0+17.sha-c380d56');
 
     /**
      * @license
