d1e005352015263c568971dcb44a2a68002283e0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 19:16:53 +0000

    29f50976db fix(docs-infra): fix keyword color in CLI pages code blocks (#42889)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.2+29.sha-e6b7472
+ * @license Angular v12.1.2+33.sha-29f5097
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2057,7 +2057,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.2+29.sha-e6b7472');
+const VERSION = new Version('12.1.2+33.sha-29f5097');
 
 /**
  * @license
