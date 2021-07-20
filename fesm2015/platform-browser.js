e65b3150b46f7956acc9e560c359a74ebb7edc0d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:56:53 +0000

    307dac67bc fix(core): use correct injector when resolving DI tokens from within a directive provider factory (#42886)

--- fesm2015/platform-browser.js
+++ fesm2015/platform-browser.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+38.sha-722eb5d
+ * @license Angular v12.2.0-next.2+39.sha-307dac6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2057,7 +2057,7 @@ function elementMatches(n, selector) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.2+38.sha-722eb5d');
+const VERSION = new Version('12.2.0-next.2+39.sha-307dac6');
 
 /**
  * @license
