0d6b474b754f3604bc37bdd9c7811e9fe21f97f6
Andrew Scott <atscott01@gmail.com>
2022-01-07 22:32:53 +0000

    abc217b28e fix(animations): retain triggers values for moved tracked list items (#44578)

--- esm2020/animations/src/providers.mjs
+++ esm2020/animations/src/providers.mjs
@@ -22,9 +22,9 @@ export class InjectableAnimationEngine extends AnimationEngine {
         this.flush();
     }
 }
-InjectableAnimationEngine.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.1.1+79.sha-919f8ae.with-local-changes", ngImport: i0, type: InjectableAnimationEngine, deps: [{ token: DOCUMENT }, { token: i1.AnimationDriver }, { token: i1.ɵAnimationStyleNormalizer }], target: i0.ɵɵFactoryTarget.Injectable });
-InjectableAnimationEngine.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.1.1+79.sha-919f8ae.with-local-changes", ngImport: i0, type: InjectableAnimationEngine });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.1.1+79.sha-919f8ae.with-local-changes", ngImport: i0, type: InjectableAnimationEngine, decorators: [{
+InjectableAnimationEngine.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.1.1+80.sha-abc217b.with-local-changes", ngImport: i0, type: InjectableAnimationEngine, deps: [{ token: DOCUMENT }, { token: i1.AnimationDriver }, { token: i1.ɵAnimationStyleNormalizer }], target: i0.ɵɵFactoryTarget.Injectable });
+InjectableAnimationEngine.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.1.1+80.sha-abc217b.with-local-changes", ngImport: i0, type: InjectableAnimationEngine });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.1.1+80.sha-abc217b.with-local-changes", ngImport: i0, type: InjectableAnimationEngine, decorators: [{
             type: Injectable
         }], ctorParameters: function () { return [{ type: undefined, decorators: [{
                     type: Inject,
