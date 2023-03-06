e9f2f6bb8d0149c64819363c2e2132935a8bf38e
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-06 19:04:56 +0000

    2312eb53ef fix(platform-browser): KeyEventsPlugin should keep the same behavior (#49330)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-10ffd03
+ * @license Angular v16.0.0-next.1+sha-2312eb5
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -871,7 +871,7 @@ export declare class ɵKeyEventsPlugin extends EventManagerPlugin {
      * @param zone The zone in which the event occurred.
      * @returns A callback function.
      */
-    static eventCallback(fullKey: string, handler: Function, zone: NgZone): Function;
+    static eventCallback(fullKey: string, handler: Function): Function;
     static ɵfac: i0.ɵɵFactoryDeclaration<ɵKeyEventsPlugin, never>;
     static ɵprov: i0.ɵɵInjectableDeclaration<ɵKeyEventsPlugin>;
 }
