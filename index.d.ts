d2a46d1926bc6275ca7e4edffbc704d4ada35af2
Andrew Kushnir <akushnir@google.com>
2023-08-17 17:30:53 +0000

    e6503930f1 docs: fix see also links. (#51379)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-5fb5f08
+ * @license Angular v17.0.0-next.0+sha-e650393
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -469,7 +469,7 @@ export declare interface HydrationFeature<FeatureKind extends HydrationFeatureKi
 
 /**
  * The list of features as an enum to uniquely type each `HydrationFeature`.
- * @see HydrationFeature
+ * @see {@link HydrationFeature}
  *
  * @publicApi
  * @developerPreview
