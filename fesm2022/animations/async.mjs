7a62fd66b18a7b975af94ba13a61bd56e0e4de55
Andrew Kushnir <akushnir@google.com>
2024-06-13 16:05:20 +0000

    ef2ad1b refactor: add select scenario to the js-web-frameworks benchmark (#56362)

--- fesm2022/animations/async.mjs
+++ fesm2022/animations/async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.2+sha-a114eba
+ * @license Angular v18.1.0-next.2+sha-ef2ad1b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -104,10 +104,10 @@ class AsyncAnimationRendererFactory {
     whenRenderingDone() {
         return this.delegate.whenRenderingDone?.() ?? Promise.resolve();
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "18.1.0-next.2+sha-a114eba", ngImport: i0, type: AsyncAnimationRendererFactory, deps: "invalid", target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "18.1.0-next.2+sha-a114eba", ngImport: i0, type: AsyncAnimationRendererFactory }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "18.1.0-next.2+sha-ef2ad1b", ngImport: i0, type: AsyncAnimationRendererFactory, deps: "invalid", target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "18.1.0-next.2+sha-ef2ad1b", ngImport: i0, type: AsyncAnimationRendererFactory }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "18.1.0-next.2+sha-a114eba", ngImport: i0, type: AsyncAnimationRendererFactory, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "18.1.0-next.2+sha-ef2ad1b", ngImport: i0, type: AsyncAnimationRendererFactory, decorators: [{
             type: Injectable
         }], ctorParameters: () => [{ type: Document }, { type: i0.RendererFactory2 }, { type: i0.NgZone }, { type: undefined }, { type: Promise }] });
 /**
