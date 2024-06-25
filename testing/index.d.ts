8c5a236630763caaee972072bffbcccfb3abbf90
Dylan Hunn <dylhunn@gmail.com>
2024-06-25 16:35:26 +0000

    ccc8c80 build: update to TypeScript 5.5 stable (#56358)

--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.1.0-next.3+sha-5be16d0
+ * @license Angular v18.1.0-next.3+sha-ccc8c80
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -26,6 +26,6 @@ export declare class BrowserTestingModule {
  *
  * @publicApi
  */
-export declare const platformBrowserTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+export declare const platformBrowserTesting: (extraProviders?: StaticProvider[]) => PlatformRef;
 
 export { }
