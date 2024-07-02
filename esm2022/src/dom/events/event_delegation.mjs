9fb0abd3caf6e9beddca26eb7a0fc72f63bef78f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:17:07 +0000

    4343cd2 fix(router): routes should not get stale providers (#56798)

--- esm2022/src/dom/events/event_delegation.mjs
+++ esm2022/src/dom/events/event_delegation.mjs
@@ -25,10 +25,10 @@ export class EventDelegationPlugin extends EventManagerPlugin {
     removeEventListener(element, eventName, callback) {
         return this.delegate.removeEventListener(element, eventName, callback);
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "18.2.0-next.0+sha-8fa6f40", ngImport: i0, type: EventDelegationPlugin, deps: [{ token: DOCUMENT }], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "18.2.0-next.0+sha-8fa6f40", ngImport: i0, type: EventDelegationPlugin }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "18.2.0-next.0+sha-4343cd2", ngImport: i0, type: EventDelegationPlugin, deps: [{ token: DOCUMENT }], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "18.2.0-next.0+sha-4343cd2", ngImport: i0, type: EventDelegationPlugin }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "18.2.0-next.0+sha-8fa6f40", ngImport: i0, type: EventDelegationPlugin, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "18.2.0-next.0+sha-4343cd2", ngImport: i0, type: EventDelegationPlugin, decorators: [{
             type: Injectable
         }], ctorParameters: () => [{ type: undefined, decorators: [{
                     type: Inject,
