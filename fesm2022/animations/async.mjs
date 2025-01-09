72792811f5082d7a5696062f9564055af9c6e633
kirjs <kirjs@users.noreply.github.com>
2025-01-09 15:02:59 +0000

    28a5a89 build: clean up pipeline-specific tests (#59450)

--- fesm2022/animations/async.mjs
+++ fesm2022/animations/async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.6+sha-21a9f53
+ * @license Angular v19.0.6+sha-28a5a89
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -122,10 +122,10 @@ class AsyncAnimationRendererFactory {
     whenRenderingDone() {
         return this.delegate.whenRenderingDone?.() ?? Promise.resolve();
     }
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "19.0.6+sha-21a9f53", ngImport: i0, type: AsyncAnimationRendererFactory, deps: "invalid", target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "19.0.6+sha-21a9f53", ngImport: i0, type: AsyncAnimationRendererFactory });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "19.0.6+sha-28a5a89", ngImport: i0, type: AsyncAnimationRendererFactory, deps: "invalid", target: i0.ɵɵFactoryTarget.Injectable });
+    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "19.0.6+sha-28a5a89", ngImport: i0, type: AsyncAnimationRendererFactory });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.0.6+sha-21a9f53", ngImport: i0, type: AsyncAnimationRendererFactory, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.0.6+sha-28a5a89", ngImport: i0, type: AsyncAnimationRendererFactory, decorators: [{
             type: Injectable
         }], ctorParameters: () => [{ type: Document }, { type: i0.RendererFactory2 }, { type: i0.NgZone }, { type: undefined }, { type: Promise }] });
 /**
