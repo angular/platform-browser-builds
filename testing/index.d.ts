290996f4ad051ecc3783b919001330aa9d2a6af7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 18:18:21 +0000

    0362665 refactor(core): move more host directive matching logic into feature (#60452)

--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.2+sha-c7cacbf
+ * @license Angular v20.0.0-next.2+sha-0362665
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7,6 +7,7 @@
 import * as i0 from '@angular/core';
 import { StaticProvider } from '@angular/core';
 import * as i1 from '@angular/platform-browser';
+import { TestComponentRenderer } from '@angular/core/testing';
 
 /**
  * Platform for testing
@@ -25,4 +26,17 @@ declare class BrowserTestingModule {
     static ɵinj: i0.ɵɵInjectorDeclaration<BrowserTestingModule>;
 }
 
-export { BrowserTestingModule, platformBrowserTesting };
+/**
+ * A DOM based implementation of the TestComponentRenderer.
+ */
+declare class DOMTestComponentRenderer extends TestComponentRenderer {
+    private _doc;
+    constructor(_doc: any);
+    insertRootElement(rootElId: string): void;
+    removeAllRootElements(): void;
+    private removeAllRootElementsImpl;
+    static ɵfac: i0.ɵɵFactoryDeclaration<DOMTestComponentRenderer, never>;
+    static ɵprov: i0.ɵɵInjectableDeclaration<DOMTestComponentRenderer>;
+}
+
+export { BrowserTestingModule, platformBrowserTesting, DOMTestComponentRenderer as ɵDOMTestComponentRenderer };
