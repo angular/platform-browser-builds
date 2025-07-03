23b6c8023d36b8e00e89f0d537ca90c210198b8c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 08:21:03 +0000

    99069c2 refactor(docs-infra): removed unused imports (#62441)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-e39bf3c
+ * @license Angular v20.2.0-next.0+sha-99069c2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -854,6 +854,7 @@ declare class KeyEventsPlugin extends EventManagerPlugin {
  */
 declare const enum RuntimeErrorCode {
     UNSUPPORTED_ZONEJS_INSTANCE = -5000,
+    HYDRATION_CONFLICTING_FEATURES = 5001,
     BROWSER_MODULE_ALREADY_LOADED = 5100,
     NO_PLUGIN_FOR_EVENT = 5101,
     UNSUPPORTED_EVENT_TARGET = 5102,
