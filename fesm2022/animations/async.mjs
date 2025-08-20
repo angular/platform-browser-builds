374ee9530a632af75b9c62ee2daa23c5fe8eb5a1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-08-20 12:18:41 +0000

    9267c2f release: cut the v20.1.8 release

--- fesm2022/animations/async.mjs
+++ fesm2022/animations/async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-cda402f
+ * @license Angular v20.1.8+sha-9267c2f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -131,10 +131,10 @@ class AsyncAnimationRendererFactory {
         this._engine?.flush();
         this.delegate.componentReplaced?.(componentId);
     }
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.1.7+sha-cda402f", ngImport: i0, type: AsyncAnimationRendererFactory, deps: "invalid", target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.1.7+sha-cda402f", ngImport: i0, type: AsyncAnimationRendererFactory });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.1.8+sha-9267c2f", ngImport: i0, type: AsyncAnimationRendererFactory, deps: "invalid", target: i0.ɵɵFactoryTarget.Injectable });
+    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.1.8+sha-9267c2f", ngImport: i0, type: AsyncAnimationRendererFactory });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.1.7+sha-cda402f", ngImport: i0, type: AsyncAnimationRendererFactory, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.1.8+sha-9267c2f", ngImport: i0, type: AsyncAnimationRendererFactory, decorators: [{
             type: Injectable
         }], ctorParameters: () => [{ type: Document }, { type: i0.RendererFactory2 }, { type: i0.NgZone }, { type: undefined }, { type: Promise }] });
 /**
