2f80153e28a5d2f7f62a7c58117272ebb032f43d
Andrew Kushnir <akushnir@google.com>
2025-08-22 21:49:14 +0000

    3df1dcc refactor: various build and import specificer fixes for strict deps (#63323)

--- fesm2022/platform-browser.mjs
+++ fesm2022/platform-browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-983cc2f
+ * @license Angular v21.0.0-next.0+sha-3df1dcc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -989,7 +989,7 @@ function provideClientHydration(...features) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-983cc2f');
+const VERSION = new Version('21.0.0-next.0+sha-3df1dcc');
 
 export { By, DomSanitizer, EVENT_MANAGER_PLUGINS, EventManagerPlugin, HAMMER_GESTURE_CONFIG, HAMMER_LOADER, HammerGestureConfig, HammerModule, HydrationFeatureKind, Meta, Title, VERSION, disableDebugTools, enableDebugTools, provideClientHydration, withEventReplay, withHttpTransferCacheOptions, withI18nSupport, withIncrementalHydration, withNoHttpTransferCache, DomSanitizerImpl as ɵDomSanitizerImpl, HammerGesturesPlugin as ɵHammerGesturesPlugin };
 //# sourceMappingURL=platform-browser.mjs.map
