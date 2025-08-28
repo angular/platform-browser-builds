12ebe050388a3c5877a435285c1e7f11230da9f6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:13:36 +0000

    fe533cd docs: update Angular CLI help (#63420)

--- fesm2022/platform-browser.mjs
+++ fesm2022/platform-browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-421edfe
+ * @license Angular v20.2.2+sha-fe533cd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -989,7 +989,7 @@ function provideClientHydration(...features) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-421edfe');
+const VERSION = new Version('20.2.2+sha-fe533cd');
 
 export { By, DomSanitizer, EVENT_MANAGER_PLUGINS, EventManagerPlugin, HAMMER_GESTURE_CONFIG, HAMMER_LOADER, HammerGestureConfig, HammerModule, HydrationFeatureKind, Meta, Title, VERSION, disableDebugTools, enableDebugTools, provideClientHydration, withEventReplay, withHttpTransferCacheOptions, withI18nSupport, withIncrementalHydration, withNoHttpTransferCache, DomSanitizerImpl as ɵDomSanitizerImpl, HammerGesturesPlugin as ɵHammerGesturesPlugin };
 //# sourceMappingURL=platform-browser.mjs.map
