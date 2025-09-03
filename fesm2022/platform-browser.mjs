ab19508f93fd9af4c141c22403704874a3b5e463
Miles Malerba <mmalerba@google.com>
2025-09-03 16:39:19 +0000

    991785f build: use common jasmine rule from dev-infra (#63568)

--- fesm2022/platform-browser.mjs
+++ fesm2022/platform-browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-0c2255b
+ * @license Angular v20.2.3+sha-991785f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -989,7 +989,7 @@ function provideClientHydration(...features) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-0c2255b');
+const VERSION = new Version('20.2.3+sha-991785f');
 
 export { By, DomSanitizer, EVENT_MANAGER_PLUGINS, EventManagerPlugin, HAMMER_GESTURE_CONFIG, HAMMER_LOADER, HammerGestureConfig, HammerModule, HydrationFeatureKind, Meta, Title, VERSION, disableDebugTools, enableDebugTools, provideClientHydration, withEventReplay, withHttpTransferCacheOptions, withI18nSupport, withIncrementalHydration, withNoHttpTransferCache, DomSanitizerImpl as ɵDomSanitizerImpl, HammerGesturesPlugin as ɵHammerGesturesPlugin };
 //# sourceMappingURL=platform-browser.mjs.map
