6f33b0096cbe6c06f0206de71655b344f9054844
Miles Malerba <mmalerba@google.com>
2025-09-03 16:05:02 +0000

    e6949ad docs: fix #63555 (#63560)

--- fesm2022/platform-browser.mjs
+++ fesm2022/platform-browser.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-d006721
+ * @license Angular v21.0.0-next.1+sha-e6949ad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -989,7 +989,7 @@ function provideClientHydration(...features) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-d006721');
+const VERSION = new Version('21.0.0-next.1+sha-e6949ad');
 
 export { By, DomSanitizer, EVENT_MANAGER_PLUGINS, EventManagerPlugin, HAMMER_GESTURE_CONFIG, HAMMER_LOADER, HammerGestureConfig, HammerModule, HydrationFeatureKind, Meta, Title, VERSION, disableDebugTools, enableDebugTools, provideClientHydration, withEventReplay, withHttpTransferCacheOptions, withI18nSupport, withIncrementalHydration, withNoHttpTransferCache, DomSanitizerImpl as ɵDomSanitizerImpl, HammerGesturesPlugin as ɵHammerGesturesPlugin };
 //# sourceMappingURL=platform-browser.mjs.map
