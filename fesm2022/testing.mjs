354b81a7dd9385ac19e0b2da14cc030268de5579
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-27 16:27:08 +0000

    8511759 build: update rules_angular digest to 1f1aa3e

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-04dd75b
+ * @license Angular v21.0.0-next.9+sha-8511759
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -12,78 +12,114 @@ import { ɵgetDOM as _getDOM, DOCUMENT } from '@angular/common';
 import { platformBrowser, BrowserModule } from './_browser-chunk.mjs';
 import './_dom_renderer-chunk.mjs';
 
-/**
- * A DOM based implementation of the TestComponentRenderer.
- */
 class DOMTestComponentRenderer extends TestComponentRenderer {
-    _doc;
-    constructor(_doc) {
-        super();
-        this._doc = _doc;
+  _doc;
+  constructor(_doc) {
+    super();
+    this._doc = _doc;
+  }
+  insertRootElement(rootElId, tagName = 'div') {
+    this.removeAllRootElementsImpl();
+    const rootElement = _getDOM().getDefaultDocument().createElement(tagName);
+    rootElement.setAttribute('id', rootElId);
+    this._doc.body.appendChild(rootElement);
+  }
+  removeAllRootElements() {
+    if (typeof this._doc.querySelectorAll === 'function') {
+      this.removeAllRootElementsImpl();
     }
-    insertRootElement(rootElId, tagName = 'div') {
-        this.removeAllRootElementsImpl();
-        const rootElement = _getDOM().getDefaultDocument().createElement(tagName);
-        rootElement.setAttribute('id', rootElId);
-        this._doc.body.appendChild(rootElement);
+  }
+  removeAllRootElementsImpl() {
+    const oldRoots = this._doc.querySelectorAll('[id^=root]');
+    for (let i = 0; i < oldRoots.length; i++) {
+      _getDOM().remove(oldRoots[i]);
     }
-    removeAllRootElements() {
-        // Check whether the `DOCUMENT` instance retrieved from DI contains
-        // the necessary function to complete the cleanup. In tests that don't
-        // interact with DOM, the `DOCUMENT` might be mocked and some functions
-        // might be missing. For such tests, DOM cleanup is not required and
-        // we skip the logic if there are missing functions.
-        if (typeof this._doc.querySelectorAll === 'function') {
-            this.removeAllRootElementsImpl();
-        }
-    }
-    removeAllRootElementsImpl() {
-        const oldRoots = this._doc.querySelectorAll('[id^=root]');
-        for (let i = 0; i < oldRoots.length; i++) {
-            _getDOM().remove(oldRoots[i]);
-        }
-    }
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "21.0.0-next.9+sha-04dd75b", ngImport: i0, type: DOMTestComponentRenderer, deps: [{ token: DOCUMENT }], target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "21.0.0-next.9+sha-04dd75b", ngImport: i0, type: DOMTestComponentRenderer });
+  }
+  static ɵfac = i0.ɵɵngDeclareFactory({
+    minVersion: "12.0.0",
+    version: "21.0.0-next.9+sha-8511759",
+    ngImport: i0,
+    type: DOMTestComponentRenderer,
+    deps: [{
+      token: DOCUMENT
+    }],
+    target: i0.ɵɵFactoryTarget.Injectable
+  });
+  static ɵprov = i0.ɵɵngDeclareInjectable({
+    minVersion: "12.0.0",
+    version: "21.0.0-next.9+sha-8511759",
+    ngImport: i0,
+    type: DOMTestComponentRenderer
+  });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "21.0.0-next.9+sha-04dd75b", ngImport: i0, type: DOMTestComponentRenderer, decorators: [{
-            type: Injectable
-        }], ctorParameters: () => [{ type: undefined, decorators: [{
-                    type: Inject,
-                    args: [DOCUMENT]
-                }] }] });
+i0.ɵɵngDeclareClassMetadata({
+  minVersion: "12.0.0",
+  version: "21.0.0-next.9+sha-8511759",
+  ngImport: i0,
+  type: DOMTestComponentRenderer,
+  decorators: [{
+    type: Injectable
+  }],
+  ctorParameters: () => [{
+    type: undefined,
+    decorators: [{
+      type: Inject,
+      args: [DOCUMENT]
+    }]
+  }]
+});
 
-/**
- * Platform for testing
- *
- * @publicApi
- */
 const platformBrowserTesting = createPlatformFactory(platformBrowser, 'browserTesting');
-/**
- * NgModule for testing.
- *
- * @publicApi
- */
 class BrowserTestingModule {
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "21.0.0-next.9+sha-04dd75b", ngImport: i0, type: BrowserTestingModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
-    static ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "21.0.0-next.9+sha-04dd75b", ngImport: i0, type: BrowserTestingModule, exports: [BrowserModule] });
-    static ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "21.0.0-next.9+sha-04dd75b", ngImport: i0, type: BrowserTestingModule, providers: [
-            { provide: APP_ID, useValue: 'a' },
-            _provideFakePlatformNavigation(),
-            { provide: TestComponentRenderer, useClass: DOMTestComponentRenderer },
-        ], imports: [BrowserModule] });
+  static ɵfac = i0.ɵɵngDeclareFactory({
+    minVersion: "12.0.0",
+    version: "21.0.0-next.9+sha-8511759",
+    ngImport: i0,
+    type: BrowserTestingModule,
+    deps: [],
+    target: i0.ɵɵFactoryTarget.NgModule
+  });
+  static ɵmod = i0.ɵɵngDeclareNgModule({
+    minVersion: "14.0.0",
+    version: "21.0.0-next.9+sha-8511759",
+    ngImport: i0,
+    type: BrowserTestingModule,
+    exports: [BrowserModule]
+  });
+  static ɵinj = i0.ɵɵngDeclareInjector({
+    minVersion: "12.0.0",
+    version: "21.0.0-next.9+sha-8511759",
+    ngImport: i0,
+    type: BrowserTestingModule,
+    providers: [{
+      provide: APP_ID,
+      useValue: 'a'
+    }, _provideFakePlatformNavigation(), {
+      provide: TestComponentRenderer,
+      useClass: DOMTestComponentRenderer
+    }],
+    imports: [BrowserModule]
+  });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "21.0.0-next.9+sha-04dd75b", ngImport: i0, type: BrowserTestingModule, decorators: [{
-            type: NgModule,
-            args: [{
-                    exports: [BrowserModule],
-                    providers: [
-                        { provide: APP_ID, useValue: 'a' },
-                        _provideFakePlatformNavigation(),
-                        { provide: TestComponentRenderer, useClass: DOMTestComponentRenderer },
-                    ],
-                }]
-        }] });
+i0.ɵɵngDeclareClassMetadata({
+  minVersion: "12.0.0",
+  version: "21.0.0-next.9+sha-8511759",
+  ngImport: i0,
+  type: BrowserTestingModule,
+  decorators: [{
+    type: NgModule,
+    args: [{
+      exports: [BrowserModule],
+      providers: [{
+        provide: APP_ID,
+        useValue: 'a'
+      }, _provideFakePlatformNavigation(), {
+        provide: TestComponentRenderer,
+        useClass: DOMTestComponentRenderer
+      }]
+    }]
+  }]
+});
 
 export { BrowserTestingModule, platformBrowserTesting };
 //# sourceMappingURL=testing.mjs.map
