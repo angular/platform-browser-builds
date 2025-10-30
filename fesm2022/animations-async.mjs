87be77b1a43cb1f7e12b5b7a61a8decae53f2dc6
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:27:01 +0000

    fee4baf docs: update dependency injection guides (part 1) (#63652)

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-d23bb3c
+ * @license Angular v21.0.0-rc.0+sha-fee4baf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-d23bb3c",
+    version: "21.0.0-rc.0+sha-fee4baf",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-d23bb3c",
+    version: "21.0.0-rc.0+sha-fee4baf",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.0+sha-d23bb3c",
+  version: "21.0.0-rc.0+sha-fee4baf",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
