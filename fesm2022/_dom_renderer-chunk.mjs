d8a8d84b75b0cd39a0c7518ee8226ce5984441bc
Andrew Scott <atscott@google.com>
2025-11-04 00:43:41 +0000

    3140b87 docs: simplify tutorial signals with self-closing component tags

--- fesm2022/_dom_renderer-chunk.mjs
+++ fesm2022/_dom_renderer-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-34f53aa
+ * @license Angular v21.0.0-rc.0+sha-3140b87
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -32,7 +32,7 @@ class DomEventsPlugin extends EventManagerPlugin {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-34f53aa",
+    version: "21.0.0-rc.0+sha-3140b87",
     ngImport: i0,
     type: DomEventsPlugin,
     deps: [{
@@ -42,14 +42,14 @@ class DomEventsPlugin extends EventManagerPlugin {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-34f53aa",
+    version: "21.0.0-rc.0+sha-3140b87",
     ngImport: i0,
     type: DomEventsPlugin
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.0+sha-34f53aa",
+  version: "21.0.0-rc.0+sha-3140b87",
   ngImport: i0,
   type: DomEventsPlugin,
   decorators: [{
@@ -103,7 +103,7 @@ class EventManager {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-34f53aa",
+    version: "21.0.0-rc.0+sha-3140b87",
     ngImport: i0,
     type: EventManager,
     deps: [{
@@ -115,14 +115,14 @@ class EventManager {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-34f53aa",
+    version: "21.0.0-rc.0+sha-3140b87",
     ngImport: i0,
     type: EventManager
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.0+sha-34f53aa",
+  version: "21.0.0-rc.0+sha-3140b87",
   ngImport: i0,
   type: EventManager,
   decorators: [{
@@ -260,7 +260,7 @@ class SharedStylesHost {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-34f53aa",
+    version: "21.0.0-rc.0+sha-3140b87",
     ngImport: i0,
     type: SharedStylesHost,
     deps: [{
@@ -277,14 +277,14 @@ class SharedStylesHost {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-34f53aa",
+    version: "21.0.0-rc.0+sha-3140b87",
     ngImport: i0,
     type: SharedStylesHost
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.0+sha-34f53aa",
+  version: "21.0.0-rc.0+sha-3140b87",
   ngImport: i0,
   type: SharedStylesHost,
   decorators: [{
@@ -335,7 +335,6 @@ const HOST_ATTR = `_nghost-${COMPONENT_VARIABLE}`;
 const CONTENT_ATTR = `_ngcontent-${COMPONENT_VARIABLE}`;
 const REMOVE_STYLES_ON_COMPONENT_DESTROY_DEFAULT = true;
 const REMOVE_STYLES_ON_COMPONENT_DESTROY = new InjectionToken(typeof ngDevMode !== undefined && ngDevMode ? 'RemoveStylesOnCompDestroy' : '', {
-  providedIn: 'root',
   factory: () => REMOVE_STYLES_ON_COMPONENT_DESTROY_DEFAULT
 });
 function shimContentAttribute(componentShortId) {
@@ -446,7 +445,7 @@ class DomRendererFactory2 {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-34f53aa",
+    version: "21.0.0-rc.0+sha-3140b87",
     ngImport: i0,
     type: DomRendererFactory2,
     deps: [{
@@ -473,14 +472,14 @@ class DomRendererFactory2 {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-34f53aa",
+    version: "21.0.0-rc.0+sha-3140b87",
     ngImport: i0,
     type: DomRendererFactory2
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.0+sha-34f53aa",
+  version: "21.0.0-rc.0+sha-3140b87",
   ngImport: i0,
   type: DomRendererFactory2,
   decorators: [{
