371c0bee31d489260305e99f6af2972d9935e355
Andrew Scott <atscott@google.com>
2025-11-05 17:54:37 +0000

    01c5ba5 docs(docs-infra): fix heading layout on mobile

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-d3d2a34
+ * @license Angular v21.0.0-rc.0+sha-01c5ba5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-d3d2a34",
+    version: "21.0.0-rc.0+sha-01c5ba5",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.0+sha-d3d2a34",
+    version: "21.0.0-rc.0+sha-01c5ba5",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.0+sha-d3d2a34",
+  version: "21.0.0-rc.0+sha-01c5ba5",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
