d80c8a0ec5d10cd245173a7931ba5bea034e14e7
Andrew Kushnir <akushnir@google.com>
2025-11-07 20:08:46 +0000

    781a329 perf(forms): only update interop controls when bound field changes

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-aada582
+ * @license Angular v21.0.0-rc.1+sha-781a329
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.1+sha-aada582",
+    version: "21.0.0-rc.1+sha-781a329",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.1+sha-aada582",
+    version: "21.0.0-rc.1+sha-781a329",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.1+sha-aada582",
+  version: "21.0.0-rc.1+sha-781a329",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
