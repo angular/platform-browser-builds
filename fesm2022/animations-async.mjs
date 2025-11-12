0fd8c3e73513a0c022374e1991ce0a5773dd82a3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 21:24:56 +0000

    be37129 refactor(compiler-cli): rework type checking for signal forms

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-eb9034d
+ * @license Angular v21.0.0-rc.2+sha-be37129
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-eb9034d",
+    version: "21.0.0-rc.2+sha-be37129",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-eb9034d",
+    version: "21.0.0-rc.2+sha-be37129",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-eb9034d",
+  version: "21.0.0-rc.2+sha-be37129",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
