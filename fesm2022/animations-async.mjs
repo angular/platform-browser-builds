07411bc1baca7845e520e93ae3baaf6d3dfbd6d2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 23:35:26 +0000

    a315afb docs: add formArray clear() example clarifies array manipulation

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-e1a7c35
+ * @license Angular v21.0.0-rc.2+sha-a315afb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-e1a7c35",
+    version: "21.0.0-rc.2+sha-a315afb",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-e1a7c35",
+    version: "21.0.0-rc.2+sha-a315afb",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-e1a7c35",
+  version: "21.0.0-rc.2+sha-a315afb",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
