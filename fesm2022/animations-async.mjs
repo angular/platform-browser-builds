28454fe5f48f893168b59a24f42ac9b74239042d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 00:07:02 +0000

    c2a52fd docs: add aria toolbar component guide

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-750da17
+ * @license Angular v21.0.0-rc.2+sha-c2a52fd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-750da17",
+    version: "21.0.0-rc.2+sha-c2a52fd",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-750da17",
+    version: "21.0.0-rc.2+sha-c2a52fd",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-750da17",
+  version: "21.0.0-rc.2+sha-c2a52fd",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
