9c161f56a20c38d1818f774fd04a130e30342881
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-18 00:40:37 +0000

    1b43bfb build: update cross-repo angular dependencies

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-9400afa
+ * @license Angular v21.0.0-rc.3+sha-1b43bfb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.3+sha-9400afa",
+    version: "21.0.0-rc.3+sha-1b43bfb",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.3+sha-9400afa",
+    version: "21.0.0-rc.3+sha-1b43bfb",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.3+sha-9400afa",
+  version: "21.0.0-rc.3+sha-1b43bfb",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
