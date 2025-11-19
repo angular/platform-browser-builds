d9962b99bf7b2fa134e6b0f089ab2313a9c0e2de
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 18:12:49 +0000

    63a95c7 release: cut the v21.0.0 release

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-8b492ce
+ * @license Angular v21.0.0+sha-63a95c7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.3+sha-8b492ce",
+    version: "21.0.0+sha-63a95c7",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.3+sha-8b492ce",
+    version: "21.0.0+sha-63a95c7",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.3+sha-8b492ce",
+  version: "21.0.0+sha-63a95c7",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
