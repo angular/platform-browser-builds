77ac8bdc6a33597b67f4386c1d5e6536392de2ac
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 16:45:13 +0000

    1628125 fix(compiler-cli): ignore non-existent files

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-68f5cee
+ * @license Angular v21.1.0-next.0+sha-1628125
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-68f5cee",
+    version: "21.1.0-next.0+sha-1628125",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-68f5cee",
+    version: "21.1.0-next.0+sha-1628125",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-68f5cee",
+  version: "21.1.0-next.0+sha-1628125",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
