fbec71d0a56c49982c2eb7c324d8c1c8135a7604
kirjs <kirts@google.com>
2025-11-24 16:16:33 +0000

    102cb87 docs: update devtools guide - splits the injector inspector into its own guide - update the screenshots - add defer block section - add hydration section

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-4303f51
+ * @license Angular v21.0.0+sha-102cb87
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-4303f51",
+    version: "21.0.0+sha-102cb87",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-4303f51",
+    version: "21.0.0+sha-102cb87",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0+sha-4303f51",
+  version: "21.0.0+sha-102cb87",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
