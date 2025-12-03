77f9317c2bf206466f361a085e198874f3ce75b2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 14:21:19 +0000

    6b4ab87 feat(forms): Allows transforms on `FormUiControl` signals

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-6491df9
+ * @license Angular v21.0.2+sha-6b4ab87
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-6491df9",
+    version: "21.0.2+sha-6b4ab87",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-6491df9",
+    version: "21.0.2+sha-6b4ab87",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.2+sha-6491df9",
+  version: "21.0.2+sha-6b4ab87",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
