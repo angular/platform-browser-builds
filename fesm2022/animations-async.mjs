d8969d1bec58d46fcf78812227b427855aa630a8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:55:32 +0000

    d343ff3 docs: release notes for the v21.0.4 release

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-a52c834
+ * @license Angular v21.1.0-next.1+sha-d343ff3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.1+sha-a52c834",
+    version: "21.1.0-next.1+sha-d343ff3",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.1+sha-a52c834",
+    version: "21.1.0-next.1+sha-d343ff3",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.1+sha-a52c834",
+  version: "21.1.0-next.1+sha-d343ff3",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
