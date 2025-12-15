38c943c1bbf6cb86e79d92f69ae66520c359fecd
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 19:40:40 +0000

    a2aa8fe docs: format CLI builder JSON examples using fenced blocks

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-23fbe5c
+ * @license Angular v21.1.0-next.3+sha-a2aa8fe
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-23fbe5c",
+    version: "21.1.0-next.3+sha-a2aa8fe",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-23fbe5c",
+    version: "21.1.0-next.3+sha-a2aa8fe",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.3+sha-23fbe5c",
+  version: "21.1.0-next.3+sha-a2aa8fe",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
