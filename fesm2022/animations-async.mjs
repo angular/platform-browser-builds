99e9b5c11fa2c717705a7a821af3f058e6b2ec19
Andrew Kushnir <akushnir@google.com>
2025-12-17 23:47:35 +0000

    59b3790 refactor(forms): rename server errors to submission errors

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-0edbee4
+ * @license Angular v21.0.6+sha-59b3790
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.6+sha-0edbee4",
+    version: "21.0.6+sha-59b3790",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.6+sha-0edbee4",
+    version: "21.0.6+sha-59b3790",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.6+sha-0edbee4",
+  version: "21.0.6+sha-59b3790",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
