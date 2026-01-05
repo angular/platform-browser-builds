6b8676428600194e361a40c0c43a759de0a5be76
kirjs <kirts@google.com>
2026-01-05 17:43:03 +0000

    9494b56 docs(docs-infra): sync heading colors between docs and API pages

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-7e9dc56
+ * @license Angular v21.0.6+sha-9494b56
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.6+sha-7e9dc56",
+    version: "21.0.6+sha-9494b56",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.6+sha-7e9dc56",
+    version: "21.0.6+sha-9494b56",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.6+sha-7e9dc56",
+  version: "21.0.6+sha-9494b56",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
