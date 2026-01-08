da1e2716d7557908f47f77945d7ca7eb6557daf1
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 19:34:30 +0000

    5239e47 fix(core): handle cancelled traversals in fake navigation

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.7+sha-8ff2776
+ * @license Angular v21.0.7+sha-5239e47
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.7+sha-8ff2776",
+    version: "21.0.7+sha-5239e47",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.7+sha-8ff2776",
+    version: "21.0.7+sha-5239e47",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.7+sha-8ff2776",
+  version: "21.0.7+sha-5239e47",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
