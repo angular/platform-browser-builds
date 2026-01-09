1f272a11540f45691100325fba79bf17d7fd55f9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 17:01:14 +0000

    24fa4a2 docs(forms): Clarify returning errors from submit functions

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-491a156
+ * @license Angular v21.0.8+sha-24fa4a2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.8+sha-491a156",
+    version: "21.0.8+sha-24fa4a2",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.8+sha-491a156",
+    version: "21.0.8+sha-24fa4a2",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.8+sha-491a156",
+  version: "21.0.8+sha-24fa4a2",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
