28791b45a14f347a535af08bd408fc7f5589e18b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 17:31:30 +0000

    e3b3290 docs: replace UnambiguousRoleValidator in asyc-valdotors part form-validation.md

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e6a8bb8
+ * @license Angular v21.2.0-next.0+sha-e3b3290
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-e6a8bb8",
+    version: "21.2.0-next.0+sha-e3b3290",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-e6a8bb8",
+    version: "21.2.0-next.0+sha-e3b3290",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-e6a8bb8",
+  version: "21.2.0-next.0+sha-e3b3290",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
