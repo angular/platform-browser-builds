d9b53cd59d8f300d6014f2a15bd1dd879ba42e9e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:23:47 +0000

    4871131 docs: improve separation between enter and leave example

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-c7d6053
+ * @license Angular v21.1.0-rc.0+sha-4871131
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-rc.0+sha-c7d6053",
+    version: "21.1.0-rc.0+sha-4871131",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-rc.0+sha-c7d6053",
+    version: "21.1.0-rc.0+sha-4871131",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-rc.0+sha-c7d6053",
+  version: "21.1.0-rc.0+sha-4871131",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
