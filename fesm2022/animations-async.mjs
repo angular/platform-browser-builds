2860c4f8689d0952113bdefe1b361f41935f48ac
Alon Mishne <amishne@users.noreply.github.com>
2026-01-15 19:14:08 +0000

    0643f54 docs: clarify that FESM can depend on shared chunks

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-c0446da
+ * @license Angular v21.1.0+sha-0643f54
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-c0446da",
+    version: "21.1.0+sha-0643f54",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-c0446da",
+    version: "21.1.0+sha-0643f54",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0+sha-c0446da",
+  version: "21.1.0+sha-0643f54",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
