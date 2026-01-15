6bb4641cb5aa9abbdbe0fc36df97556034da7ac1
Alon Mishne <amishne@users.noreply.github.com>
2026-01-15 17:04:50 +0000

    c0446da docs(docs-infra): prevent heading from linking symbols

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-ae4e2e0
+ * @license Angular v21.1.0+sha-c0446da
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-ae4e2e0",
+    version: "21.1.0+sha-c0446da",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-ae4e2e0",
+    version: "21.1.0+sha-c0446da",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0+sha-ae4e2e0",
+  version: "21.1.0+sha-c0446da",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
