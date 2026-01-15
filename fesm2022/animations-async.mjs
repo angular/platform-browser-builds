fa853817bcc56279dd1f6304cf839c0af7adc61a
Alon Mishne <amishne@users.noreply.github.com>
2026-01-15 21:48:41 +0000

    ded654d build: initial test of TypeScript 6

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-aa39f87
+ * @license Angular v21.1.0+sha-ded654d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-aa39f87",
+    version: "21.1.0+sha-ded654d",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-aa39f87",
+    version: "21.1.0+sha-ded654d",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0+sha-aa39f87",
+  version: "21.1.0+sha-ded654d",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
