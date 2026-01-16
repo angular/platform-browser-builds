365faa58d7885ad3cb63507cf3a2087f6259e3c0
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 17:24:53 +0000

    54fc393 build: update dependency @csstools/css-calc to v3

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-d9c980a
+ * @license Angular v21.2.0-next.0+sha-54fc393
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-d9c980a",
+    version: "21.2.0-next.0+sha-54fc393",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-d9c980a",
+    version: "21.2.0-next.0+sha-54fc393",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-d9c980a",
+  version: "21.2.0-next.0+sha-54fc393",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
