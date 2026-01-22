a8f3e53a12267bcd5178a2b7d16f3b7879207600
Andrew Scott <atscott01@gmail.com>
2026-01-22 23:05:50 +0000

    30b50e7 docs(docs-infra): Exempts more symbols from automatic linking

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ebae211
+ * @license Angular v21.2.0-next.0+sha-30b50e7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-ebae211",
+    version: "21.2.0-next.0+sha-30b50e7",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-ebae211",
+    version: "21.2.0-next.0+sha-30b50e7",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-ebae211",
+  version: "21.2.0-next.0+sha-30b50e7",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
