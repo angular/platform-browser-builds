2eba997384cdb5b683ae1110270779f884666669
Andrew Scott <atscott01@gmail.com>
2026-01-23 18:49:04 +0000

    b203ab0 build: update bazel dependencies

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-f7eb996
+ * @license Angular v21.1.1+sha-b203ab0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-f7eb996",
+    version: "21.1.1+sha-b203ab0",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-f7eb996",
+    version: "21.1.1+sha-b203ab0",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.1+sha-f7eb996",
+  version: "21.1.1+sha-b203ab0",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
