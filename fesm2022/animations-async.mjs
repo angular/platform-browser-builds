fb0a2eb4644d484d8a0a8adb2eed26cf37bccf8f
Andrew Scott <atscott@google.com>
2026-01-23 01:07:52 +0000

    f7eb996 docs: update signal form tutorials to use `FormField`

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-d9e7ab3
+ * @license Angular v21.1.1+sha-f7eb996
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-d9e7ab3",
+    version: "21.1.1+sha-f7eb996",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-d9e7ab3",
+    version: "21.1.1+sha-f7eb996",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.1+sha-d9e7ab3",
+  version: "21.1.1+sha-f7eb996",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
