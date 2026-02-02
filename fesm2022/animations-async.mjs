00eeeaecc27a6a86f98416308da9e9c0151b5143
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 18:04:58 +0000

    eb28032 build: update cross-repo angular dependencies

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-6f75b6e-with-local-changes
+ * @license Angular v21.1.2+sha-eb28032
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.2+sha-6f75b6e-with-local-changes",
+    version: "21.1.2+sha-eb28032",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.2+sha-6f75b6e-with-local-changes",
+    version: "21.1.2+sha-eb28032",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.2+sha-6f75b6e-with-local-changes",
+  version: "21.1.2+sha-eb28032",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
