c0a850733f778670f3eb6abc354be23c607b92e0
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 18:23:06 +0000

    a64a90d docs(animations): fix small typo in animations migrations docs

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-8d5210c
+ * @license Angular v21.2.0-next.1+sha-a64a90d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.1+sha-8d5210c",
+    version: "21.2.0-next.1+sha-a64a90d",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.1+sha-8d5210c",
+    version: "21.2.0-next.1+sha-a64a90d",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.1+sha-8d5210c",
+  version: "21.2.0-next.1+sha-a64a90d",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
