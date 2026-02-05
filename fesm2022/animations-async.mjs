92a9512bdc7e7ae3060e762d9712b5d2f346602f
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-05 00:20:11 +0000

    c3229b3 release: cut the v21.2.0-next.2 release

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-8fe34e1
+ * @license Angular v21.2.0-next.2+sha-c3229b3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.1+sha-8fe34e1",
+    version: "21.2.0-next.2+sha-c3229b3",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.1+sha-8fe34e1",
+    version: "21.2.0-next.2+sha-c3229b3",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.1+sha-8fe34e1",
+  version: "21.2.0-next.2+sha-c3229b3",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
