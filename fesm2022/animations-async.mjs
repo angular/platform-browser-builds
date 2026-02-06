59d99d8faf3ee71672fe10eea1fc52ddcc5b0440
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 15:47:57 +0000

    24c0c5a feat(forms): support signal-based schemas in validateStandardSchema

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-6fa2270
+ * @license Angular v21.2.0-next.2+sha-24c0c5a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.2+sha-6fa2270",
+    version: "21.2.0-next.2+sha-24c0c5a",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.2+sha-6fa2270",
+    version: "21.2.0-next.2+sha-24c0c5a",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.2+sha-6fa2270",
+  version: "21.2.0-next.2+sha-24c0c5a",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
