61f8c942f6754ccda456ecc8c70ba882e26d9945
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 22:06:48 +0000

    4937d16 ci: update pullapprove reviewer list

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-a4fceda
+ * @license Angular v21.2.0-next.3+sha-4937d16
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.3+sha-a4fceda",
+    version: "21.2.0-next.3+sha-4937d16",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.3+sha-a4fceda",
+    version: "21.2.0-next.3+sha-4937d16",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.3+sha-a4fceda",
+  version: "21.2.0-next.3+sha-4937d16",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
