53f29cf4e1c862fc84426c23204ae8a97452e22b
Matthew Beck <me@mattrb.com>
2026-02-18 19:11:55 +0000

    a4eb347 docs: add signal forms async operations guide

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-3c494b4
+ * @license Angular v21.1.4+sha-a4eb347
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.4+sha-3c494b4",
+    version: "21.1.4+sha-a4eb347",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.4+sha-3c494b4",
+    version: "21.1.4+sha-a4eb347",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.4+sha-3c494b4",
+  version: "21.1.4+sha-a4eb347",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
