03d37f6f474e2a3cfcedbbd864fd8fd4997d4836
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 17:24:25 +0000

    246a984 feat(core): add TestBed.getFixture

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-f90e556
+ * @license Angular v22.0.0-next.0+sha-246a984
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-f90e556",
+    version: "22.0.0-next.0+sha-246a984",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-f90e556",
+    version: "22.0.0-next.0+sha-246a984",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.0+sha-f90e556",
+  version: "22.0.0-next.0+sha-246a984",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
