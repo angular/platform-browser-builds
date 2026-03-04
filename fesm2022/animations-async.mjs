e479b2c6462b87b7b2bc98793c2c05eb9c795fc5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-04 16:10:31 +0000

    84e79f5 build: update pnpm to v10.30.3

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-657d532
+ * @license Angular v21.2.0+sha-84e79f5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0+sha-657d532",
+    version: "21.2.0+sha-84e79f5",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0+sha-657d532",
+    version: "21.2.0+sha-84e79f5",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0+sha-657d532",
+  version: "21.2.0+sha-84e79f5",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
