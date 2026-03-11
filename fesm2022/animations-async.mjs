2b1259ac138c5a2785c664fc88a7f66ccd83b4be
Andrew Scott <atscott01@gmail.com>
2026-03-11 18:40:36 +0000

    21b1c3b fix(core): include signal debug names in their `toString()` representation

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.2+sha-6c73aac
+ * @license Angular v21.2.2+sha-21b1c3b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.2+sha-6c73aac",
+    version: "21.2.2+sha-21b1c3b",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.2+sha-6c73aac",
+    version: "21.2.2+sha-21b1c3b",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.2+sha-6c73aac",
+  version: "21.2.2+sha-21b1c3b",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
