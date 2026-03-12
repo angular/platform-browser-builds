63b9e06bd847145ffb777b36482f2122a2416c5c
Matthew Beck <me@mattrb.com>
2026-03-12 17:11:54 +0000

    ed2d324 fix(compiler): disallow translations of iframe src

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.3+sha-c06e3a0
+ * @license Angular v21.2.3+sha-ed2d324
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.3+sha-c06e3a0",
+    version: "21.2.3+sha-ed2d324",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.3+sha-c06e3a0",
+    version: "21.2.3+sha-ed2d324",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.3+sha-c06e3a0",
+  version: "21.2.3+sha-ed2d324",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
