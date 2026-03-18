951f88f5b6395ee26171101df6ae83bfa400928b
Matthew Beck <me@mattrb.com>
2026-03-18 00:08:55 +0000

    076d41c fix(migrations): prevent trailing comma syntax errors after removing NgStyle

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-a8f80c1
+ * @license Angular v21.2.4+sha-076d41c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.4+sha-a8f80c1",
+    version: "21.2.4+sha-076d41c",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.4+sha-a8f80c1",
+    version: "21.2.4+sha-076d41c",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.4+sha-a8f80c1",
+  version: "21.2.4+sha-076d41c",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
