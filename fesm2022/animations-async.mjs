b27b615454d16a7451eef596adf47960b97909cc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-26 17:54:11 +0000

    807cabd fix(dev-infra): improve PR review skill handling of git worktrees

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-13f050d
+ * @license Angular v21.2.6+sha-807cabd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.6+sha-13f050d",
+    version: "21.2.6+sha-807cabd",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.6+sha-13f050d",
+    version: "21.2.6+sha-807cabd",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.6+sha-13f050d",
+  version: "21.2.6+sha-807cabd",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
