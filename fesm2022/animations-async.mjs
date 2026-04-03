297be71eb7f1335cf46175b0c1845b5ba49e2c97
Andrew Scott <atscott01@gmail.com>
2026-04-03 18:30:22 +0000

    b0dc2fb docs(forms): clarify disabled FormArray value behavior

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-ef204bd
+ * @license Angular v21.2.7+sha-b0dc2fb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.7+sha-ef204bd",
+    version: "21.2.7+sha-b0dc2fb",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.7+sha-ef204bd",
+    version: "21.2.7+sha-b0dc2fb",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.7+sha-ef204bd",
+  version: "21.2.7+sha-b0dc2fb",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
