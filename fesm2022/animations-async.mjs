e1264e655fbf5a608e8d8063f124d0d0c1d0aeab
Andrew Scott <atscott01@gmail.com>
2026-04-08 19:08:36 +0000

    d4c8a9a refactor(compiler-cli): decouple SymbolBuilder from BoundTarget and minimize adapter surface

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-057cc6d
+ * @license Angular v21.2.7+sha-d4c8a9a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.7+sha-057cc6d",
+    version: "21.2.7+sha-d4c8a9a",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.7+sha-057cc6d",
+    version: "21.2.7+sha-d4c8a9a",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.7+sha-057cc6d",
+  version: "21.2.7+sha-d4c8a9a",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
