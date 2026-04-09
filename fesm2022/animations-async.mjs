b121db530ce4a2252bb45b88b198049aeaa1f616
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 15:19:06 +0000

    b9265cc docs: update typescript version requirement for angular 22

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-1a16a40
+ * @license Angular v22.0.0-next.7+sha-b9265cc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.7+sha-1a16a40",
+    version: "22.0.0-next.7+sha-b9265cc",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.7+sha-1a16a40",
+    version: "22.0.0-next.7+sha-b9265cc",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.7+sha-1a16a40",
+  version: "22.0.0-next.7+sha-b9265cc",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
