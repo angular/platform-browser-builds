56d4b6b844041fd58a7e557c9992fc15b3aeb622
kirjs <kirts@google.com>
2026-04-14 15:36:57 +0000

    c3d69ae fix(docs-infra): prevent inline code wrapping in CLI reference table

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-684e9fd
+ * @license Angular v21.2.8+sha-c3d69ae
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.8+sha-684e9fd",
+    version: "21.2.8+sha-c3d69ae",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.8+sha-684e9fd",
+    version: "21.2.8+sha-c3d69ae",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.8+sha-684e9fd",
+  version: "21.2.8+sha-c3d69ae",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
