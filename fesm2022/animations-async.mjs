438571a6ffee62a279d5492c39c214662150475b
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 17:00:22 +0000

    e5b93ea docs: fix wording in reactive forms guide

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-ce883d9
+ * @license Angular v21.2.9+sha-e5b93ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.9+sha-ce883d9",
+    version: "21.2.9+sha-e5b93ea",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.9+sha-ce883d9",
+    version: "21.2.9+sha-e5b93ea",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.9+sha-ce883d9",
+  version: "21.2.9+sha-e5b93ea",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
