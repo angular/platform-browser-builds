459c3906af5730436ddea6345bca3fdb19445678
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 18:29:29 +0000

    7f1cfcb docs: clarify ngDoCheck invocation behavior with OnPush strategy

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-cbddb5c
+ * @license Angular v22.0.0-rc.0+sha-7f1cfcb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-rc.0+sha-cbddb5c",
+    version: "22.0.0-rc.0+sha-7f1cfcb",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-rc.0+sha-cbddb5c",
+    version: "22.0.0-rc.0+sha-7f1cfcb",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-rc.0+sha-cbddb5c",
+  version: "22.0.0-rc.0+sha-7f1cfcb",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
