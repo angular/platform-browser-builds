2af7e25b6d4eee5408f6afcfff5809060b91852e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 13:08:44 +0000

    e81c7e8 refactor(forms): type built-in getError results

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-4795b35
+ * @license Angular v22.0.0-rc.2+sha-e81c7e8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-rc.2+sha-4795b35",
+    version: "22.0.0-rc.2+sha-e81c7e8",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-rc.2+sha-4795b35",
+    version: "22.0.0-rc.2+sha-e81c7e8",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-rc.2+sha-4795b35",
+  version: "22.0.0-rc.2+sha-e81c7e8",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
