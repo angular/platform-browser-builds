eb49e325b3dfde1f9f502ce927607eedbee2a09a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 14:41:03 +0000

    61b3483 docs: align `main` and `22.0.x` for aria guides

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-2891f7e
+ * @license Angular v22.0.0-rc.2+sha-61b3483
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-rc.2+sha-2891f7e",
+    version: "22.0.0-rc.2+sha-61b3483",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-rc.2+sha-2891f7e",
+    version: "22.0.0-rc.2+sha-61b3483",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-rc.2+sha-2891f7e",
+  version: "22.0.0-rc.2+sha-61b3483",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
