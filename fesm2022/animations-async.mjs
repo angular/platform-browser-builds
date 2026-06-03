5a1c32587bd6c81c2ebde7128b2b34e7d4ad010c
Andrew Scott <atscott01@gmail.com>
2026-06-03 19:17:43 +0000

    3d9ca2f fix(compiler-cli): bind switch exhaustive check expressions

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-32b348a
+ * @license Angular v22.0.0+sha-3d9ca2f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0+sha-32b348a",
+    version: "22.0.0+sha-3d9ca2f",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0+sha-32b348a",
+    version: "22.0.0+sha-3d9ca2f",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0+sha-32b348a",
+  version: "22.0.0+sha-3d9ca2f",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
