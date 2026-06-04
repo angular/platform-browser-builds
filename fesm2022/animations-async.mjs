f45309b68cf53472c54388ae8f2780c5aa5452db
Andrew Scott <atscott01@gmail.com>
2026-06-04 18:50:32 +0000

    e50d47a fix(core): disable WebMCP during SSR

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-8da1198
+ * @license Angular v22.1.0-next.0+sha-e50d47a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-8da1198",
+    version: "22.1.0-next.0+sha-e50d47a",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-8da1198",
+    version: "22.1.0-next.0+sha-e50d47a",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.0-next.0+sha-8da1198",
+  version: "22.1.0-next.0+sha-e50d47a",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
