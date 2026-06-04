cc4d225167688a0fcb83aa0b7b9344ab1960c548
Andrew Scott <atscott01@gmail.com>
2026-06-04 21:25:00 +0000

    df77e42 fix(forms): set `additionalProperties: false` on generated WebMCP form

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-cb8ceb1
+ * @license Angular v22.1.0-next.0+sha-df77e42
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-cb8ceb1",
+    version: "22.1.0-next.0+sha-df77e42",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-cb8ceb1",
+    version: "22.1.0-next.0+sha-df77e42",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.0-next.0+sha-cb8ceb1",
+  version: "22.1.0-next.0+sha-df77e42",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
