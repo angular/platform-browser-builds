f5a52b71fea67378a5ddb987e758d057da4c904c
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-12 17:24:23 +0000

    1d97355 docs: clarify value attribute on radio/checkbox inputs is allowed with formField

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-15314c1
+ * @license Angular v22.0.1+sha-1d97355
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.1+sha-15314c1",
+    version: "22.0.1+sha-1d97355",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.1+sha-15314c1",
+    version: "22.0.1+sha-1d97355",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.1+sha-15314c1",
+  version: "22.0.1+sha-1d97355",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
