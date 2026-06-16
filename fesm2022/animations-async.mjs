097adb46d5d5e804cba3ed20d20596667ed488d0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 15:55:50 +0000

    f2c5045 docs: use the @Service decorator in the learn-angular DI tutorial

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-93d0a5f
+ * @license Angular v22.0.1+sha-f2c5045
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.1+sha-93d0a5f",
+    version: "22.0.1+sha-f2c5045",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.1+sha-93d0a5f",
+    version: "22.0.1+sha-f2c5045",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.1+sha-93d0a5f",
+  version: "22.0.1+sha-f2c5045",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
