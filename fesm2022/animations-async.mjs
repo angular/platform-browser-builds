e4983969eacdacf60d02ef28dd35803155842cfa
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 20:17:45 +0000

    45192ba ci: update dev-infra reference to new commit SHA

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-1e79dd3
+ * @license Angular v22.1.0-next.0+sha-45192ba
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-1e79dd3",
+    version: "22.1.0-next.0+sha-45192ba",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-1e79dd3",
+    version: "22.1.0-next.0+sha-45192ba",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.0-next.0+sha-1e79dd3",
+  version: "22.1.0-next.0+sha-45192ba",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
