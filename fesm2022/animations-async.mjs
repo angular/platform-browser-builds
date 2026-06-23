e6bf9bd3756db73247737f44a013eaceba0185d6
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 18:22:49 +0000

    50f252e build: update bazel dependencies

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-2799304
+ * @license Angular v22.0.2+sha-50f252e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.2+sha-2799304",
+    version: "22.0.2+sha-50f252e",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.2+sha-2799304",
+    version: "22.0.2+sha-50f252e",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.2+sha-2799304",
+  version: "22.0.2+sha-50f252e",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
