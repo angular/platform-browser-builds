2db6e1e47cfd53294c6b51a87f13e4689db9f829
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 14:40:31 +0000

    548ef47 release: cut the v22.0.8 release

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.7+sha-45eef01
+ * @license Angular v22.0.8+sha-548ef47
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.7+sha-45eef01",
+    version: "22.0.8+sha-548ef47",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.7+sha-45eef01",
+    version: "22.0.8+sha-548ef47",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.7+sha-45eef01",
+  version: "22.0.8+sha-548ef47",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
