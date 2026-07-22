559f4eb1790d887b8146dad7665381bc881f6861
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 14:51:29 +0000

    3bf2430 docs: release notes for the v22.1.0-rc.0 release

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-509cad6
+ * @license Angular v22.2.0-next.0+sha-3bf2430
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.0-next.6+sha-509cad6",
+    version: "22.2.0-next.0+sha-3bf2430",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.0-next.6+sha-509cad6",
+    version: "22.2.0-next.0+sha-3bf2430",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.0-next.6+sha-509cad6",
+  version: "22.2.0-next.0+sha-3bf2430",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
