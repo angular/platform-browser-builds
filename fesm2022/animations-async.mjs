2971c13373272e5730d8acc00156a3a15cb9b90f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 15:33:52 +0000

    28d59e8 docs: use https for external links in adev

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-e779309
+ * @license Angular v22.2.0-next.0+sha-28d59e8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.0+sha-e779309",
+    version: "22.2.0-next.0+sha-28d59e8",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.0+sha-e779309",
+    version: "22.2.0-next.0+sha-28d59e8",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.0+sha-e779309",
+  version: "22.2.0-next.0+sha-28d59e8",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
