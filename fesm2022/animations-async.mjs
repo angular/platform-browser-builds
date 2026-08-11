b2a6528a4e1790430e45274395bbd18eab8eabec
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-11 19:15:57 +0000

    435f8b2 fix(router): limit protocol-relative URL handling to serialization

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.1+sha-b65dea4
+ * @license Angular v22.2.0-next.1+sha-435f8b2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.1+sha-b65dea4",
+    version: "22.2.0-next.1+sha-435f8b2",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.1+sha-b65dea4",
+    version: "22.2.0-next.1+sha-435f8b2",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.1+sha-b65dea4",
+  version: "22.2.0-next.1+sha-435f8b2",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
