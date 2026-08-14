afb25818d97f720038baac7756429a6f7f87d0b0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-14 15:29:48 +0000

    0cd243d build: resolve CI failures

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.2+sha-a9f8fb2
+ * @license Angular v22.1.2+sha-0cd243d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.2+sha-a9f8fb2",
+    version: "22.1.2+sha-0cd243d",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.2+sha-a9f8fb2",
+    version: "22.1.2+sha-0cd243d",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.2+sha-a9f8fb2",
+  version: "22.1.2+sha-0cd243d",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
