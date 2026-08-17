56f1888013ce3aefdde031d927c337c0df5bf150
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-17 20:42:04 +0000

    1c2b057 feat(docs-infra): add Angie to the roadmap illustration

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.2+sha-5980138
+ * @license Angular v22.2.0-next.2+sha-1c2b057
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.2+sha-5980138",
+    version: "22.2.0-next.2+sha-1c2b057",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.2+sha-5980138",
+    version: "22.2.0-next.2+sha-1c2b057",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.2+sha-5980138",
+  version: "22.2.0-next.2+sha-1c2b057",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
