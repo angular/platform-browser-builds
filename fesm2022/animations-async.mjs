9d151ec828d6f2cd2a8fc3496945d974eca95aeb
Matthew Beck <me@mattrb.com>
2026-09-03 02:41:08 +0000

    468b65b release: cut the v22.1.5 release

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-e03f20a
+ * @license Angular v22.1.5+sha-468b65b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.4+sha-e03f20a",
+    version: "22.1.5+sha-468b65b",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.4+sha-e03f20a",
+    version: "22.1.5+sha-468b65b",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.4+sha-e03f20a",
+  version: "22.1.5+sha-468b65b",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
