265e004a2bad75442ebe1e929f5d12305cd6fc2f
Andrew Scott <atscott01@gmail.com>
2026-09-11 23:09:07 +0000

    fc2f6fd docs: stop the disabled aria toolbar radio group from changing selection

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-f2cf65a
+ * @license Angular v22.2.0-next.7+sha-fc2f6fd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.7+sha-f2cf65a",
+    version: "22.2.0-next.7+sha-fc2f6fd",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.7+sha-f2cf65a",
+    version: "22.2.0-next.7+sha-fc2f6fd",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.7+sha-f2cf65a",
+  version: "22.2.0-next.7+sha-fc2f6fd",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
