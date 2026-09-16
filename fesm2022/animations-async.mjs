1b58eeaf64f540007bf69fa1a2d4a1b63cf61776
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 14:54:41 +0000

    312e1d8 feat(compiler-cli): add strictUnclaimedEventNames option to catch misspelled output bindings

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-5cdae0a
+ * @license Angular v22.2.0-next.7+sha-312e1d8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.7+sha-5cdae0a",
+    version: "22.2.0-next.7+sha-312e1d8",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.7+sha-5cdae0a",
+    version: "22.2.0-next.7+sha-312e1d8",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.7+sha-5cdae0a",
+  version: "22.2.0-next.7+sha-312e1d8",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
