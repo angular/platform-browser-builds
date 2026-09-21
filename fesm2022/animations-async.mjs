5bbb475b600fb91565bdda928a4bee15e4ace2dc
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 19:17:02 +0000

    805aa91 docs: update `disabledReasons` and `transformedValue` examples

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-78c7ab3
+ * @license Angular v22.1.7+sha-805aa91
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.7+sha-78c7ab3",
+    version: "22.1.7+sha-805aa91",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.7+sha-78c7ab3",
+    version: "22.1.7+sha-805aa91",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.7+sha-78c7ab3",
+  version: "22.1.7+sha-805aa91",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
