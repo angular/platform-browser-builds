1c023d3b1122c7a740ca9c67bd85b68f3d3a3fc4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 17:53:34 +0000

    e65f114 release: cut the v21.2.24 release

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.23+sha-03872a8
+ * @license Angular v21.2.24+sha-e65f114
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.23+sha-03872a8",
+    version: "21.2.24+sha-e65f114",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.23+sha-03872a8",
+    version: "21.2.24+sha-e65f114",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.23+sha-03872a8",
+  version: "21.2.24+sha-e65f114",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
