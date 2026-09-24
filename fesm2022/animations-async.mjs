e6c3aaa1be3c509caf6da0a0876eb4f95ddb212a
Andrew Scott <atscott01@gmail.com>
2026-09-24 19:45:18 +0000

    21851fc fix(core): add debugName on afterRenderEffect

--- fesm2022/animations-async.mjs
+++ fesm2022/animations-async.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0+sha-7f9c14d
+ * @license Angular v22.2.0+sha-21851fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -92,7 +92,7 @@ class AsyncAnimationRendererFactory {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0+sha-7f9c14d",
+    version: "22.2.0+sha-21851fc",
     ngImport: i0,
     type: AsyncAnimationRendererFactory,
     deps: "invalid",
@@ -100,14 +100,14 @@ class AsyncAnimationRendererFactory {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0+sha-7f9c14d",
+    version: "22.2.0+sha-21851fc",
     ngImport: i0,
     type: AsyncAnimationRendererFactory
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0+sha-7f9c14d",
+  version: "22.2.0+sha-21851fc",
   ngImport: i0,
   type: AsyncAnimationRendererFactory,
   decorators: [{
